b0d0348eefa6ee139376b84482df6e481859a541
like2000 <kevin.shing.bruce.li@cern.ch>
2014-04-18 19:59:04 +0200

    Add new compact particle monitor.

--- monitors/monitors.py
+++ monitors/monitors.py
@@ -102,7 +102,7 @@ class BunchMonitor(Monitor):
 
 class SliceMonitor(Monitor):
 
-    def __init__(self, filename, n_steps):
+    def __init__(self, filename, n_steps, dictionary=None):
 
         self.h5file = hp.File(filename + '.h5', 'w')
         self.n_steps = n_steps
@@ -195,51 +195,83 @@ class SliceMonitor(Monitor):
 
 class ParticleMonitor(Monitor):
 
-    def __init__(self, filename, stride=1):
+    def __init__(self, filename, stride=1, dictionary=None, n_steps=None):
 
         self.h5file = hp.File(filename + '.h5part', 'w')
-        self.stride = stride
-        # self.n_steps = n_steps
+        self.n_steps = n_steps
         self.i_steps = 0
+        self.stride = stride
 
+        if dictionary:
+            for key in dictionary:
+                self.h5file.attrs[key] = dictionary[key]
+        
     def dump(self, bunch):
 
-        if not self.i_steps:
-            resorting_indices = np.argsort(bunch.id)[::self.stride]
-            self.z0 = np.copy(bunch.dz[resorting_indices])
+        if self.n_steps:
+            if not self.i_steps:
+                resorting_indices = np.argsort(bunch.id)[::self.stride]
+                self.z0 = np.copy(bunch.dz[resorting_indices])
+                n_macroparticles = len(self.z0)
 
-        h5group = self.h5file.create_group("Step#" + str(self.i_steps))
-        self.create_data(h5group, (bunch.n_macroparticles // self.stride,))
-        self.write_data(bunch, h5group)
+                self.create_data(self.h5file, (n_macroparticles, self.n_steps))
+
+            self.write_data(bunch, self.i_steps)
+        else:
+            if not self.i_steps:
+                resorting_indices = np.argsort(bunch.id)[::self.stride]
+                self.z0 = np.copy(bunch.dz[resorting_indices])
+            n_macroparticles = len(self.z0)
+
+            h5group = self.h5file.create_group("Step#" + str(self.i_steps))
+            self.create_data(h5group, (n_macroparticles,))
+            self.write_data(bunch, h5group)
 
         self.i_steps += 1
 
     def create_data(self, h5group, dims):
 
-        h5group.create_dataset("x", dims) # perhaps name to x1, x2, x3, v1, v2, v3
-        h5group.create_dataset("xp", dims)
-        h5group.create_dataset("y", dims)
-        h5group.create_dataset("yp", dims)
-        h5group.create_dataset("dz", dims)
-        h5group.create_dataset("dp", dims)
+        if self.n_steps:
+            h5group.create_dataset("x", dims, compression="gzip", compression_opts=9)
+            h5group.create_dataset("xp", dims, compression="gzip", compression_opts=9)
+            h5group.create_dataset("y", dims, compression="gzip", compression_opts=9)
+            h5group.create_dataset("yp", dims, compression="gzip", compression_opts=9)
+            h5group.create_dataset("dz", dims, compression="gzip", compression_opts=9)
+            h5group.create_dataset("dp", dims, compression="gzip", compression_opts=9)
+        else:
+            h5group.create_dataset("x1", dims, compression="gzip", compression_opts=9)
+            h5group.create_dataset("v1", dims, compression="gzip", compression_opts=9)
+            h5group.create_dataset("x2", dims, compression="gzip", compression_opts=9)
+            h5group.create_dataset("v2", dims, compression="gzip", compression_opts=9)
+            h5group.create_dataset("x3", dims, compression="gzip", compression_opts=9)
+            h5group.create_dataset("v3", dims, compression="gzip", compression_opts=9)
 
         # Do we need/want this here?
-        h5group.create_dataset("id", dims, dtype=np.int)
-
-        h5group.create_dataset("c", dims)
+        h5group.create_dataset("id", dims, dtype=np.int, compression="gzip", compression_opts=9)
+        h5group.create_dataset("c", dims, compression="gzip", compression_opts=9)
 
     def write_data(self, bunch, h5group):
 
         resorting_indices = np.argsort(bunch.id)[::self.stride]
 
-        h5group["x"][:] = bunch.x[resorting_indices]
-        h5group["xp"][:] = bunch.yp[resorting_indices]
-        h5group["y"][:] = bunch.y[resorting_indices]
-        h5group["yp"][:] = bunch.yp[resorting_indices]
-        h5group["dz"][:] = bunch.dz[resorting_indices]
-        h5group["dp"][:] = bunch.dp[resorting_indices]
+        if self.n_steps:
+            self.h5file['x'][:,h5group] = bunch.x.take(resorting_indices)
+            self.h5file['xp'][:,h5group] = bunch.xp.take(resorting_indices)
+            self.h5file['y'][:,h5group] = bunch.y.take(resorting_indices)
+            self.h5file['yp'][:,h5group] = bunch.yp.take(resorting_indices)
+            self.h5file['dz'][:,h5group] = bunch.dz.take(resorting_indices)
+            self.h5file['dp'][:,h5group] = bunch.dp.take(resorting_indices)
 
-        # Do we need/want this here?
-        h5group["id"][:] = bunch.id[resorting_indices]
+            self.h5file['id'][:,h5group] = bunch.id.take(resorting_indices)
+            self.h5file['c'][:,h5group] = self.z0
+        else:
+            h5group["x"][:] = bunch.x[resorting_indices]
+            h5group["xp"][:] = bunch.yp[resorting_indices]
+            h5group["y"][:] = bunch.y[resorting_indices]
+            h5group["yp"][:] = bunch.yp[resorting_indices]
+            h5group["dz"][:] = bunch.dz[resorting_indices]
+            h5group["dp"][:] = bunch.dp[resorting_indices]
 
-        h5group["c"][:] = self.z0
+            # Do we need/want this here?
+            h5group["id"][:] = bunch.id[resorting_indices]
+            h5group["c"][:] = self.z0
