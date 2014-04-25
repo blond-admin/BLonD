402f5698c578b75b33a88e69a3e4597ff729bbf2
dquartul <dquartul@cern.ch>
2014-04-25 17:19:55 +0200

    deleted all longitudinal methods from the transverse impedance file

--- impedances/transverse_impedance.py
+++ impedances/transverse_impedance.py
@@ -69,21 +69,8 @@ class Wakefields(object):
         return compute_apply_kicks
 
 
-    #~ @profile
-    def longitudinal_wakefield_kicks(self, bunch):          
-        wake = self.wake_longitudinal 
-        
-        # matrix with distances to target slice
-        dz_to_target_slice = [bunch.slices.dz_centers[1:-2]] - np.transpose([bunch.slices.dz_centers[1:-2]])
-
-        # compute kicks
-        self.longitudinal_kick = np.zeros(bunch.slices.n_slices+4)
-        self.longitudinal_kick[1:-3] = np.dot(bunch.slices.n_macroparticles[1:-3], wake(bunch, dz_to_target_slice)) * self.wake_factor(bunch)
-
-        # apply kicks
-        bunch.dp += self.longitudinal_kick[bunch.in_slice]           
+     
              
- 
 def BB_Resonator_Circular(R_shunt, frequency, Q):
 	return BB_Resonator_transverse(R_shunt, frequency, Q, Yokoya_X1=1, Yokoya_Y1=1, Yokoya_X2=0, Yokoya_Y2=0)
 
@@ -209,7 +196,7 @@ class Resistive_wall_transverse(Wakefields):
         wakefield_kicks_y(bunch)       
 
     
-class Wake_table(Wakefields):
+class Wake_table_transverse(Wakefields):
     '''
     classdocs
     '''
@@ -283,17 +270,6 @@ class Wake_table(Wakefields):
         return 0
                        
 
-    def wake_longitudinal(self, bunch, z):
-        time = np.array(self.wake_table['time'])
-        wake = np.array(self.wake_table['longitudinal'])
-        wake_interpolated = np.interp(- z / c / bunch.beta, time, wake, left=0, right=0)
-        if time[0] < 0:
-            return wake_interpolated
-        elif time[0] == 0:
-            # beam loading theorem: half value of wake at z=0; 
-            return (np.sign(-z) + 1) / 2 * wake_interpolated
-    
-    
     def track(self, bunch):
         bunch.compute_statistics()    
         if ('dipolar_x' or 'quadrupolar_x') in self.wake_field_keys:
@@ -302,49 +278,6 @@ class Wake_table(Wakefields):
         if ('dipolar_y' or 'quadrupolar_y') in self.wake_field_keys:
             wakefield_kicks_y = self.transverse_wakefield_kicks('y')
             wakefield_kicks_y(bunch) 
-        if 'longitudinal' in self.wake_field_keys:
-            self.longitudinal_wakefield_kicks(bunch)
-
-
-class BB_Resonator_longitudinal(Wakefields):
-    '''
-    classdocs
-    '''
-    def __init__(self, R_shunt, frequency, Q):
-        '''
-        Constructor
-        '''
-        self.R_shunt = np.array([R_shunt]).flatten()
-        self.frequency = np.array([frequency]).flatten()
-        self.Q = np.array([Q]).flatten()
-        assert(len(self.R_shunt) == len(self.frequency) == len(self.Q))
-
-
-    def wake_longitudinal(self, bunch, z):
-        return reduce(lambda x,y: x+y, [self.wake_BB_resonator(self.R_shunt[i], self.frequency[i], self.Q[i], bunch, z) for i in np.arange(len(self.Q))])
-
-    
-    def wake_BB_resonator(self, R_shunt, frequency, Q, bunch, z):        
-        # Taken from Alex Chao's resonator model (2.82)
-        omega = 2 * np.pi * frequency
-        alpha = omega / (2 * Q)
-        omegabar = np.sqrt(np.abs(omega ** 2 - alpha ** 2))
-
-        if Q > 0.5:
-            wake =  - (np.sign(z) - 1) * R_shunt * alpha * np.exp(alpha * z.clip(max=0) / c / bunch.beta) * \
-                    (cos(omegabar * z.clip(max=0) / c / bunch.beta) + alpha / omegabar * sin(omegabar * z.clip(max=0) / c / bunch.beta))
-        elif Q == 0.5:
-            wake =  - (np.sign(z) - 1) * R_shunt * alpha * np.exp(alpha * z.clip(max=0) / c / bunch.beta) * \
-                    (1. + alpha * z.clip(max=0) / c / bunch.beta)
-        elif Q < 0.5:
-            wake =  - (np.sign(z) - 1) * R_shunt * alpha * np.exp(alpha * z.clip(max=0) / c / bunch.beta) * \
-                    (np.cosh(omegabar * z.clip(max=0) / c / bunch.beta) + alpha / omegabar * np.sinh(omegabar * z.clip(max=0) / c / bunch.beta))
-        return wake
         
-        
-    def track(self, bunch):
-        bunch.compute_statistics()  
-        self.longitudinal_wakefield_kicks(bunch)
-        
-  
- 
+
+
