2824aa94cc863e5c559dacc7367f46d603162503
Iliakis Konstantinos <iliakiskostis@gmail.com>
2016-08-09 17:21:58 +0200

    Update convolution.py

--- toolbox/convolution.py
+++ toolbox/convolution.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def convolution(signal, kernel):
     size = len(signal) + len(kernel) - 1
-    result = np.ascontiguousarray(np.empty(size))
+    result = np.empty(size)
     libfib.convolution(signal.ctypes.data_as(ctypes.c_void_p),
                        ctypes.c_int(len(signal)),
                        kernel.ctypes.data_as(ctypes.c_void_p),
