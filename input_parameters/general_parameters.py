bd6cbae10ce4f6589795ca861b4230208ccc1a16
htimko <helga.timko@cern.ch>
2017-05-03 19:22:39 +0200

    Cleaning up general_parameters: adding explicite type definition, new-style documentation, regrouping declarations. Separated particle 2 definition into separate function. To be continued.

--- input_parameters/general_parameters.py
+++ input_parameters/general_parameters.py
@@ -22,114 +22,142 @@ from scipy.constants import m_p, m_e, e, c
 
 
 class GeneralParameters(object):
-    '''
-    *Object containing all the general input parameters used for the simulation.*
-    '''
-
-    def __init__(self, n_turns, ring_length, alpha, momentum, 
-                 particle_type, user_mass = None, user_charge = None, 
-                 particle_type_2 = None, user_mass_2 = None, 
-                 user_charge_2 = None, number_of_sections = 1, force_beta_equal_one = False):
+#     Examples
+#     --------
+#     >>> from input_parameters.general_parameters import GeneralParameters
+#     >>> from beams.beams import Beam
+#     >>>
+#     >>> n_turns = 10
+#     >>> C = 100
+#     >>> eta = 0.03
+#     >>> momentum = 26e9
+#     >>> general_parameters = GeneralParameters(n_turns, C, eta, momentum, 'proton')
+#     >>> n_macroparticle = 1e6
+#     >>> intensity = 1e11
+#     >>>
+#     >>> my_beam = Beam(GeneralParameters, n_macroparticle, intensity)
+#     
+#     """
+    """ Class containing the general properties of the synchrotron that are 
+    independent of the RF system or the beam.
+    
+    Parameters
+    ----------
+    n_turns : int
+        number of turns to be simulated [1].
+    ring_length : float
+        length of the n_sections ring segments of the synchrotron [m].
+        Input as a list for multiple RF stations.
+    alpha : float (opt: float array)
+        momentum compaction factor [1]; can be input as single float (only 0th 
+        order element) or float array (up to 2nd order elements).
+        :math:`: \quad \alpha_{k,i}`
+        Input as a list of lists for multiple RF stations and higher-order 
+        elements.
+    momentum : float (opt: float array/matrix)
+        design synchronous particle momementum on the design orbit [eV]. Input
+        for each RF section :math:`: \quad p_{s,k}^n`. Can be input as a single
+        constant float, or as a program of (n_turns + 1) turns. In case of 
+        several sections without acceleration, input: [[momentum_section_1], 
+        [momentum_section_2], etc.]. In case of several sections with 
+        acceleration, input: [momentum_program_section_1, 
+        momentum_program_section_2, etc.].
+    particle_type : string
+        primary particle type that is reference for the momentum; Recognized 
+        types are 'proton' and 'electron'. Use 'user_input' to input mass and 
+        charge manually.
+    n_sections : int
+        optional: number of RF sections over the ring [1]; default is 1.
         
-        #: | *Number of RF sections over the ring; default is one.*
-        self.n_sections = number_of_sections
+    Attributes
+    ----------
+    ring_circumference : float
+        circumference of the synchrotron [m]. Sum of ring segment lengths, 
+        :math:`: \quad C = \sum_k L_k`.
+    ring_radius : float
+        radius of the synchrotron [m], :math:`: \quad R = C/(2 \pi)`
+    alpha_order : int
+        number of orders of the momentum compaction factor
+    cumulative_times : float array
+        cycle times at which cycle parameters can be extracted [s].
+    mass : float
+        primary particle mass [eV].
+    charge : float
+        primary particle charge [e].
+    """
+    
+    def __init__(self, n_turns, ring_length, alpha, momentum, particle_type, 
+                 user_mass = None, user_charge = None, n_sections = 1): 
         
-        #: | *Particle type. Recognized types are: 'proton' and 'electron'.*
-        #: | *Use 'user_input' to input mass and charge manually.*
-        #: | *Input particle mass in [eV]* :math:`: \quad m`
-        #: | *Input particle charge in [e]* :math:`: \quad q`
-        self.particle_type = particle_type
+        self.n_turns = int(n_turns) 
+        self.n_sections = int(n_sections)
         
-        #: *Second particle type: optional; does not affect the momentum, 
-        #: energy, beta, and gamma.*
-        self.particle_type_2 = particle_type_2
+#        self.ring_length = ring_length
+#        if isinstance(self.ring_length, float) or isinstance(self.ring_length, int):
+#            self.ring_length = [self.ring_length]
+        self.ring_length = np.array(ring_length, dtype = float)
+        self.ring_circumference = np.sum(self.ring_length)
+        self.ring_radius = self.ring_circumference/(2*np.pi)         
+        if self.n_sections != len(self.ring_length): 
+            raise RuntimeError('ERROR: Number of sections and ring length'+
+                               ' size do not match!')    
         
-        # Attribution of mass and charge with respect to particle_type
-        if self.particle_type is 'proton':
-            self.mass =  m_p*c**2/e # [eV]
-            self.charge = 1. # [e]
-        elif self.particle_type is 'electron':
-            self.mass =  m_e*c**2/e # [eV]
-            self.charge = -1. # [e]
-        elif self.particle_type is 'user_input':
-            self.mass = user_mass # [eV]
-            self.charge = user_charge # [e]
-        else:
-            raise RuntimeError('ERROR: Particle type not recognized!')
-        
-        if self.particle_type_2 == None:
-            pass
-        elif self.particle_type_2 is 'proton':
-            self.mass2 =  m_p*c**2/e # [eV]
-            self.charge2 = 1. # [e]
-        elif self.particle_type_2 is 'electron':
-            self.mass2 =  m_e*c**2/e # [eV]
-            self.charge2 = -1. # [e]
-        elif self.particle_type_2 is 'user_input':
-            self.mass2 = user_mass_2 # [eV]
-            self.charge2 = user_charge_2 # [e]
-        else:
-            raise RuntimeError('ERROR: Second particle type not recognized!')
-        
-        #: *Number of turns of the simulation*
-        self.n_turns = n_turns 
-        
+        self.alpha = np.array(alpha, ndmin = 2, dtype = float) 
+        self.alpha_order = int(self.alpha.shape[1])
+        if self.alpha_order > 3:
+            warnings.filterwarnings("once")
+            warnings.warn('WARNING: Momentum compaction factor is implemented'+
+                          ' only up to 2nd order. Higher orders are ignored.')
+            self.alpha_order = 3         
+        if self.n_sections != self.alpha.shape[0]:
+            raise RuntimeError('ERROR: Number of sections and the momentum'+
+                               ' compaction factor size do not match!')    
+                
         if type(momentum)==tuple:
             self.momentum = np.array(momentum[1], ndmin = 2)
             self.cumulative_times = momentum[0]
         else:
-            #: | *Synchronous momentum (program) in [eV] for each RF section* :math:`: \quad p_{s,k}^n`
-            #: | *Can be given as a single constant value, or as a program of (n_turns + 1) turns.*
-            #: | *In case of several sections without acceleration, input: [[momentum_section_1], [momentum_section_2]]*
-            #: | *In case of several sections with acceleration, input: [momentum_program_section_1, momentum_program_section_2]*
             self.momentum = np.array(momentum, ndmin = 2)
-
-        #: | *Momentum compaction factor (up to 2nd order) for each RF section* :math:`: \quad \alpha_{k,i}`
-        #: | *Should be given as a list for multiple RF stations (each element of the list should be a list of alpha factors up to 2nd order)*
-        self.alpha = np.array(alpha, ndmin = 2) 
-        
-        #: *Number of orders for the momentum compaction*
-        self.alpha_order = self.alpha.shape[1]
-
-        #: | *Ring length contains the length of the RF sections, in [m]*
-        #: | *Should be given as a list for multiple RF stations*
-        self.ring_length = ring_length
-        if isinstance(self.ring_length, float) or isinstance(self.ring_length, int):
-            self.ring_length = [self.ring_length]
-        
-        #: | *Ring circumference is the sum of section lengths* :math:`: \quad C = \sum_k L_k`
-        self.ring_circumference = np.sum(self.ring_length)
-        
-        #: *Ring radius in [m]* :math:`: \quad R`
-        self.ring_radius = self.ring_circumference/(2*np.pi)         
-        
-        # Check consistency of input data; raise error if not consistent
-        
-        if self.n_sections != len(self.ring_length) or \
-           self.n_sections != self.alpha.shape[0] or \
-           self.n_sections != self.momentum.shape[0]:
-            raise RuntimeError('ERROR: Number of sections, ring length, alpha,'+
-                               ' and/or momentum data do not match!')    
-        
+        if self.n_sections != self.momentum.shape[0]:
+            raise RuntimeError('ERROR: Number of sections and momentum data'+
+                               ' do not match!')           
         if self.n_sections > 1:
             if self.momentum.shape[1] == 1:
                 self.momentum = self.momentum*np.ones(self.n_turns + 1)
         else:
             if self.momentum.size == 1:
                 self.momentum = self.momentum*np.ones(self.n_turns + 1)
-
         if not self.momentum.shape[1] == self.n_turns + 1:
-                raise RuntimeError('The input momentum program does not match'+ 
-                ' the proper length (n_turns+1)')
+                raise RuntimeError('ERROR: The momentum program does not'+ 
+                                   'match the proper length (n_turns+1)')
             
         
+        self.particle_type = str(particle_type)        
+        # Derived: mass and charge from particle_type
+        if self.particle_type is 'proton':
+            self.mass =  float(m_p*c**2/e) 
+            self.charge = float(1) 
+        elif self.particle_type is 'electron':
+            self.mass =  float(m_e*c**2/e)
+            self.charge = float(-1)
+        elif self.particle_type is 'user_input':
+            if user_mass > 0. and user_charge > 0.:
+                self.mass = float(user_mass)
+                self.charge = float(user_charge)
+            else:
+                raise RuntimeError('ERROR: Particle mass and/or charge not'+
+                                   ' recognized!')
+        else:
+            raise RuntimeError('ERROR: Particle type not recognized!')
+        
+# STOPPED HERE
         #: *Synchronous relativistic beta (program)* :math:`: \quad \beta_{s,k}^n`
         #:
         #: .. math:: \beta_s = \frac{1}{\sqrt{1 + \left(\frac{m}{p_s}\right)^2} }
         
         self.beta = np.sqrt(1/(1 + (self.mass/self.momentum)**2))
-        if force_beta_equal_one:
-            self.beta = np.array([np.ones(self.n_turns + 1)])
+#        if force_beta_equal_one:
+#            self.beta = np.array([np.ones(self.n_turns + 1)])
         
         #: *Synchronous relativistic gamma (program)* :math:`: \quad \gamma_{s,k}^n`
         #:
@@ -177,17 +205,48 @@ class GeneralParameters(object):
         #: .. math:: \eta_2 = -\frac{\beta_s^2\left(5\beta_s^2-1\right)}{2\gamma_s^2} + \alpha_2 - 2\alpha_0\alpha_1 + \frac{\alpha_1}{\gamma_s^2} + \alpha_0^2\eta_0 - \frac{3\beta_s^2\alpha_0}{2\gamma_s^2}
         self.eta_2 = 0
         
-        # Warning that higher orders for alpha will not be used
-        if self.alpha_order > 3:
-            warnings.filterwarnings("once")
-            warnings.warn("WARNING: Momentum compaction factor is implemented \
-                          only up to 2nd order")
-            self.alpha_order = 3         
                  
         # Processing the slippage factor
         self.eta_generation()
                 
                 
+    def add_species(self, particle_type_2, user_mass_2 = None, 
+                    user_charge_2 = None):
+        """ Function to declare an optional second particle type
+    
+        Parameters
+        ----------
+        particle_type_2 : string
+            secondary particle type that is not a reference for the momentum; 
+            Recognized types are 'proton' and 'electron'. 
+            Use 'user_input' to input mass and charge manually.    
+
+        Attributes
+        ----------
+        mass2 : float
+            primary particle mass [eV].
+        charge2 : float
+            primary particle charge [e].
+        """
+        
+        self.particle_type_2 = str(particle_type_2)
+        if self.particle_type_2 is 'proton':
+            self.mass2 =  float(m_p*c**2/e)
+            self.charge2 = float(1)
+        elif self.particle_type_2 is 'electron':
+            self.mass2 =  float(m_e*c**2/e)
+            self.charge2 = float(-1)
+        elif self.particle_type_2 is 'user_input':
+            if user_mass_2 > 0. and user_charge_2 > 0.:
+                self.mass2 = float(user_mass_2)
+                self.charge2 = float(user_charge_2)
+            else:
+                raise RuntimeError('ERROR: Particle mass and/or charge not \
+                                    recognized!')
+        else:
+            raise RuntimeError('ERROR: Second particle type not recognized!')
+
+    
     def eta_generation(self):
         '''
         | *Pre-processing of the slippage factor parameters with respect to the input momentum compaction factor (up to 2nd order) and the momentum program.*
