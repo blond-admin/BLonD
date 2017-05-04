6bfff1e2376f73c290302354ddbd1163dd31edd2
htimko <helga.timko@cern.ch>
2017-05-04 19:06:32 +0200

    General_parameters: finished internal restucturing. To be done: compile documentation, add pre-process and optional energy input.

--- input_parameters/general_parameters.py
+++ input_parameters/general_parameters.py
@@ -22,22 +22,6 @@ from scipy.constants import m_p, m_e, e, c
 
 
 class GeneralParameters(object):
-#     Examples
-#     --------
-#     >>> from input_parameters.general_parameters import GeneralParameters
-#     >>> from beams.beams import Beam
-#     >>>
-#     >>> n_turns = 10
-#     >>> C = 100
-#     >>> eta = 0.03
-#     >>> momentum = 26e9
-#     >>> general_parameters = GeneralParameters(n_turns, C, eta, momentum, 'proton')
-#     >>> n_macroparticle = 1e6
-#     >>> intensity = 1e11
-#     >>>
-#     >>> my_beam = Beam(GeneralParameters, n_macroparticle, intensity)
-#     
-#     """
     """ Class containing the general properties of the synchrotron that are 
     independent of the RF system or the beam. The index 'n' denotes time steps,
     'k' ring segments and 'i' momentum compaction orders.
@@ -45,71 +29,102 @@ class GeneralParameters(object):
     Parameters
     ----------
     n_turns : int
-        number of turns [1] to be simulated.
+        Number of turns [1] to be simulated.
     ring_length : float
-        length [m] of the n_sections ring segments of the synchrotron.
+        Length [m] of the n_sections ring segments of the synchrotron.
         Input as a list for multiple RF stations.
     alpha : float (opt: float array)
-        momentum compaction factor [1]; can be input as single float (only 0th 
+        Momentum compaction factor [1]; can be input as single float (only 0th 
         order element) or float array (up to 2nd order elements). In case of 
         several sections without higher orders, input: [[alpha_section_1], 
         [alpha_section_2], etc.]. In case of several sections and higher order
         alphas, input: [alpha_array_section_1, alpha_array_section_2, etc.], 
         :math:`: \quad \alpha_{k,i}`.
     momentum : float (opt: float array/matrix)
-        design synchronous particle momementum [eV] on the design orbit. Input
+        Design synchronous particle momementum [eV] on the design orbit. Input
         for each RF section :math:`: \quad p_{s,k}^n`. Can be input as a single
         constant float, or as a program of (n_turns + 1) turns. In case of 
         several sections without acceleration, input: [[momentum_section_1], 
         [momentum_section_2], etc.]. In case of several sections with 
         acceleration, input: [momentum_program_section_1, 
-        momentum_program_section_2, etc.].
+        momentum_program_section_2, etc.]. Can be input also as a tuple of time
+        and momentum, see also 'cumulative_times'.
     particle_type : string
-        primary particle type that is reference for the momentum; Recognized 
+        Primary particle type that is reference for the momentum; Recognized 
         types are 'proton' and 'electron'. Use 'user_input' to input mass and 
         charge manually.
     n_sections : int
-        optional: number of RF sections [1] over the ring; default is 1.
+        Optional: number of RF sections [1] over the ring; default is 1.
         
     Attributes
     ----------
     ring_circumference : float
-        circumference [m] of the synchrotron. Sum of ring segment lengths, 
+        Circumference [m] of the synchrotron. Sum of ring segment lengths, 
         :math:`: \quad C = \sum_k L_k`.
     ring_radius : float
-        radius [m] of the synchrotron, :math:`: \quad R = C/(2 \pi)`
+        Radius [m] of the synchrotron, :math:`: \quad R = C/(2 \pi)`
     alpha_order : int
-        number of orders of the momentum compaction factor
+        Number of orders of the momentum compaction factor
     eta_0 : float
-        zeroth order slippage factor [1] :math:`: \quad \eta_{k,0}`
+        Zeroth order slippage factor [1] :math:`: \quad \eta_{k,0}`
         .. math:: \eta_0 = \alpha_0 - \frac{1}{\gamma_s^2}
     eta_1 : float
-        first order slippage factor [1] :math:`: \quad \eta_{k,1}`
+        First order slippage factor [1] :math:`: \quad \eta_{k,1}`
         .. math:: \eta_1 = \frac{3\beta_s^2}{2\gamma_s^2} + \alpha_1 - \alpha_0\eta_0
     eta_2 : float
-        second order slippage factor [1] :math:`: \quad \eta_{k,2}`
+        Second order slippage factor [1] :math:`: \quad \eta_{k,2}`
         .. math:: \eta_2 = -\frac{\beta_s^2\left(5\beta_s^2-1\right)}{2\gamma_s^2} + \alpha_2 - 2\alpha_0\alpha_1 + \frac{\alpha_1}{\gamma_s^2} + \alpha_0^2\eta_0 - \frac{3\beta_s^2\alpha_0}{2\gamma_s^2}
     cumulative_times : float array
-        cycle times at which cycle parameters can be extracted [s].
+        Cycle time moments [s] taken from preprocess ramp method. Possibility
+        to extract cycle parameters at these moments using 'parameters_at_time'
     mass : float
-        primary particle mass [eV].
+        Primary particle mass [eV].
     charge : float
-        primary particle charge [e].
+        Primary particle charge [e].
     beta : float matrix
-        synchronous relativistic beta program [1] for each segment of the ring
+        Synchronous relativistic beta program [1] for each segment of the ring
         :math:`: \quad \beta_{s,k}^n`
         .. math:: \beta_s = \frac{1}{\sqrt{1 + \left(\frac{m}{p_s}\right)^2} }
     gamma : float matrix
-        synchronous relativistic gamma program [1] for each segment of the ring
+        Synchronous relativistic gamma program [1] for each segment of the ring
         :math:`: \quad \gamma_{s,k}^n`
         .. math:: \gamma_s = \sqrt{ 1 + \left(\frac{p_s}{m}\right)^2 }
     energy : float matrix
-        synchronous total energy program [eV] for each segment of the ring
+        Synchronous total energy program [eV] for each segment of the ring
         :math:`: \quad E_{s,k}^n`
         .. math:: E_s = \sqrt{ p_s^2 + m^2 }
     kin_energy : float matrix
-        synchronous kinetic energy program [eV] for each segment of the ring
+        Synchronous kinetic energy program [eV] for each segment of the ring
         .. math:: E_s^kin = \sqrt{ p_s^2 + m^2 } - m
+    t_rev : float array
+        Revolution period [s] turn by turn.
+        :math:`: \quad T_0 = \frac{C}{\beta_s c}`
+    f_rev : float array
+        Revolution frequency [Hz] :math:`: \quad f_0 = \frac{1}{T_0}`
+    omega_rev : float array
+        Revolution angular frequency [1/s] :math:`: \quad \omega_0 = 2\pi f_0`
+
+    Examples
+    --------
+    To declare a single-stationed synchrotron at constant energy:
+    >>> from input_parameters.general_parameters import GeneralParameters
+    >>>
+    >>> n_turns = 10
+    >>> C = 26659
+    >>> alpha = 3.21e-4
+    >>> momentum = 450e9
+    >>> general_parameters = GeneralParameters(n_turns, C, eta, momentum, 'proton')
+
+    To declare a double-stationed synchrotron at constant energy and higher-
+    order momentum compaction factors:
+    >>> from input_parameters.general_parameters import GeneralParameters
+    >>>
+    >>> n_turns = 10
+    >>> C = [13000, 13659]
+    >>> alpha = [[3.21e-4, 2.e-5, 5.e-7], [2.89e-4, 1.e-5, 5.e-7]]
+    >>> momentum = 450e9
+    >>> general_parameters = GeneralParameters(n_turns, C, eta, momentum, 'proton')
+
     """
     
     def __init__(self, n_turns, ring_length, alpha, momentum, particle_type, 
@@ -126,7 +141,7 @@ class GeneralParameters(object):
             raise RuntimeError('ERROR: Number of sections and ring length'+
                                ' size do not match!')    
         
-        # Momentum compation, checks, and derived slippage factors
+        # Momentum compaction, checks, and derived slippage factors
         self.alpha = np.array(alpha, ndmin = 2, dtype = float) 
         self.alpha_order = int(self.alpha.shape[1])
         if self.alpha_order > 3:
@@ -181,21 +196,16 @@ class GeneralParameters(object):
         self.energy = np.sqrt(self.momentum**2 + self.mass**2)
         self.kin_energy = np.sqrt(self.momentum**2 + self.mass**2) - self.mass
         
-        # Be careful that self.cycle_time in the else statement starts always with 0.
+        # N.B. self.cycle_time in the else statement should start always with 0
         if type(momentum)==tuple:
-            #: *Cumulative times [s] taken from preprocess ramp method*
             self.cycle_time = self.cumulative_times
-            #: *Revolution period [s]* :math:`: \quad T_0 = \frac{C}{\beta_s c}`
-            self.t_rev = np.append(np.diff(self.cycle_time),self.ring_circumference/(self.beta[0][-1]*c))
+            self.t_rev = np.append(np.diff(self.cycle_time),
+                                   self.ring_circumference/
+                                   (self.beta[0][-1]*c))
         else:    
             self.t_rev = np.dot(self.ring_length, 1/(self.beta*c))
             self.cycle_time = np.cumsum(self.t_rev)
-            
-        #: *Revolution frequency [Hz]* :math:`: \quad f_0 = \frac{1}{T_0}`
         self.f_rev = 1/self.t_rev
-         
-        
-        #: *Revolution angular frequency [1/s]* :math:`: \quad \omega_0 = 2\pi f_0`
         self.omega_rev = 2*np.pi*self.f_rev
 
         # Slippage factor derived from alpha, beta, gamma
