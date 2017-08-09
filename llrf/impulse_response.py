4adcd37a74acf3e672863d99fda07069a847ce06
htimko <helga.timko@cern.ch>
2017-08-09 19:06:25 +0200

    OTFB cavity impulse response...

--- llrf/impulse_response.py
+++ llrf/impulse_response.py
@@ -20,8 +20,50 @@ import logging
 
 
 
+def rect(t, tau):
+    r"""Rectangular function of time
+    
+    .. math:: \mathsf{rect} \left( \frac{t}{\tau} \right) = 
+        \begin{cases}
+            1 \, , \, t \in (-\tau/2, \tau/2) \\
+            0.5 \, , \, t = \pm \tau/2 \\
+            0 \, , \, \textsf{otherwise}
+        \end{cases}
+    """
+    
+
+
 class TravellingWaveCavity(object):
-    """Impulse responses of a travelling wave cavity.
+    r"""Impulse responses of a travelling wave cavity. The induced voltage 
+    :math:`V(t)` from the impulse response :math:`h(t)` and the I,Q (cavity or
+    generator) current :math:`I(t)` can be written in matrix form,
+    
+    .. math:: 
+        \left( \begin{matrix} V_I(t) \\ 
+        V_Q(t) \end{matrix} \right)
+        = \left( \begin{matrix} h_s(t) & - h_c(t) \\
+        h_c(t) & h_s(t) \end{matrix} \right)
+        * \left( \begin{matrix} I_I(t) \\ 
+        I_Q(t) \end{matrix} \right) \, ,
+        
+    where :math:`*` denotes convolution, 
+    :math:`h(t)*x(t) = \int d\tau h(\tau)x(t-\tau)`. For the **cavity-to-beam 
+    induced voltage**,
+    
+    .. math::
+        h_s(t) &= \frac{\rho l^2}{8 \tau}\left( 1 - \frac{t}{\tau} \right) \cos((\omega_c - \omega_r)t) \, , \\
+        h_c(t) &= \frac{\rho l^2}{8 \tau}\left( 1 - \frac{t}{\tau} \right) \sin((\omega_c - \omega_r)t) \, ,
+        
+    where :math:`\rho` is the series impedance, :math:`l` the accelerating
+    length, :math:`\tau` the filling time, and :math:`\omega_r` the central 
+    frequency of the cavity; :math:`\omega_c` is the carrier frequency of the 
+    I,Q demodulated current signal. On the carrier frequency, 
+    :math:`\omega_c = \omega_r`,
+    
+    .. math::
+        h_s(t) &= \frac{\rho l^2}{8 \tau}\left( 1 - \frac{t}{\tau} \right) \\
+        h_c(t) &= 0 \, .
+     
     
     Parameters
     ----------
@@ -33,7 +75,7 @@ class TravellingWaveCavity(object):
         Series impedance [Ohms/m^2] of the cavity
     v_g : float
         Group velocity [c] in units of the speed of light
-    omega_0 : flaot
+    omega_r : flaot
         Central (resonance) revolution frequency [1/s] of the cavity
         
     Attributes
@@ -45,7 +87,7 @@ class TravellingWaveCavity(object):
         
     """
         
-    def __init__(self, l_cell, N_cells, rho, v_g, omega_0):
+    def __init__(self, l_cell, N_cells, rho, v_g, omega_r):
         
         self.l_cell = float(l_cell)
         self.N_cells = int(N_cells)
@@ -55,7 +97,7 @@ class TravellingWaveCavity(object):
         else:
             raise RuntimeError("ERROR in TravellingWaveCavity: group" +
                 " velocity out of limits (0,1)!")
-        self.omega_0 = float(omega_0)
+        self.omega_r = float(omega_r)
         
         self.l_cav = float(self.l_cell*self.N_cells)
         self.tau = self.l_cav*c/self.v_g*(1 + self.v_g) # v_g opposite to wave!
@@ -66,11 +108,14 @@ class TravellingWaveCavity(object):
         self.logger.debug("Filling time %.4e s", self.tau)
         
         
-    def cavity_to_beam(self, omega_c, time_array):
-        """Impulse response from the cavity towards the beam. For a signal that
-        is I,Q demodulated at a given carrier frequency. The approximations
-        used in the formulae require the carrier frequency to be close to the
-        central frequency and assume that the signal is low-pass filtered.
+#    def cavity_to_beam(self, omega_c, time_array):
+    def impulse_response(self, omega_c, time_array):
+        """Impulse response from the cavity towards the beam and towards the 
+        generator. For a signal that is I,Q demodulated at a given carrier 
+        frequency :math:`\omega_c`. The formulae assume that the carrier 
+        frequency is be close to the central frequency 
+        :math:`\omega_c/\omega_r \ll 1` and that the signal is low-pass
+        filtered (i.e.\ high-frequency components can be neglected).
         
         Parameters
         ----------
@@ -81,29 +126,27 @@ class TravellingWaveCavity(object):
         """
         
         self.omega_c = float(omega_c)
-        self.d_omega = self.omega_c - self.omega_0
+        self.d_omega = self.omega_c - self.omega_r
+        if np.fabs((self.d_omega)/self.omega_r) > 0.1:
+            raise RuntimeError("ERROR in TravellingWaveCavity" +
+                " impulse_response(): carrier frequency should be close to" +
+                " central frequency of the cavity!")
     
-        # Time in range (0, tau)
+        # Time in range (0, tau), otherwise zero
         self.time_array = np.zeros(len(time_array))
         indices = np.where((self.tau - time_array)*(time_array) >= 0)[0]
         self.time_array[indices] = time_array[indices]
         
-        if np.fabs((self.d_omega)/self.omega_0) > 0.1:
-            raise RuntimeError("ERROR in TravellingWaveCavity" +
-                " cavity_to_beam(): carrier frequency should be close to" +
-                " central frequency of the cavity!")
-        else:
-            
-            # If on carrier frequency
-            self.h_s = self.rho*self.l_cav**2/(8*self.tau) \
-                *(1 - self.time_array/self.tau)
-            self.h_c = None
-            
-            # If not on carrier frequency
-            if np.fabs((self.d_omega)/self.omega_0) > 1e-12:
-                self.h_c = np.copy(self.h_s)*np.sin(self.d_omega*
-                                                    self.time_array)
-                self.h_s *= np.cos(self.d_omega*self.time_array)
+        # If on carrier frequency
+        self.h_s = self.rho*self.l_cav**2/(8*self.tau) \
+            *(1 - self.time_array/self.tau)
+        self.h_c = None
+        
+        # If not on carrier frequency
+        if np.fabs((self.d_omega)/self.omega_r) > 1e-12:
+            self.h_c = np.copy(self.h_s)*np.sin(self.d_omega*
+                                                self.time_array)
+            self.h_s *= np.cos(self.d_omega*self.time_array)
                 
 
 class SPS4Section200MHzTWC(TravellingWaveCavity):
--- llrf/signal_processing.py
+++ llrf/signal_processing.py
@@ -1,8 +1,8 @@
 # coding: utf8
 # Copyright 2014-2017 CERN. This software is distributed under the
-# terms of the GNU General Public Licence version 3 (GPL Version 3), 
+# terms of the GNU General Public Licence version 3 (GPL Version 3),
 # copied verbatim in the file LICENCE.md.
-# In applying this licence, CERN does not waive the privileges and immunities 
+# In applying this licence, CERN does not waive the privileges and immunities
 # granted to it by virtue of its status as an Intergovernmental Organization or
 # submit itself to any jurisdiction.
 # Project website: http://blond.web.cern.ch/
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 
 def polar_to_cartesian(amplitude, phase):
     """Convert data from polar to cartesian (I,Q) coordinates.
-    
+
     Parameters
     ----------
     amplitude : float array
@@ -83,7 +83,7 @@ def real_to_cartesian(signal):
     """
     
     amplitude = np.max(signal)
-    phase = np.arccos(signal/amplitude)
+    phase = np.arccos(signal / amplitude)
     logger.debug("Creating complex IQ array from real array")
     
     return signal + 1j*amplitude*np.sin(phase)
@@ -112,8 +112,8 @@ def modulator(signal, f_initial, f_final, T_sampling):
     """
     
     if len(signal) < 2:
-        raise RuntimeError("ERROR in filters.py/demodulator: signal should be"+
-                           " an array!")
+        raise RuntimeError("ERROR in filters.py/demodulator: signal should" +
+                           " be an array!")
     delta = 2*np.pi*(f_initial - f_final)*T_sampling
     indices = np.arange(len(signal))
     try:
@@ -122,8 +122,8 @@ def modulator(signal, f_initial, f_final, T_sampling):
         Q_new = np.sin(delta*indices)*signal.real \
             + np.cos(delta*indices)*signal.imag  
     except:
-        raise RuntimeError("ERROR in filters.py/demodulator: signal should be"+
-                           " complex!")
+        raise RuntimeError("ERROR in filters.py/demodulator: signal should" +
+                           " be complex!")
         
     return I_new + 1j*Q_new
 
@@ -134,7 +134,8 @@ def rf_beam_current(Slices, frequency, T_rev):
     beam profile :math:`\lambda_i`, the particle charge :math:`q_p` and the 
     real vs. macro-particle ratio :math:`N_{\mathsf{real}}/N_{\mathsf{macro}}`
     
-    .. math:: Q_{\mathsf{tot}} = \frac{N_{\mathsf{real}}}{N_{\mathsf{macro}}} q_p \sum_i{\lambda_i}
+    .. math:: 
+        Q_{\mathsf{tot}} = \frac{N_{\mathsf{real}}}{N_{\mathsf{macro}}} q_p \sum_i{\lambda_i}
     
     The DC beam current [A] is the total number of charges per turn :math:`T_0`
     
@@ -160,12 +161,12 @@ def rf_beam_current(Slices, frequency, T_rev):
     IQ = real_to_cartesian(Slices.n_macroparticles)
     
     # Convert from dimensionless to Ampères
-    #T_range = Slices.bin_centers[-1] - Slices.bin_centers[0]
+    # T_range = Slices.bin_centers[-1] - Slices.bin_centers[0]
     # Macro-particle charge with real-to-macro-particle ratio
     q_m = Slices.Beam.intensity/Slices.Beam.n_macroparticles \
         *Slices.Beam.charge*e
     IQ *= q_m/T_rev#T_range
-    #Q_tot = Beam.intensity*Beam.charge*e/Beam.n_macroparticles*np.sum(Slices.n_macroparticles)
+    # Q_tot = Beam.intensity*Beam.charge*e/Beam.n_macroparticles*np.sum(Slices.n_macroparticles)
     logger.debug("Sum of slices: %d, total charge: %.4e C", 
                  np.sum(Slices.n_macroparticles), np.sum(IQ.real)*T_rev)
     logger.debug("DC current is %.4e A", np.sum(IQ.real))
@@ -192,7 +193,7 @@ def comb_filter(x, y, a):
     return a*y + (1 - a)*x
 
 
-def low_pass_filter(signal, cutoff_frequency = 0.5):
+def low_pass_filter(signal, cutoff_frequency=0.5):
     """Low-pass filter based on Butterworth 5th order digital filter from 
     scipy,
     http://docs.scipy.org
@@ -212,7 +213,7 @@ def low_pass_filter(signal, cutoff_frequency = 0.5):
         
     """
     
-    b, a = sgn.butter(5, 0.5, 'low', analog = False)
+    b, a = sgn.butter(5, 0.5, 'low', analog=False)
     
     return sgn.filtfilt(b, a, signal)
     
@@ -226,7 +227,8 @@ def cavity_impedance():
     """Model of the SPS cavity impedance.
     """
 
-def moving_average(x, N, center = False):
+
+def moving_average(x, N, center=False):
     """Function to calculate the moving average (or running mean) of the input
     data.
     
@@ -249,7 +251,7 @@ def moving_average(x, N, center = False):
         
     """
     
-    if center == True:
+    if center is True:
         # Round up to next impair number
         N_half = int(N/2)
         N = N_half*2 + 1
@@ -257,8 +259,9 @@ def moving_average(x, N, center = False):
         x = np.concatenate((x[0]*np.ones(N_half), x, x[-1]*np.ones(N_half)))
         
     cumulative_sum = np.cumsum(np.insert(x, 0, 0))
-    #print(cumulative_sum) 
+    # print(cumulative_sum) 
    
     return (cumulative_sum[N:] - cumulative_sum[:-N]) / N
+
 
 
