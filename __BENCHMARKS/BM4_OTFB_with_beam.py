3cbcad86bc84c5b822f71539652d8bbb796aff53
htimko <helga.timko@cern.ch>
2017-09-13 19:25:34 +0200

    Adding plotting to BM4

--- __BENCHMARKS/BM4_OTFB_with_beam.py
+++ __BENCHMARKS/BM4_OTFB_with_beam.py
@@ -16,7 +16,8 @@ Example for llrf.filters and llrf.cavity_feedback
 import h5py
 import numpy as np
 from scipy.constants import e
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
 import logging
 
 from toolbox.logger import Logger
@@ -44,7 +45,7 @@ phi = [0.]                    # 200 MHz RF phase
 # Beam and tracking parameters
 N_m = 1e5                   # Number of macro-particles for tracking
 N_b = 1.e11                 # Bunch intensity [ppb]
-N_t = 30                    # Number of turns to track
+N_t = 5                    # Number of turns to track
 n_bunches = 144             # Number of bunches
 bunch_spacing = 5           # In RF buckets
 # CERN SPS --------------------------------------------------------------------
@@ -66,7 +67,7 @@ print("Machine parameters set!")
 
 # Set up RF parameters
 rf = RFStation(ring, 1, h, V, phi)
-rf.omega_rf[0,0] = 2*np.pi*200.222e6 # cavity central frequency
+#rf.omega_rf[0,0] = 2*np.pi*200.222e6 # cavity central frequency
 logging.debug("RF frequency %.6e Hz", rf.omega_rf[0,0]/(2*np.pi))
 logging.debug("Revolution period %.6e s", rf.t_rev[0])
 print("RF parameters set!")
@@ -75,55 +76,6 @@ print("RF parameters set!")
 bunch = Beam(ring, N_m, N_b)
 bigaussian(ring, rf, bunch, 3.2e-9/4, seed = 1234, reinsertion = True) 
 
-
-# # Load PS beam to clone
-# #PS_folder = 'C:/Users/schwarz/git/BLonD_PS_SPS/models/PS_SPS_model/test/'
-# PS_case = '2x40_2x80'
-# #with h5py.File(PS_folder + PS_case + '.hd5', 'r') as data_file:
-# with h5py.File(PS_case + '.hd5', 'r') as data_file:
-#     n_macroparticles_PS = data_file['n_macroparticles'].value
-#     
-#     #get time and energy coordinates of particles
-#     PS_dt = data_file['dt'][:]
-#     PS_dE = data_file['dE'][:]
-#     
-#     data_file.close()
-#     
-#     PS_dt -= np.mean(PS_dt) #make sure PS bunch is centered at 0
-# print('PS beam loaded')
-
-# # how many clones and where to put them
-# n_bunches_PS = 1    # one bunch from the PS
-# bunch_copies = 72   # how many times to clone the PS bunches
-# bunch_spacing = 5   # how many SPS RF buckets between bunches in the SPS
-# 
-# n_macroparticles_pb = len(PS_dt)  # number of macro-particles per SPS bunch
-# 
-# n_bunches = n_bunches_PS * bunch_copies # number of bunches in the SPS (72)
-# intensity = n_bunches * N_b     # total intensity SPS
-# n_macroparticles = n_macroparticles_pb * bunch_copies
-# 
-# t_rf = 2*np.pi/rf.omega_rf_d[0,0]
-# 
-# PS_dt += t_rf/2 #shift PS beam to center of SPS bucket
-# 
-# # Create SPS beam
-# beam = Beam(ring, n_macroparticles, intensity)
-
-# # Create additional SPS bunches by cloning the PS beam
-# for it in range(bunch_copies):
-#     # Place SPS bunch at correct RF bucket
-#     beam.dt[int(it*n_macroparticles/bunch_copies) \
-#                 :int((it+1)*n_macroparticles/bunch_copies)] \
-#     = PS_dt + it * t_rf * n_bunches_PS*bunch_spacing
-#     
-#     # Set energy of SPS bunch
-#     beam.dE[int(it*n_macroparticles/bunch_copies) \
-#                 :int((it+1)*n_macroparticles/bunch_copies)] \
-#     = PS_dE
-# print("Beam set! Number of particles %d" %len(beam.dt))
-
-
 # Create beam
 beam = Beam(ring, n_bunches*N_m, n_bunches*N_b)
 for i in range(n_bunches):
@@ -131,23 +83,25 @@ for i in range(n_bunches):
     beam.dE[int(i*N_m):int((i+1)*N_m)] = bunch.dE
 
 profile = Profile(beam, CutOptions = CutOptions(cut_left=0.e-9, 
-    cut_right=rf.t_rev[0], n_slices=4620))
+    cut_right=rf.t_rev[0], n_slices=4620))#4620*2))
 profile.track()
+print(profile.Beam.ratio)
 
 # # Compare with induced voltage from impedances
-# TWC200_4 = TravelingWaveCavity(0.876e6, 200.222e6, 3.899e-6)
-# TWC200_5 = TravelingWaveCavity(1.38e6, 200.222e6, 4.897e-6)
-# indVoltageTWC = InducedVoltageTime(beam, profile, [TWC200_4, TWC200_4, TWC200_5, TWC200_5])
-# indVoltage = TotalInducedVoltage(beam, profile, [indVoltageTWC])
-# indVoltage.induced_voltage_sum()
-# plt.figure()
-# plt.plot(indVoltage.time_array, indVoltage.induced_voltage, 
-#          label='Time domain w FFT')
-# plt.show()
+TWC200_4 = TravelingWaveCavity(0.876e6, 200.222e6, 3.899e-6)
+TWC200_5 = TravelingWaveCavity(1.38e6, 200.222e6, 4.897e-6)
+indVoltageTWC = InducedVoltageTime(beam, profile, [TWC200_4, TWC200_4,
+                                                   TWC200_5, TWC200_5])
+indVoltage = TotalInducedVoltage(beam, profile, [indVoltageTWC])
+indVoltage.induced_voltage_sum()
+plt.figure()
+plt.plot(indVoltage.time_array, indVoltage.induced_voltage, 
+         label='Time domain w FFT')
+plt.show()
 
 
 OTFB = SPSCavityFeedback(rf, beam, profile, G_llrf=5, G_tx=0.5, a_comb=15/16, 
-                         turns=50, Commissioning=CavityFeedbackCommissioning())
+                         turns=5, Commissioning=CavityFeedbackCommissioning())
 
 tracker = RingAndRFTracker(rf, beam, CavityFeedback=OTFB, interpolation=True, 
                            Profile=profile)
@@ -158,14 +112,48 @@ tracker = RingAndRFTracker(rf, beam, CavityFeedback=OTFB, interpolation=True,
 map_ = [profile] + [OTFB] + [tracker] 
 
 
-plt.figure(4)
-ax4 = plt.axes()
+# fig1 = plt.figure(4, figsize=(8,10))
+# ax1_1 = fig1.add_subplot(2,1,1)
+# ax1_1.set_xlabel("Time [s]")
+# ax1_1.set_ylabel("Cavity voltage, real part [V]")
+# ax1_2 = fig1.add_subplot(2,1,2)
+# ax1_2.set_xlabel("Time [s]")
+# ax1_2.set_ylabel("Cavity voltage, imaginary part [V]")
+
+
+fig = plt.figure(4, figsize=(8,10))
+gs = gridspec.GridSpec(2, 1) 
+ax1_1 = plt.subplot(gs[0])
+ax1_2 = plt.subplot(gs[1], sharex=ax1_1)
+plt.setp(ax1_1.get_xticklabels(), visible=False)
+# remove last tick label for the second subplot
+yticks = ax1_2.yaxis.get_major_ticks()
+yticks[-1].label1.set_visible(False)
+plt.subplots_adjust(hspace=.0)
+ax1_1.set_ylabel("Cavity voltage, real part [MV]")
+ax1_2.set_xlabel("Time [us]")
+ax1_2.set_ylabel("Cavity voltage, imaginary part [MV]")
+#ax1_2.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+#ax1_2.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+#ax1_1.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+
+#ax4 = plt.axes()
+#ax4.set_xlabel("Time [s]")
+#             ax1.
+#             ax1.set_ylabel("Total voltage, real part [V]")
+#             ax2.set_ylabel("Total voltage, imaginary part (dotted) [V]")
+#             ax1.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+#             ax1.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+#             ax2.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+
 
 plt.figure(5)
 ax5 = plt.axes()
+ax5.set_xlabel("Time [s]")
 
 plt.figure(6)
 ax6 = plt.axes()
+ax6.set_xlabel("Time [s]")
 
 print("Starting to track...")
 for i in range(N_t):
@@ -174,12 +162,12 @@ for i in range(N_t):
     for m in map_:
         m.track()
         
-    ax4.plot(profile.bin_centers, OTFB.V_sum.real)
-    ax4.plot(profile.bin_centers, OTFB.V_sum.imag, ':')
-    ax5.plot(profile.bin_centers, OTFB.OTFB_4.V_ind_beam.real + OTFB.OTFB_5.V_ind_beam.real)
-    ax5.plot(profile.bin_centers, OTFB.OTFB_4.V_ind_beam.imag + OTFB.OTFB_5.V_ind_beam.imag, ':')
-    ax6.plot(profile.bin_centers, OTFB.OTFB_4.I_beam + OTFB.OTFB_5.I_beam.real)
-    ax6.plot(profile.bin_centers, OTFB.OTFB_4.I_beam.imag + OTFB.OTFB_5.I_beam.imag, ':')
+    ax1_1.plot(1e6*profile.bin_centers, 1e-6*OTFB.V_sum.real)
+    ax1_2.plot(1e6*profile.bin_centers, 1e-6*OTFB.V_sum.imag, ':')
+    ax5.plot(1e6*profile.bin_centers, 1e-6*OTFB.OTFB_4.V_ind_beam.real + 1e-6*OTFB.OTFB_5.V_ind_beam.real)
+    ax5.plot(1e6*profile.bin_centers, 1e-6*OTFB.OTFB_4.V_ind_beam.imag + 1e-6*OTFB.OTFB_5.V_ind_beam.imag, ':')
+#    ax6.plot(profile.bin_centers, OTFB.OTFB_4.I_beam + OTFB.OTFB_5.I_beam.real)
+#    ax6.plot(profile.bin_centers, OTFB.OTFB_4.I_beam.imag + OTFB.OTFB_5.I_beam.imag, ':')
     
 plt.show()
        
