48798c4d3db69c1c1870c167d38d2c7a61248b8c
alasheen <alexandre.lasheen@cern.ch>
2018-02-12 11:01:35 +0100

    Clean-up preprocess example and PEP8

--- __EXAMPLES/main_files/EX_06_Preprocess.py
+++ __EXAMPLES/main_files/EX_06_Preprocess.py
@@ -1,8 +1,8 @@
 # coding: utf8
 # Copyright 2014-2017 CERN. This software is distributed under the
-# terms of the GNU General Public Licence version 3 (GPL Version 3), 
+# terms of the GNU General Public Licence version 3 (GPL Version 3),
 # copied verbatim in the file LICENCE.md.
-# In applying this licence, CERN does not waive the privileges and immunities 
+# In applying this licence, CERN does not waive the privileges and immunities
 # granted to it by virtue of its status as an Intergovernmental Organization or
 # submit itself to any jurisdiction.
 # Project website: http://blond.web.cern.ch/
@@ -25,11 +25,11 @@ import os
 
 try:
     os.mkdir('../output_files')
-except:
+except FileExistsError:
     pass
 try:
     os.mkdir('../output_files/EX_06_fig')
-except:
+except FileExistsError:
     pass
 
 # Beam parameters
@@ -37,14 +37,14 @@ n_particles = 3e12
 
 
 # Machine and RF parameters
-radius = 25 # [m]
+radius = 25  # [m]
 gamma_transition = 4.076750841
 alpha = 1 / gamma_transition**2
 C = 2*np.pi*radius  # [m]
 
 # Initial and final simulation times
-initial_time = 0.277 # [s]
-final_time = 0.700 # [s]
+initial_time = 0.277  # [s]
+final_time = 0.700  # [s]
 
 momentum_program = np.loadtxt('../input_files/EX_06_Source_TOF_P.csv',
                               delimiter=',')
@@ -53,24 +53,27 @@ momentum = momentum_program[:, 1]*1e9  # [eV/c]
 
 particle_type = Proton()
 ring_opt = RingOptions(interpolation='linear', plot=True,
-                                  figdir='../output_files/EX_06_fig',
-                                  t_start=initial_time, t_end=final_time)
+                       figdir='../output_files/EX_06_fig',
+                       t_start=initial_time, t_end=final_time)
 
-general_params = Ring(C, alpha, ((time_array, momentum), ), 
-                                   particle_type, RingOptions=ring_opt)
+general_params = Ring(C, alpha, (time_array, momentum), particle_type,
+                      RingOptions=ring_opt)
 
 # Cavities parameters
-n_rf_systems = 3                                     
-harmonic_numbers_1 = 1 
-harmonic_numbers_2 = 2  
-harmonic_numbers_3 = 16                  
-phi_rf_1 = 0   # [rad]
-phi_rf_2 = np.pi # [rad]
-phi_rf_3 = np.pi/6 # [rad]
+n_rf_systems = 3
+harmonic_numbers_1 = 1
+harmonic_numbers_2 = 2
+harmonic_numbers_3 = 16
+phi_rf_1 = 0  # [rad]
+phi_rf_2 = np.pi  # [rad]
+phi_rf_3 = np.pi/6  # [rad]
 
-voltage_program_C02 = np.loadtxt('../input_files/EX_06_voltage_program_LHC25_c02.txt')
-voltage_program_C04 = np.loadtxt('../input_files/EX_06_voltage_program_LHC25_c04.txt')
-voltage_program_C16 = np.loadtxt('../input_files/EX_06_voltage_program_LHC25_c16.txt')
+voltage_program_C02 = np.loadtxt(
+    '../input_files/EX_06_voltage_program_LHC25_c02.txt')
+voltage_program_C04 = np.loadtxt(
+    '../input_files/EX_06_voltage_program_LHC25_c04.txt')
+voltage_program_C16 = np.loadtxt(
+    '../input_files/EX_06_voltage_program_LHC25_c16.txt')
 time_C02 = voltage_program_C02[:, 0]*1e-3  # [s]
 voltage_C02 = voltage_program_C02[:, 1]*1e3  # [V]
 time_C04 = voltage_program_C04[:, 0]*1e-3  # [s]
@@ -78,15 +81,19 @@ voltage_C04 = voltage_program_C04[:, 1]*1e3  # [V]
 time_C16 = voltage_program_C16[:, 0]*1e-3  # [s]
 voltage_C16 = voltage_program_C16[:, 1]*1e3  # [V]
 
-rf_station_options = RFStationOptions(interpolation = 'linear', smoothing = 0, 
-                                   plot = True, figdir='../output_files/EX_06_fig', 
-                 figname=['voltage_C02 [V]', 'voltage_C04 [V]', 'voltage_C16 [V]'], 
-                 sampling = 1)
+rf_station_options = RFStationOptions(
+    interpolation='linear', smoothing=0,
+    plot=True, figdir='../output_files/EX_06_fig',
+    figname=['voltage_C02 [V]', 'voltage_C04 [V]', 'voltage_C16 [V]'],
+    sampling=1)
 
-rf_params = RFStation(general_params,
-                      [harmonic_numbers_1, harmonic_numbers_2, harmonic_numbers_3], 
-                      ((time_C02, voltage_C02), (time_C04, voltage_C04), (time_C16, voltage_C16)), 
-                      [phi_rf_1, phi_rf_2, phi_rf_3], n_rf_systems,
-                      RFStationOptions=rf_station_options)
+rf_params = RFStation(
+    general_params,
+    [harmonic_numbers_1, harmonic_numbers_2, harmonic_numbers_3],
+    ((time_C02, voltage_C02),
+     (time_C04, voltage_C04),
+     (time_C16, voltage_C16)),
+    [phi_rf_1, phi_rf_2, phi_rf_3], n_rf_systems,
+    RFStationOptions=rf_station_options)
 
 print("Done!")
