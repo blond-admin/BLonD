b8c461a07555c9187837d2f38887fb61bd70efa8
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2018-06-11 11:55:05 +0200

    added missing package

--- blond/__init__.py
+++ blond/__init__.py
@@ -1,9 +1,13 @@
 import os
+import ctypes
+
+path = os.path.realpath(__file__)
+parent_path = os.sep.join(path.split(os.sep)[:-1])
 
 if ('posix' in os.name):
-    libblond = ctypes.CDLL('cpp_routines/libblond.so')
+    libblond = ctypes.CDLL(os.path.join(parent_path, 'cpp_routines/libblond.so'))
 elif ('win' in sys.platform):
-    libblond = ctypes.CDLL('cpp_routines\\libblond.dll')
+    libblond = ctypes.CDLL(os.path.join(parent_path, 'cpp_routines\\libblond.dll'))
 else:
     print('YOU DO NOT HAVE A WINDOWS OR LINUX OPERATING SYSTEM. ABORTING...')
     sys.exit()
