e51b3ed24a8a117348044ca8a003c2f796fb96bf
Simon Albright <simon.albright@cern.ch>
2018-11-14 12:04:13 +0100

    added docstrings and slightly improved error handling

--- blond/utils/track_iteration.py
+++ blond/utils/track_iteration.py
@@ -14,63 +14,121 @@
 
 
 class TrackIteration(object):
-
-    def __init__(self, trackMap, initTurn = 0, finalTurn = -1):
+    '''
+    Class to provide an iterator for tracking with an option to run passed
+    functions every n turns
+    
+    Parameters
+    ----------
+    
+    trackMap : iterable of objects
+        Each object will be called on every turn with object.track()
+    initTurn : integer
+        The turn number tracking will start from, only used to initialise
+        a turn counter
+    finalTurn : integer
+        The last turn number to track next(TrackIteration) will raise
+        StopIteration when turnNumber == finalTurn
         
+    Attributes
+    ----------
+    
+    functionList : List of functions to be called with specified interval
+    '''
+    
+    
+    def __init__(self, trackMap, initTurn = 0, finalTurn = -1):
+
         if not all((hasattr(m, 'track') for m in trackMap)):
             raise AttributeError("All map objects must be trackable")
             
         self._map = trackMap
-        self._initTurn = initTurn
-        self._finalTurn = finalTurn
-        self.turnNumber = initTurn
+        if isinstance(initTurn, int):
+            self.turnNumber = initTurn
+        else:
+            raise TypeError("initTurn must be an integer")
+        if isinstance(finalTurn, int):
+            self._finalTurn = finalTurn
+        else:
+            raise TypeError("finalTurn must be an integer")
+
         self.functionList = []
 
-    def track_turns(self, n_turns):
+
+    def _track_turns(self, n_turns):
+        '''
+        Function to track for specified number of turns
+        calls next() function n_turns times
+        '''
+        
         for i in range(n_turns):
             next(self)
 
 
-    def add_function(self, predicate, repitionRate, *args, **kwargs):
-        
+    def add_function(self, predicate, repetionRate, *args, **kwargs):
         '''
-        Takes a user defined callable and calls it every repritionRate
+        Takes a user defined callable and calls it every repetionRate
         number of turns with predicate(trackMap, turnNumber, *args, **kwargs)
         '''
         
-        self.functionList.append((self._partial(predicate, args, kwargs), repitionRate))
+        self.functionList.append((self._partial(predicate, args, kwargs)
+                                  , repetionRate))
         
 
     def __next__(self):
-
+        '''
+        First raises StopIteration if turnNumber == finalTurn
+        
+        Next calls track() from each element in trackMap list and raises
+        StopIteration if no more turns available
+        
+        Finally iterates over each function specified in add_function
+        and calls them with predicate(trackMap, turnNumber) if
+        turnNumber % repetitionRate == 0
+        '''
+        
         if self.turnNumber == self._finalTurn:
             raise StopIteration
 
         try:
             for m in self._map:
                 m.track()
-            self.turnNumber += 1
-            for func, rate in self.functionList:
-                if self.turnNumber % rate == 0:
-                    func(self._map, self.turnNumber)
-            return self.turnNumber
-
         except IndexError:
             raise StopIteration
 
+        self.turnNumber += 1
+
+        for func, rate in self.functionList:
+            if self.turnNumber % rate == 0:
+                func(self._map, self.turnNumber)
+
+        return self.turnNumber
+
 
     def __iter__(self):
+        '''
+        returns self
+        '''
+        
         return self
 
 
     def __call__(self, n_turns = 1):
-        self.track_turns(n_turns)
+        '''
+        Makes object callable with option to specify number of tracked turns
+        default tracks 1 turn
+        '''
+        
+        self._track_turns(n_turns)
+        return self.turnNumber
         
     
     def _partial(self, predicate, args, kwargs):
         '''
-        reimplementation of functools.partial to prepend rather than append to *args
+        reimplementation of functools.partial to prepend
+        rather than append to *args
         '''
+        
         def partFunc(_map, turn):
             return predicate(_map, turn, *args, **kwargs)
 
