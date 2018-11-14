b3729dc87f28109d7ab0cea5728c55fe118a4df4
Simon Albright <simon.albright@cern.ch>
2018-11-14 08:59:24 +0100

    added partial functions to call on nth turn

--- blond/utils/track_iteration.py
+++ blond/utils/track_iteration.py
@@ -12,27 +12,66 @@
 :Authors: **Simon Albright**
 '''
 
+
 class TrackIteration(object):
-    
-    def __init__(self, map):
+
+    def __init__(self, trackMap, initTurn = 0, finalTurn = -1):
         
-        if not all((hasattr(m, 'track') for m in map)):
+        if not all((hasattr(m, 'track') for m in trackMap)):
             raise AttributeError("All map objects must be trackable")
             
-        self.map = map
-        
-
+        self._map = trackMap
+        self._initTurn = initTurn
+        self._finalTurn = finalTurn
+        self.turnNumber = initTurn
+        self.functionList = []
 
     def track_turns(self, n_turns):
-        
         for i in range(n_turns):
             next(self)
 
 
+    def add_function(self, predicate, repitionRate, *args, **kwargs):
+        
+        '''
+        Takes a user defined callable and calls it every repritionRate
+        number of turns with predicate(trackMap, turnNumber, *args, **kwargs)
+        '''
+        
+        self.functionList.append((self._partial(predicate, args, kwargs), repitionRate))
+        
+
     def __next__(self):
-        for m in self.map:
-            m.track()
-    
+
+        if self.turnNumber == self._finalTurn:
+            raise StopIteration
+
+        try:
+            for m in self._map:
+                m.track()
+            self.turnNumber += 1
+            for func, rate in self.functionList:
+                if self.turnNumber % rate == 0:
+                    func(self._map, self.turnNumber)
+            return self.turnNumber
+
+        except IndexError:
+            raise StopIteration
+
+
     def __iter__(self):
         return self
 
+
+    def __call__(self, n_turns = 1):
+        self.track_turns(n_turns)
+        
+    
+    def _partial(self, predicate, args, kwargs):
+        '''
+        reimplementation of functools.partial to prepend rather than append to *args
+        '''
+        def partFunc(_map, turn):
+            return predicate(_map, turn, *args, **kwargs)
+
+        return partFunc
