12871223997a4ecfd99c2b86d5e402a1cf27570b
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2018-12-05 17:31:20 +0100

    Better reporting for the integration tests

--- unittests/integration/test_examples.py
+++ unittests/integration/test_examples.py
@@ -18,44 +18,44 @@ import numpy as np
 import os
 import glob
 import subprocess
-from functools import wraps
-
+# from functools import wraps
+from parameterized import parameterized
 
 this_directory = os.path.dirname(os.path.realpath(__file__)) + '/'
 main_files_dir = '__EXAMPLES/main_files'
 main_files_pattern = 'EX_*.py'
+
+os.chdir(this_directory + '../../')
+
+main_files = glob.glob(os.path.join(main_files_dir, main_files_pattern))
 
 
 class TestExamples(unittest.TestCase):
 
     # Run before every test
     def setUp(self):
-        os.chdir(this_directory + '../../')
-        self.main_files = glob.glob(os.path.join(
-            main_files_dir, main_files_pattern))
+        pass
+        # self.output = open(this_directory + 'output.txt', 'w')
 
-        self.output = open(this_directory + 'output.txt', 'w')
-    
     # Run after every test
     def tearDown(self):
-        self.output.close()
+        pass
+        # self.output.close()
 
+    @parameterized.expand(main_files)
+    def test_example(self, file):
 
-    def test_all_examples(self):
-
-        for file in self.main_files:
-            self.output.write('\nTesting file: ' + file + '\n')
-            with self.subTest(file=file):
-                try:
-                    ret = subprocess.call(['python', file], timeout=60,
-                                          stdout=self.output, stderr=subprocess.STDOUT)
-                    self.output.flush()
-                    self.assertEqual(ret, 0)
-                except subprocess.TimeoutExpired as e:
-                    self.output.write(
-                        'Timed out (timeout={}s)\n'.format(e.timeout))
-            self.output.write('Finished Testing.\n\n')
-
+        # self.output.write('\nTesting file: ' + file + '\n')
+        try:
+            ret = subprocess.call(['python', file], timeout=60,
+                                  stdout=self.output, stderr=subprocess.STDOUT)
+            self.output.flush()
+            self.assertEqual(ret, 0)
+        except subprocess.TimeoutExpired as e:
+            # self.output.write(
+            #     'Timed out (timeout={}s)\n'.format(e.timeout))
+            raise unittest.SkipTest('Timed out (timeout={}s)'.format(e.timeout))
+        # self.output.write('Finished Testing.\n\n')
 
 
 if __name__ == '__main__':
