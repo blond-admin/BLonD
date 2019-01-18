8797cfa24d10c85e31568ed9a756bec62c04e2b2
Simon Albright <simon.albright@cern.ch>
2019-01-18 14:37:31 +0100

    modifying modulation to work with redsigned blond

--- blond/llrf/rf_modulation.py
+++ blond/llrf/rf_modulation.py
@@ -16,170 +16,195 @@ and offset functions**
 
 from __future__ import division
 from __future__ import print_function
-from builtins import str, range, object
+#from builtins import str, range, object
 import numpy as np
 import scipy.interpolate as interp
 
-from ..input_parameters.rf_parameters_options import RFStationOptions
-prep = RFStationOptions()
+#from ..input_parameters.rf_parameters_options import RFStationOptions
+#prep = RFStationOptions()
 
 
-class PhaseModulation(object):
 
-	def __init__(self, frequency, amplitude, offset, time, freqMultiplier = 1, interpType = 'cubic', smoothing = 1000, ontime = 0, offtime = 0):
+class PhaseModulation:
+    
+    def __init__(self, frequency, amplitude, offset, multiplier = 1, system = None):
+        
+        
+        try:
+            if len(frequency) == 2:
+                self.frequency = frequency
+            else:
+                raise ValueError("frequency must be single valued or shape: (2, n)")
+        except TypeError:
+            try:
+                int(frequency)
+                self.frequency = frequency
+            except TypeError
+            
+        
+        self.frequency = frequency
+        self.amplitude = amplitude
+        self.offset = offset
+        self.multiplier = multiplier
+        self.system = system
+        
+        
 
-		'''
-		Generate phase modulation to be applied to phi_rf
-		frequency, amplitude and offset.  All three will be
-		interpolated onto the passed time array.  They can
-		either be a single value, a list of 2 points or a
-		2D numpy array with array[0] as time and array[1] as
-		the function.  If a list of 2 points is passed it
-		will be assumed that the first and last points are
-		at the beginning and end of the time array
-	
-		freqMultiplier is a multiplier applied to the modulation
-		frequency allowing e.g. a fixed multiple of synchrotron
-		frequency to be used.
-		'''
 
-		if not isinstance(frequency, np.ndarray):
-			if isinstance(frequency, list):
-				frequency = np.array([[time[0], time[-1]], [frequency[0], frequency[-1]]])
-			else:
-				frequency = np.array([[time[0], time[-1]], [frequency, frequency]])
+class OldPhaseModulation:
 
-		else:
-			early = np.where(frequency[0] < time[0])[0]
-			late = np.where(frequency[0] > time[-1])[0]
+    def __init__(self, frequency, amplitude, offset, time, freqMultiplier = 1, interpType = 'cubic', smoothing = 1000, ontime = 0, offtime = 0):
 
-			if len(early) == 0:
-				early = 0
-			else:
-				early = early[-1]
-			if len(late) == 0:
-				late = -1
-			else:
-				late = late[0]
+        '''
+        Generate phase modulation to be applied to phi_rf
+        frequency, amplitude and offset.  All three will be
+        interpolated onto the passed time array.  They can
+        either be a single value, a list of 2 points or a
+        2D numpy array with array[0] as time and array[1] as
+        the function.  If a list of 2 points is passed it
+        will be assumed that the first and last points are
+        at the beginning and end of the time array
+    
+        freqMultiplier is a multiplier applied to the modulation
+        frequency allowing e.g. a fixed multiple of synchrotron
+        frequency to be used.
+        '''
 
-			newFreq = frequency[1][early:late]
-			newTime = frequency[0][early:late]
+        if not isinstance(frequency, np.ndarray):
+            if isinstance(frequency, list):
+                frequency = np.array([[time[0], time[-1]], [frequency[0], frequency[-1]]])
+            else:
+                frequency = np.array([[time[0], time[-1]], [frequency, frequency]])
 
-			frequency = np.zeros([2, len(newFreq)])
-			frequency[0] = newTime
-			frequency[1] = newFreq
+        else:
+            early = np.where(frequency[0] < time[0])[0]
+            late = np.where(frequency[0] > time[-1])[0]
 
-		frequency[1] *= freqMultiplier
+            if len(early) == 0:
+                early = 0
+            else:
+                early = early[-1]
+            if len(late) == 0:
+                late = -1
+            else:
+                late = late[0]
 
-		if not isinstance(amplitude, np.ndarray):
-			if isinstance(amplitude, list):
-				amplitude = np.array([[time[0], time[-1]], [amplitude[0], amplitude[-1]]])
-			else:
-				amplitude = np.array([time, [amplitude]*len(time)])
+            newFreq = frequency[1][early:late]
+            newTime = frequency[0][early:late]
 
-		if not isinstance(offset, np.ndarray):
-			if isinstance(offset, list):
-				offset = np.array([[time[0], time[-1]], [offset[0], offset[-1]]])
-			else:
-				offset = np.array([time, [offset]*len(time)])
+            frequency = np.zeros([2, len(newFreq)])
+            frequency[0] = newTime
+            frequency[1] = newFreq
 
-		self.time = time
+        frequency[1] *= freqMultiplier
 
-		if interpType == 'cubic':
-			freqInterp = interp.splrep(frequency[0], frequency[1], s=smoothing)
-			ampInterp = interp.splrep(amplitude[0], amplitude[1], s=smoothing)
-			offInterp = interp.splrep(offset[0], offset[1], s=smoothing)
+        if not isinstance(amplitude, np.ndarray):
+            if isinstance(amplitude, list):
+                amplitude = np.array([[time[0], time[-1]], [amplitude[0], amplitude[-1]]])
+            else:
+                amplitude = np.array([time, [amplitude]*len(time)])
 
-			self.frequency = interp.splev(self.time, freqInterp)
-			self.amplitude = interp.splev(self.time, ampInterp)
-			self.offset = interp.splev(self.time, offInterp)
+        if not isinstance(offset, np.ndarray):
+            if isinstance(offset, list):
+                offset = np.array([[time[0], time[-1]], [offset[0], offset[-1]]])
+            else:
+                offset = np.array([time, [offset]*len(time)])
 
-		elif interpType == 'linear':
-			self.frequency = np.interp(self.time, frequency[0], frequency[1])
-			self.amplitude = np.interp(self.time, amplitude[0], amplitude[1])
-			self.offset = np.interp(self.time, offset[0], offset[1])
+        self.time = time
 
-		else:
-			print("Interpolation type not recognised, defaulting to cubic spline")
-			freqInterp = interp.splrep(frequency[0], frequency[1], s=smoothing)
-			ampInterp = interp.splrep(amplitude[0], amplitude[1], s=smoothing)
-			offInterp = interp.splrep(offset[0], offset[1], s=smoothing)
+        if interpType == 'cubic':
+            freqInterp = interp.splrep(frequency[0], frequency[1], s=smoothing)
+            ampInterp = interp.splrep(amplitude[0], amplitude[1], s=smoothing)
+            offInterp = interp.splrep(offset[0], offset[1], s=smoothing)
 
-			self.frequency = interp.splev(self.time, freqInterp)
-			self.amplitude = interp.splev(self.time, ampInterp)
-			self.offset = interp.splev(self.time, offInterp)
+            self.frequency = interp.splev(self.time, freqInterp)
+            self.amplitude = interp.splev(self.time, ampInterp)
+            self.offset = interp.splev(self.time, offInterp)
 
-		self.ontime = ontime
-		self.offtime = offtime
+        elif interpType == 'linear':
+            self.frequency = np.interp(self.time, frequency[0], frequency[1])
+            self.amplitude = np.interp(self.time, amplitude[0], amplitude[1])
+            self.offset = np.interp(self.time, offset[0], offset[1])
+
+        else:
+            print("Interpolation type not recognised, defaulting to cubic spline")
+            freqInterp = interp.splrep(frequency[0], frequency[1], s=smoothing)
+            ampInterp = interp.splrep(amplitude[0], amplitude[1], s=smoothing)
+            offInterp = interp.splrep(offset[0], offset[1], s=smoothing)
+
+            self.frequency = interp.splev(self.time, freqInterp)
+            self.amplitude = interp.splev(self.time, ampInterp)
+            self.offset = interp.splev(self.time, offInterp)
+
+        self.ontime = ontime
+        self.offtime = offtime
 
 
 
 def ModulateHarmonic(Ring, RFStation, modulationList, harmonic, includeFreq = True, preshift = 0):
 
 
-	'''
-	Method to apply modulation(s) to RFStation
-	modulationList is a list of modulation objects applied
-	sequentially to RFStation.
+    '''
+    Method to apply modulation(s) to RFStation
+    modulationList is a list of modulation objects applied
+    sequentially to RFStation.
 
-	harmonicList specifies which harmonic of RFStation
-	will be modulated
+    harmonicList specifies which harmonic of RFStation
+    will be modulated
 
-	includeFreq flag determines if effect of modulation on
-	RF frequency should be included.  False will treat
-	modulation as pure phase shift, True will include the
-	small frequency shift needed to adjust the phase.
-	'''
+    includeFreq flag determines if effect of modulation on
+    RF frequency should be included.  False will treat
+    modulation as pure phase shift, True will include the
+    small frequency shift needed to adjust the phase.
+    '''
 
-	print("Modulating " + str(len(modulationList)) + " sections")
-	print("Harmonic: " + str(harmonic))
 
-	if isinstance(preshift, np.ndarray):
-		if len(preshift.shape) == 2:
-			preshift = prep.preprocess_rf_params(Ring, [preshift[0]], [preshift[1]], plot=False)[0]
-		elif len(preshift) == 2:
-			preshift = prep.preprocess_rf_params(Ring, [[Ring.cumulative_times[0], Ring.cumulative_times[-1]]], [[preshift[0], preshift[1]]], plot=False)[0]
-		else:
-			print("ERROR: preshift array is incorrectly shaped")
-	elif isinstance(preshift, list):
-		if len(preshift) == 2:
-			preshift = prep.preprocess_rf_params(Ring, [[Ring.cumulative_times[0], Ring.cumulative_times[-1]]], [[preshift[0], preshift[1]]], plot=False)[0]
-		else:
-			print("ERROR: preshift list is the wrong length")
+    if isinstance(preshift, np.ndarray):
+        if len(preshift.shape) == 2:
+            preshift = prep.preprocess_rf_params(Ring, [preshift[0]], [preshift[1]], plot=False)[0]
+        elif len(preshift) == 2:
+            preshift = prep.preprocess_rf_params(Ring, [[Ring.cumulative_times[0], Ring.cumulative_times[-1]]], [[preshift[0], preshift[1]]], plot=False)[0]
+        else:
+            print("ERROR: preshift array is incorrectly shaped")
+    elif isinstance(preshift, list):
+        if len(preshift) == 2:
+            preshift = prep.preprocess_rf_params(Ring, [[Ring.cumulative_times[0], Ring.cumulative_times[-1]]], [[preshift[0], preshift[1]]], plot=False)[0]
+        else:
+            print("ERROR: preshift list is the wrong length")
 
-	fullPhiAddition = np.zeros(len(Ring.cumulative_times)) + preshift
-	
-	startPoints = []
-	stopPoints = []
+    fullPhiAddition = np.zeros(len(Ring.cycle_time)) + preshift
+    
+    startPoints = []
+    stopPoints = []
 
-	for i in range(len(modulationList)):
-		
-		modulation = modulationList[i]
+    for i in range(len(modulationList)):
+        
+        modulation = modulationList[i]
 
-		modOff = prep.preprocess_rf_params(Ring, [[modulation.time[0] - modulation.ontime] + modulation.time.tolist() + [modulation.time[-1] + modulation.offtime]], [[0] + modulation.offset.tolist() + [0]], plot=False)
-		modDepth = prep.preprocess_rf_params(Ring, [modulation.time], [modulation.amplitude], plot=False)
-		modFreq = prep.preprocess_rf_params(Ring, [modulation.time], [modulation.frequency], plot=False)
+        modOff = prep.preprocess_rf_params(Ring, [[modulation.time[0] - modulation.ontime] + modulation.time.tolist() + [modulation.time[-1] + modulation.offtime]], [[0] + modulation.offset.tolist() + [0]], plot=False)
+        modDepth = prep.preprocess_rf_params(Ring, [modulation.time], [modulation.amplitude], plot=False)
+        modFreq = prep.preprocess_rf_params(Ring, [modulation.time], [modulation.frequency], plot=False)
 
-		start = np.where(Ring.cumulative_times < modulation.time[0])[0]
-		if len(start) == 0:
-			start = [0, 1]
-		stop = np.where(Ring.cumulative_times > modulation.time[-1])[0]
-		if len(stop)==0:
-			stop = [-1, -2]
+        start = np.where(Ring.cumulative_times < modulation.time[0])[0]
+        if len(start) == 0:
+            start = [0, 1]
+        stop = np.where(Ring.cumulative_times > modulation.time[-1])[0]
+        if len(stop)==0:
+            stop = [-1, -2]
 
-		modDepth[0][:start[-2]] = 0
-		modDepth[0][stop[1]:] = 0
+        modDepth[0][:start[-2]] = 0
+        modDepth[0][stop[1]:] = 0
 
-		phiAddition = (modDepth*np.sin(2*np.pi*(np.cumsum(modFreq*np.gradient(Ring.cumulative_times)))) + modOff)[0]
+        phiAddition = (modDepth*np.sin(2*np.pi*(np.cumsum(modFreq*np.gradient(Ring.cumulative_times)))) + modOff)[0]
 
-		fullPhiAddition[start[-2]:stop[1]] += phiAddition[start[-2]:stop[1]]
+        fullPhiAddition[start[-2]:stop[1]] += phiAddition[start[-2]:stop[1]]
 
-		startPoints.append(start[-1])
-		stopPoints.append(stop[0])
+        startPoints.append(start[-1])
+        stopPoints.append(stop[0])
 
-	RFStation.phi_RF[harmonic] += fullPhiAddition
+    RFStation.phi_RF[harmonic] += fullPhiAddition
 
-	if includeFreq:
+    if includeFreq:
 
-		freqAddition = (np.gradient(fullPhiAddition)*RFStation.omega_RF[harmonic])/(2*np.pi*RFStation.harmonic[harmonic])
-		RFStation.omega_RF[harmonic] += freqAddition
+        freqAddition = (np.gradient(fullPhiAddition)*RFStation.omega_RF[harmonic])/(2*np.pi*RFStation.harmonic[harmonic])
+        RFStation.omega_RF[harmonic] += freqAddition
