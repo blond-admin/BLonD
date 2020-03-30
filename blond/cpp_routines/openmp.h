f529e8551cf596a911e1c450034d1990576d416a
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2020-03-30 13:59:15 +0200

    Fixes compile error in Windows
    It is best to separate the function declaration (header file) from the
    function definition(.cpp file).

--- blond/cpp_routines/openmp.h
+++ blond/cpp_routines/openmp.h
@@ -1,13 +1,13 @@
 
-#ifndef OPENMP_H
-#define OPENMP_H
+#ifndef _OPENMP_H_
+#define _OPENMP_H_
 
 #ifdef PARALLEL
-#include <omp.h>        // omp_get_thread_num(), omp_get_num_threads()
+	#include <omp.h>        // omp_get_thread_num(), omp_get_num_threads()
 #else
-int omp_get_max_threads() {return 1;}
-int omp_get_num_threads() {return 1;}
-int omp_get_thread_num() {return 0;}
+	int omp_get_max_threads();
+	int omp_get_num_threads();
+	int omp_get_thread_num();
 #endif
 
-#endif // OPENMP_H
+#endif // _OPENMP_H_
