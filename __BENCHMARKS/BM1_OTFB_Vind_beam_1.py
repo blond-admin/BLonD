Fixed some typos and tried changing some signs for the OTFB.

__BENCHMARKS/BM1_OTFB_Vind_beam_1.py
@@ -51,10 +51,10 @@ N_t = 1000                  # Number of turns to track
 
 # OPTIONS TO TEST -------------------------------------------------------------
 LOGGING = False              # Logging messages
-RF_CURRENT = False           # RF beam current
-RF_CURRENT2 = False          # RF beam current
-IMP_RESP = False             # Impulse response of travelling wave cavity
-FINE_COARSE = False          # Beam-induced voltage on fine/coarse grid
+RF_CURRENT = True           # RF beam current
+RF_CURRENT2 = True          # RF beam current
+IMP_RESP = True             # Impulse response of travelling wave cavity
+FINE_COARSE = True          # Beam-induced voltage on fine/coarse grid
 VIND_BEAM = True            # Beam-induced voltage
 
 # OPTIONS TO TEST -------------------------------------------------------------
@@ -250,7 +250,7 @@ if FINE_COARSE == True:
     # Impulse response and induced voltage through OTFB object
     OTFB.TWC.impulse_response_beam(OTFB.omega_c, OTFB.profile.bin_centers,
                                    OTFB.rf_centers)
-    OTFB.beam_induced_voltage(lpf=False)
+    OTFB.beam_response()
     np.set_printoptions(precision=10)
     #print(repr((OTFB.TWC.h_beam[::1000])[:100]))
     #print(repr(OTFB.TWC.h_beam_coarse[:100]))
@@ -273,13 +273,13 @@ if FINE_COARSE == True:
 
     plt.figure()
     plt.plot(time_fine*1e6, V_beam_fine.real*1e-6, 'b', marker='.', label='V_beam, fine, real')
-    plt.plot(time_fine*1e6, OTFB.V_fine_ind_beam.real*1e-6, 'b', marker='.', alpha=0.5, label='V_beam, fine, real')
+    plt.plot(time_fine*1e6, OTFB.V_IND_FINE_BEAM[-OTFB.profile.n_slices:].real*1e-6, 'b', marker='.', alpha=0.5, label='V_beam, fine, real')
     plt.plot(time_coarse*1e6, V_beam_coarse.real*1e-6, 'teal', marker='.', label='V_beam, coarse, real')
-    plt.plot(time_coarse*1e6, OTFB.V_coarse_ind_beam.real*1e-6, 'teal', marker='.', alpha=0.5, label='V_beam, coarse, real')
+    plt.plot(time_coarse*1e6, OTFB.V_IND_COARSE_BEAM[-h[0]:].real*1e-6, 'teal', marker='.', alpha=0.5, label='V_beam, coarse, real')
     plt.plot(time_fine*1e6, V_beam_fine.imag*1e-6, 'r', marker='.', label='V_beam, fine, imag')
-    plt.plot(time_fine*1e6, OTFB.V_fine_ind_beam.imag*1e-6, 'r', marker='.', alpha=0.5, label='V_beam, fine, imag')
+    plt.plot(time_fine*1e6, OTFB.V_IND_FINE_BEAM[-OTFB.profile.n_slices:].imag*1e-6, 'r', marker='.', alpha=0.5, label='V_beam, fine, imag')
     plt.plot(time_coarse*1e6, V_beam_coarse.imag*1e-6, 'orange', marker='.', label='V_beam, coarse, imag')
-    plt.plot(time_coarse*1e6, OTFB.V_coarse_ind_beam.imag*1e-6, 'orange', marker='.', alpha=0.5, label='V_beam, coarse, imag')
+    plt.plot(time_coarse*1e6, OTFB.V_IND_COARSE_BEAM[-h[0]:].imag*1e-6, 'orange', marker='.', alpha=0.5, label='V_beam, coarse, imag')
     plt.xlabel("Time [us]")
     plt.ylabel("Induced voltage per cavity [MV]")
     plt.xlim((-1,5))
@@ -306,13 +306,14 @@ if VIND_BEAM == True:
     OTFB_3.omega_c = omega_c
     OTFB_4.omega_c = omega_c
     OTFB_5.omega_c = omega_c
-    OTFB_3.TWC.impulse_response_beam(omega_c, profile.bin_centers)
-    OTFB_4.TWC.impulse_response_beam(omega_c, profile.bin_centers)
-    OTFB_5.TWC.impulse_response_beam(omega_c, profile.bin_centers)
-    OTFB_3.beam_induced_voltage(lpf=False)
-    OTFB_4.beam_induced_voltage(lpf=False)
-    OTFB_5.beam_induced_voltage(lpf=False)
-    V_ind_beam = OTFB_3.V_fine_ind_beam +OTFB_4.V_fine_ind_beam + OTFB_5.V_fine_ind_beam
+    #OTFB_3.TWC.impulse_response_beam(omega_c, profile.bin_centers)
+    #OTFB_4.TWC.impulse_response_beam(omega_c, profile.bin_centers)
+    #OTFB_5.TWC.impulse_response_beam(omega_c, profile.bin_centers)
+    OTFB_3.track()
+    OTFB_4.track()
+    OTFB_5.track()
+    V_ind_beam = OTFB_3.V_IND_FINE_BEAM[-OTFB_3.profile.n_slices:] + OTFB_4.V_IND_FINE_BEAM[-OTFB_4.profile.n_slices:] + \
+                 OTFB_5.V_IND_FINE_BEAM[-OTFB_5.profile.n_slices:]
     plt.figure()
     convtime = np.linspace(-1e-9, -1e-9+len(V_ind_beam.real)*
                            profile.bin_size, len(V_ind_beam.real))

blond/beam/distributions_multibunch.py
@@ -271,9 +271,9 @@ def matched_from_distribution_density_multibunch(beam, Ring, FullRingAndRF, dist
             voltages = np.append(voltages, RingAndRFSectionElement.voltage[rf_system, 0])
             harmonics = np.append(harmonics, RingAndRFSectionElement.harmonic[rf_system, 0])
 
-    if main_harmonic_option is 'lowest_freq':
+    if main_harmonic_option == 'lowest_freq':
         main_harmonic = np.min(harmonics)
-    elif main_harmonic_option is 'highest_voltage':
+    elif main_harmonic_option == 'highest_voltage':
         main_harmonic = np.min(harmonics[voltages == np.max(voltages)])
     elif isinstance(main_harmonic_option, int) or isinstance(main_harmonic_option, float):
         if harmonics[harmonics == main_harmonic_option].size == 0:
@@ -340,12 +340,12 @@ def matched_from_distribution_density_multibunch(beam, Ring, FullRingAndRF, dist
         else:
             distribution_variable = None
 
-        if distribution_options['type'] is 'user_input':
+        if distribution_options['type'] == 'user_input':
             distribution_function_input = distribution_options['function']
         else:
             distribution_function_input = None
 
-        if distribution_options['type'] is 'user_input_table':
+        if distribution_options['type'] == 'user_input_table':
             distribution_user_table = {
                 'user_table_action': distribution_options['user_table_action'],
                 'user_table_density': distribution_options['user_table_density']}

blond/llrf/cavity_feedback.py
@@ -33,7 +33,8 @@ class CavityFeedbackCommissioning(object):
 
     def __init__(self, debug=False, open_loop=False, open_FB=False,
                  open_drive=False, open_FF=False, V_SET=None,
-                 cpp_conv = False, pwr_clamp = False, phase_corr_sign = +1):
+                 cpp_conv = False, pwr_clamp = False, phase_corr_sign = +1,
+                 rot_IQ = 1):
         """Class containing commissioning settings for the cavity feedback
 
         Parameters
@@ -62,6 +63,7 @@ class CavityFeedbackCommissioning(object):
         self.cpp_conv = cpp_conv
         self.pwr_clamp = pwr_clamp
         self.phase_corr_sign = phase_corr_sign
+        self.rot_IQ = rot_IQ
 
 
 class SPSCavityFeedback(object):
@@ -119,7 +121,7 @@ class SPSCavityFeedback(object):
     """
 
     def __init__(self, RFStation, Beam, Profile, G_ff=1, G_llrf=10, G_tx=0.5,
-                 a_comb=None, turns=1000, post_LS2=True, V_part=None, domega=0,
+                 a_comb=None, turns=1000, post_LS2=True, V_part=None, df=0,
                  Commissioning=CavityFeedbackCommissioning()):
 
 
@@ -151,12 +153,12 @@ class SPSCavityFeedback(object):
             G_tx_1 = G_tx
             G_tx_2 = G_tx
 
-        if type(domega) is list:
-            domega_1 = domega[0]
-            domega_2 = domega[1]
+        if type(df) is list:
+            df_1 = df[0]
+            df_2 = df[1]
         else:
-            domega_1 = domega
-            domega_2 = domega
+            df_1 = df
+            df_2 = df
 
         # Voltage partitioning has to be a fraction
         if V_part and V_part*(1 - V_part) < 0:
@@ -175,7 +177,7 @@ class SPSCavityFeedback(object):
                                              G_llrf=float(G_llrf_1),
                                              G_tx=float(G_tx_1),
                                              a_comb=float(a_comb),
-                                             domega=float(domega_1),
+                                             df=float(df_1),
                                              Commissioning=self.Commissioning)
             self.OTFB_2 = SPSOneTurnFeedback(RFStation, Beam, Profile, 4,
                                              n_cavities=2, V_part=1-V_part,
@@ -183,7 +185,7 @@ class SPSCavityFeedback(object):
                                              G_llrf=float(G_llrf_2),
                                              G_tx=float(G_tx_2),
                                              a_comb=float(a_comb),
-                                             domega=float(domega_2),
+                                             df=float(df_2),
                                              Commissioning=self.Commissioning)
         else:
             if not a_comb:
@@ -197,7 +199,7 @@ class SPSCavityFeedback(object):
                                              G_llrf=float(G_llrf_1),
                                              G_tx=float(G_tx_1),
                                              a_comb=float(a_comb),
-                                             domega=float(domega_1),
+                                             df=float(df_1),
                                              Commissioning=self.Commissioning)
             self.OTFB_2 = SPSOneTurnFeedback(RFStation, Beam, Profile, 5,
                                              n_cavities=2, V_part=1-V_part,
@@ -205,7 +207,7 @@ class SPSCavityFeedback(object):
                                              G_llrf=float(G_llrf_2),
                                              G_tx=float(G_tx_2),
                                              a_comb=float(a_comb),
-                                             domega=float(domega_2),
+                                             df=float(df_2),
                                              Commissioning=self.Commissioning)
 
         # Set up logging
@@ -276,7 +278,7 @@ class SPSCavityFeedback(object):
 class SPSOneTurnFeedback(object):
 
     def __init__(self, RFStation, Beam, Profile, n_sections, n_cavities=4,
-                 V_part=4/9, G_ff=1, G_llrf=10, G_tx=0.5, a_comb=63/64, domega=0,
+                 V_part=4/9, G_ff=1, G_llrf=10, G_tx=0.5, a_comb=63/64, df=0,
                  Commissioning=CavityFeedbackCommissioning()):
 
         # Set up logging
@@ -310,6 +312,7 @@ class SPSOneTurnFeedback(object):
             self.set_point_modulation = True
 
         self.cpp_conv = Commissioning.cpp_conv
+        self.rot_IQ = Commissioning.rot_IQ
 
         # Read input
         self.rf = RFStation
@@ -572,7 +575,7 @@ class SPSOneTurnFeedback(object):
 
         # Convert to array
         self.V_SET[:self.n_coarse] = self.V_SET[-self.n_coarse]
-        self.V_SET[-self.n_coarse:] = self.V_set * np.ones(self.n_coarse)
+        self.V_SET[-self.n_coarse:] = self.V_set * np.ones(self.n_coarse) * self.rot_IQ
 
 
     def set_point_mod(self):
@@ -664,11 +667,13 @@ class SPSOneTurnFeedback(object):
 
         if coarse:
             self.V_IND_COARSE_BEAM[:self.n_coarse] = self.V_IND_COARSE_BEAM[-self.n_coarse:]
-            self.V_IND_COARSE_BEAM[-self.n_coarse:] = self.n_cavities * self.matr_conv(self.I_COARSE_BEAM,
+            self.V_IND_COARSE_BEAM[-self.n_coarse:] = self.rot_IQ * self.n_cavities * \
+                                                      self.matr_conv(self.I_COARSE_BEAM,
                                                                             self.TWC.h_beam_coarse)[-self.n_coarse:]
         else:
             self.V_IND_FINE_BEAM[:self.profile.n_slices] = self.V_IND_FINE_BEAM[-self.profile.n_slices:]
-            self.V_IND_FINE_BEAM[-self.profile.n_slices:] = self.n_cavities * self.matr_conv(self.I_FINE_BEAM,
+            self.V_IND_FINE_BEAM[-self.profile.n_slices:] = self.rot_IQ * self.n_cavities * \
+                                                            self.matr_conv(self.I_FINE_BEAM,
                                                                             self.TWC.h_beam)[-self.profile.n_slices:]
 
 

blond/llrf/impulse_response.py
@@ -208,7 +208,7 @@ class TravellingWaveCavity(object):
 
     """
 
-    def __init__(self, l_cell, N_cells, rho, v_g, omega_r, domega = 0):
+    def __init__(self, l_cell, N_cells, rho, v_g, omega_r, df = 0):
 
         self.l_cell = float(l_cell)
         self.N_cells = int(N_cells)
@@ -219,7 +219,7 @@ class TravellingWaveCavity(object):
             #ImpulseError
             raise RuntimeError("ERROR in TravellingWaveCavity: group" +
                                " velocity out of limits (0,1)!")
-        self.omega_r = float(omega_r) + 2 * np.pi * float(domega)
+        self.omega_r = float(omega_r) + 2 * np.pi * float(df)
 
         # Calculated
         self.l_cav = float(self.l_cell*self.N_cells)
@@ -326,7 +326,7 @@ class TravellingWaveCavity(object):
         t_beam = time_fine - time_fine[0]
 
         # Impulse response if on carrier frequency
-        self.h_beam = (-2*self.R_beam/self.tau*                                 # TODO: minus
+        self.h_beam = -(-2*self.R_beam/self.tau*                                 # TODO: minus (without minus wake from impedance matches)
                        triangle(t_beam, self.tau)).astype(np.complex128)
 
         # Impulse response if not on carrier frequency
@@ -378,23 +378,23 @@ class TravellingWaveCavity(object):
 
 class SPS3Section200MHzTWC(TravellingWaveCavity):
 
-    def __init__(self, domega = 0):
+    def __init__(self, df = 0):
 
         TravellingWaveCavity.__init__(self, 0.374, 32, 2.71e4, 0.0946,
-                                      2*np.pi*200.03766667e6, domega = domega)
+                                      2*np.pi*200.03766667e6, df = df)
 
 
 class SPS4Section200MHzTWC(TravellingWaveCavity):
 
-    def __init__(self, domega = 0):
+    def __init__(self, df = 0):
 
         TravellingWaveCavity.__init__(self, 0.374, 43, 2.71e4, 0.0946,
-                                      2*np.pi*199.9945e6, domega = domega)
+                                      2*np.pi*199.9945e6, df = df)
 
 
 class SPS5Section200MHzTWC(TravellingWaveCavity):
 
-    def __init__(self, domega = 0):
+    def __init__(self, df = 0):
 
         TravellingWaveCavity.__init__(self, 0.374, 54, 2.71e4, 0.0946,
-                                      2*np.pi*200.1e6, domega = domega)
+                                      2*np.pi*200.1e6, df = df)