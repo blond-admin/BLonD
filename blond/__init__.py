0e8e6889cf63663b65c0204ca505216170bbeea7
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2022-05-09 15:44:11 +0200

    sets up efficient compile mechanism

--- blond/__init__.py
+++ blond/__init__.py
@@ -5,17 +5,24 @@ import sys
 path = os.path.realpath(__file__)
 basepath = os.sep.join(path.split(os.sep)[:-1])
 libblond = None
+libblond_path = os.environ.get('LIBBLOND', None)
 try:
-    if ('posix' in os.name):
-        libblond = ctypes.CDLL(os.path.join(
-            basepath, 'cpp_routines/libblond.so'))
-    elif ('win' in sys.platform):
-        dllpath = os.path.join(basepath, 'cpp_routines')
-        if hasattr(os, 'add_dll_directory'):
-            os.add_dll_directory(dllpath)
-            libblond = ctypes.CDLL(os.path.join(dllpath, 'libblond.dll'), winmode=0)
+    if 'posix' in os.name:
+        if libblond_path:
+            libblond_path = os.path.abspath(libblond_path)
         else:
-            libblond = ctypes.CDLL(os.path.join(dllpath, 'libblond.dll'))
+            libblond_path = os.path.join(basepath, 'cpp_routines/libblond.so')
+        libblond = ctypes.CDLL(libblond_path)
+    elif 'win' in sys.platform:
+        if libblond_path:
+            libblond_path = os.path.abspath(libblond_path)
+        else:
+            libblond_path = os.path.join(basepath, 'cpp_routines/libblond.dll')
+        if hasattr(os, 'add_dll_directory'):
+            os.add_dll_directory(os.path.dirname(libblond_path))
+            libblond = ctypes.CDLL(libblond_path, winmode=0)
+        else:
+            libblond = ctypes.CDLL(libblond_path)
     else:
         print('YOU DO NOT HAVE A WINDOWS OR LINUX OPERATING SYSTEM. ABORTING...')
         sys.exit()
