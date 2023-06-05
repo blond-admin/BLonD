94274de43f7e8cefc6fe92b93e4f864a374d6a7a
Oleksandr Naumenko <oleksandr.naumenko@cern.ch>
2023-06-05 12:18:23 +0200

    Adds show_plot argument to Plot class to show plots directly in Console/Notebook cells

--- blond/plots/plot_llrf.py
+++ blond/plots/plot_llrf.py
@@ -22,8 +22,10 @@ import numpy as np
 from pylab import cm
 
 
-def plot_noise_spectrum(frequency, spectrum, sampling=1, dirname='fig',
-                        figno=0):
+
+def plot_noise_spectrum(frequency, spectrum, sampling = 1, dirname = 'fig', show_plot = False,
+                        figno = 0):
+    
     """
     Plot of the phase noise spectrum.
     For large amount of data, use "sampling" to plot a fraction of the data.
@@ -31,9 +33,9 @@ def plot_noise_spectrum(frequency, spectrum, sampling=1, dirname='fig',
 
     # Plot
     fig = plt.figure(1)
-    fig.set_size_inches(8, 6)
-    ax = plt.axes([0.15, 0.1, 0.8, 0.8])
-    ax.set_xlim([0, 300])
+    fig.set_size_inches(8,6)
+    ax = plt.axes()
+    ax.set_xlim([0, 300])    
     ax.plot(frequency[::sampling], spectrum[::sampling])
     ax.set_xlabel("Frequency [Hz]")
     params = {'text.usetex': False, 'mathtext.default': 'sf'}
@@ -41,13 +43,19 @@ def plot_noise_spectrum(frequency, spectrum, sampling=1, dirname='fig',
     ax.set_ylabel(r"Noise spectrum [$\frac{rad^2}{Hz}$]")
     ax.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
 
-    # Save figure
-    fign = dirname + '/noise_spectrum_' "%d" % figno + '.png'
-    plt.savefig(fign)
+    # Output plot
+    if show_plot:
+        plt.show()
+    else:
+        fign = dirname +'/noise_spectrum_' "%d" %figno +'.png'
+        plt.savefig(fign)
     plt.clf()
 
-
-def plot_phase_noise(time, dphi, sampling=1, dirname='fig', figno=0):
+        
+    
+def plot_phase_noise(time, dphi, sampling = 1, dirname = 'fig', show_plot = False,
+                     figno = 0):
+    
     """
     Plot of phase noise as a function of time.
     For large amount of data, use "sampling" to plot a fraction of the data.
@@ -55,20 +63,24 @@ def plot_phase_noise(time, dphi, sampling=1, dirname='fig', figno=0):
 
     # Plot
     fig = plt.figure(1)
-    fig.set_size_inches(8, 6)
-    ax = plt.axes([0.15, 0.1, 0.8, 0.8])
+    fig.set_size_inches(8,6)
+    ax = plt.axes()
     ax.plot(time[::sampling], dphi[::sampling])
     ax.set_xlabel("Time [s]")
     ax.set_ylabel(r"Phase noise [rad]")
 
-    # Save figure
-    fign = dirname + '/phase_noise_' "%d" % figno + '.png'
-    plt.savefig(fign)
-    plt.clf()
+    # Output plot
+    if show_plot:
+        plt.show()
+    else:
+        fign = dirname +'/phase_noise_' "%d" %figno +'.png'
+        plt.savefig(fign)
+    plt.clf()     
 
 
-def plot_PL_bunch_phase(RFStation, h5data, output_freq=1,
-                        dirname='fig'):
+def plot_PL_bunch_phase(RFStation, h5data, output_freq = 1, 
+                        dirname = 'fig', show_plot = False):
+    
     """
     Plot of bunch phase measured by the Phase Loop as a function of time.
     For large amount of data, monitor with larger 'output_freq'.
@@ -87,23 +99,27 @@ def plot_PL_bunch_phase(RFStation, h5data, output_freq=1,
 
     # Plot
     fig = plt.figure(1)
-    fig.set_size_inches(8, 6)
-    ax = plt.axes([0.15, 0.1, 0.8, 0.8])
-    ax.plot(t, dphi, '.')
-    ax.set_xlabel(r"No. turns [T$_0$]")
-    ax.set_ylabel(r"PL $\phi_{\mathsf{bunch}}$ [rad]")
-    ax.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+    fig.set_size_inches(8,6)
+    ax = plt.axes()
+    ax.plot(t, dphi,'.')
+    ax.set_xlabel(r"No. turns [T$_0$]")    
+    ax.set_ylabel (r"PL $\phi_{\mathsf{bunch}}$ [rad]")
+    ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
     if time_step > 100000:
         ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 
-    # Save figure
-    fign = dirname + '/PL_bunch_phase.png'
-    plt.savefig(fign)
-    plt.clf()
+    # Output plot
+    if show_plot:
+        plt.show()
+    else:
+        fign = dirname +'/PL_bunch_phase.png'
+        plt.savefig(fign)
+    plt.clf()     
 
 
-def plot_PL_RF_phase(RFStation, h5data, output_freq=1,
-                     dirname='fig'):
+def plot_PL_RF_phase(RFStation, h5data, output_freq = 1, 
+                    dirname = 'fig', show_plot = False):
+    
     """
     Plot of RF phase; monitored with Phase Loop.
     For large amount of data, monitor with larger 'output_freq'.
@@ -121,8 +137,8 @@ def plot_PL_RF_phase(RFStation, h5data, output_freq=1,
     dphi[time_step:] = np.nan
 
     # Plot
-    plt.figure(1, figsize=(8, 6))
-    ax = plt.axes([0.15, 0.1, 0.8, 0.8])
+    plt.figure(1, figsize=(8,6))
+    ax = plt.axes()
     ax.plot(t, dphi, '.')
     ax.set_xlabel(r"No. turns [T$_0$]")
     ax.set_ylabel(r"RF phase $\phi_{\mathsf{RF}}$ [rad]")
@@ -130,14 +146,18 @@ def plot_PL_RF_phase(RFStation, h5data, output_freq=1,
     if time_step > 100000:
         ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 
-    # Save figure
-    fign = dirname + '/RF_phase.png'
-    plt.savefig(fign)
-    plt.clf()
+    # Output plot
+    if show_plot:
+        plt.show()
+    else:
+        fign = dirname +'/RF_phase.png'
+        plt.savefig(fign)
+    plt.clf()     
 
 
-def plot_PL_phase_corr(RFStation, h5data, output_freq=1,
-                       dirname='fig'):
+def plot_PL_phase_corr(RFStation, h5data, output_freq = 1, 
+                       dirname = 'fig', show_plot = False):
+    
     """
     Plot of phase correction applied by the Phase Loop as a function of time.
     For large amount of data, monitor with larger 'output_freq'.
@@ -156,23 +176,27 @@ def plot_PL_phase_corr(RFStation, h5data, output_freq=1,
 
     # Plot
     fig = plt.figure(1)
-    fig.set_size_inches(8, 6)
-    ax = plt.axes([0.15, 0.1, 0.8, 0.8])
-    ax.plot(t, dphi, '.')
-    ax.set_xlabel(r"No. turns [T$_0$]")
-    ax.set_ylabel(r"PL $\phi$ correction [rad]")
-    ax.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+    fig.set_size_inches(8,6)
+    ax = plt.axes()
+    ax.plot(t, dphi,'.')
+    ax.set_xlabel(r"No. turns [T$_0$]")    
+    ax.set_ylabel (r"PL $\phi$ correction [rad]")
+    ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
     if time_step > 100000:
         ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 
-    # Save figure
-    fign = dirname + '/PL_phase_corr.png'
-    plt.savefig(fign)
-    plt.clf()
+    # Output plot
+    if show_plot:
+        plt.show()
+    else:
+        fign = dirname +'/PL_phase_corr.png'
+        plt.savefig(fign)
+    plt.clf()     
 
 
-def plot_PL_RF_freq(RFStation, h5data, output_freq=1,
-                    dirname='fig'):
+def plot_PL_RF_freq(RFStation, h5data, output_freq = 1, 
+                    dirname = 'fig', show_plot = False):
+    
     """
     Plot of RF revolution frequency; monitored with Phase Loop.
     For large amount of data, monitor with larger 'output_freq'.
@@ -190,8 +214,8 @@ def plot_PL_RF_freq(RFStation, h5data, output_freq=1,
     dphi[time_step:] = np.nan
 
     # Plot
-    plt.figure(1, figsize=(8, 6))
-    ax = plt.axes([0.15, 0.1, 0.8, 0.8])
+    plt.figure(1, figsize=(8,6))
+    ax = plt.axes()
     ax.plot(t, dphi, '.')
     ax.set_xlabel(r"No. turns [T$_0$]")
     ax.set_ylabel(r"RF revolution frequency $\omega_{\mathsf{RF}}$ [1/s]")
@@ -199,14 +223,18 @@ def plot_PL_RF_freq(RFStation, h5data, output_freq=1,
     if time_step > 100000:
         ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 
-    # Save figure
-    fign = dirname + '/RF_freq.png'
-    plt.savefig(fign)
-    plt.clf()
+    # Output plot
+    if show_plot:
+        plt.show()
+    else:
+        fign = dirname +'/RF_freq.png'
+        plt.savefig(fign)
+    plt.clf()     
 
 
-def plot_PL_freq_corr(RFStation, h5data, output_freq=1,
-                      dirname='fig'):
+def plot_PL_freq_corr(RFStation, h5data, output_freq = 1, 
+                      dirname = 'fig', show_plot = False):
+    
     """
     Plot of frequency correction applied by the Phase Loop as a function of time.
     For large amount of data, monitor with larger 'output_freq'.
@@ -225,23 +253,27 @@ def plot_PL_freq_corr(RFStation, h5data, output_freq=1,
 
     # Plot
     fig = plt.figure(1)
-    fig.set_size_inches(8, 6)
-    ax = plt.axes([0.15, 0.1, 0.8, 0.8])
-    ax.plot(t, dphi, '.')
-    ax.set_xlabel(r"No. turns [T$_0$]")
-    ax.set_ylabel(r"PL $\omega_{\mathsf{RF}}$ correction [1/s]")
-    ax.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+    fig.set_size_inches(8,6)
+    ax = plt.axes()
+    ax.plot(t, dphi,'.')
+    ax.set_xlabel(r"No. turns [T$_0$]")    
+    ax.set_ylabel (r"PL $\omega_{\mathsf{RF}}$ correction [1/s]")
+    ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
     if time_step > 100000:
         ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 
-    # Save figure
-    fign = dirname + '/PL_freq_corr.png'
-    plt.savefig(fign)
-    plt.clf()
+    # Output plot
+    if show_plot:
+        plt.show()
+    else:
+        fign = dirname +'/PL_freq_corr.png'
+        plt.savefig(fign)
+    plt.clf()     
 
 
-def plot_RF_phase_error(RFStation, h5data, output_freq=1,
-                        dirname='fig'):
+def plot_RF_phase_error(RFStation, h5data, output_freq = 1, 
+                       dirname = 'fig', show_plot = False):
+    
     """
     Plot of accumulated RF phase error; the Synchro Loop can act on this.
     For large amount of data, monitor with larger 'output_freq'.
@@ -259,8 +291,8 @@ def plot_RF_phase_error(RFStation, h5data, output_freq=1,
     dphi[time_step:] = np.nan
 
     # Plot
-    plt.figure(1, figsize=(8, 6))
-    ax = plt.axes([0.15, 0.1, 0.8, 0.8])
+    plt.figure(1, figsize=(8,6))
+    ax = plt.axes()
     ax.plot(t, dphi, '.')
     ax.set_xlabel(r"No. turns [T$_0$]")
     ax.set_ylabel(r"RF phase error $\Delta \phi_{\mathsf{RF}}$ [rad]")
@@ -268,14 +300,19 @@ def plot_RF_phase_error(RFStation, h5data, output_freq=1,
     if time_step > 100000:
         ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 
-    # Save figure
-    fign = dirname + '/RF_phase_error.png'
-    plt.savefig(fign)
-    plt.clf()
-
-
-def plot_RL_radial_error(RFStation, h5data, output_freq=1,
-                         dirname='fig'):
+    # Output plot
+    if show_plot:
+        plt.show()
+    else:
+        fign = dirname +'/RF_phase_error.png'
+        plt.savefig(fign)
+    plt.clf()    
+    
+    
+     
+def plot_RL_radial_error(RFStation, h5data, output_freq = 1, 
+                         dirname = 'fig', show_plot = False):
+    
     """
     Plot of relative radial error; monitored with Phase Loop.
     For large amount of data, monitor with larger 'output_freq'.
@@ -293,8 +330,8 @@ def plot_RL_radial_error(RFStation, h5data, output_freq=1,
     dphi[time_step:] = np.nan
 
     # Plot
-    plt.figure(1, figsize=(8, 6))
-    ax = plt.axes([0.15, 0.1, 0.8, 0.8])
+    plt.figure(1, figsize=(8,6))
+    ax = plt.axes()
     ax.plot(t, dphi, '.')
     ax.set_xlabel(r"No. turns [T$_0$]")
     ax.set_ylabel(r"Relative radial error [1]")
@@ -302,14 +339,18 @@ def plot_RL_radial_error(RFStation, h5data, output_freq=1,
     if time_step > 100000:
         ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 
-    # Save figure
-    fign = dirname + '/RL_radial_error.png'
-    plt.savefig(fign)
-    plt.clf()
+    # Output plot
+    if show_plot:
+        plt.show()
+    else:
+        fign = dirname +'/RL_radial_error.png'
+        plt.savefig(fign)
+    plt.clf()     
+    
+               
 
-
-def plot_COM_motion(Ring, RFStation, h5data,
-                    output_freq=1, dirname='fig'):
+def plot_COM_motion(Ring, RFStation, h5data,  
+                    output_freq = 1, dirname = 'fig', show_plot = False):
     """
     Evolution of bunch C.O.M. in longitudinal phase space. 
     Optional use of histograms and separatrix.
@@ -329,8 +370,8 @@ def plot_COM_motion(Ring, RFStation, h5data,
 
     # Plot
     fig = plt.figure(1)
-    fig.set_size_inches(8, 8)
-    ax = plt.axes([0.15, 0.1, 0.8, 0.8])
+    fig.set_size_inches(8,8)
+    ax = plt.axes()
     ax.scatter(mean_dt, mean_dE, s=5, edgecolor='none')
 
     ax.set_xlabel(r"$\Delta t$ [s]")
@@ -338,17 +379,21 @@ def plot_COM_motion(Ring, RFStation, h5data,
     ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
     ax.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
     ax.set_xlim((-0.7e-6, 0.7e-6))
-    plt.figtext(0.95, 0.95, 'C.O.M. evolution', fontsize=16, ha='right',
-                va='center')
-
-    # Save plot
-    fign = dirname + '/COM_evolution.png'
-    plt.savefig(fign)
+    plt.figtext(0.95, 0.95, 'C.O.M. evolution', fontsize=16, ha='right', 
+                va='center') 
+            
+    # Output plot
+    if show_plot:
+        plt.show()
+    else:
+        fign = dirname +'/COM_evolution.png'
+        plt.savefig(fign)
     plt.clf()
 
 
-def plot_LHCNoiseFB(RFStation, LHCNoiseFB, h5data, output_freq=1,
-                    dirname='fig'):
+def plot_LHCNoiseFB(RFStation, LHCNoiseFB, h5data, output_freq = 1, 
+                    dirname = 'fig', show_plot = False):
+    
     """
     Plot of the phase noise multiplication factor as a function of time.
     For large amount of data, monitor with larger 'output_freq'.
@@ -367,23 +412,28 @@ def plot_LHCNoiseFB(RFStation, LHCNoiseFB, h5data, output_freq=1,
 
     # Plot
     fig = plt.figure(1)
-    fig.set_size_inches(8, 6)
-    ax = plt.axes([0.15, 0.1, 0.8, 0.8])
-    ax.plot(t, x, '.')
-    ax.set_xlabel(r"No. turns [T$_0$]")
-    ax.set_ylabel(r"LHC noise FB scaling factor [1]")
-    ax.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+    fig.set_size_inches(8,6)
+    ax = plt.axes()
+    ax.plot(t, x,'.')
+    ax.set_xlabel(r"No. turns [T$_0$]")    
+    ax.set_ylabel (r"LHC noise FB scaling factor [1]")
+    ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
     if time_step > 100000:
         ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 
-    # Save figure
-    fign = dirname + '/LHC_noise_FB.png'
-    plt.savefig(fign)
-    plt.clf()
+    # Output plot
+    if show_plot:
+        plt.show()
+    else:
+        fign = dirname +'/LHC_noise_FB.png'
+        plt.savefig(fign)
+    plt.clf()         
 
 
-def plot_LHCNoiseFB_FWHM(RFStation, LHCNoiseFB, h5data,
-                         output_freq=1, dirname='fig'):
+    
+def plot_LHCNoiseFB_FWHM(RFStation, LHCNoiseFB, h5data, 
+                         output_freq = 1, dirname = 'fig', show_plot = False):
+    
     """
     Plot of the FWHM bunch length used in LHCNoiseFB as a function of time.
     For large amount of data, monitor with larger 'output_freq'.
@@ -402,22 +452,27 @@ def plot_LHCNoiseFB_FWHM(RFStation, LHCNoiseFB, h5data,
 
     # Plot
     fig = plt.figure(1)
-    fig.set_size_inches(8, 6)
-    ax = plt.axes([0.15, 0.1, 0.8, 0.8])
-    ax.plot(t, x, '.')
-    ax.set_xlabel(r"No. turns [T$_0$]")
-    ax.set_ylabel(r"4-sigma FWHM bunch length [s]")
+    fig.set_size_inches(8,6)
+    ax = plt.axes()
+    ax.plot(t, x,'.')
+    ax.set_xlabel(r"No. turns [T$_0$]")    
+    ax.set_ylabel (r"4-sigma FWHM bunch length [s]")
     if time_step > 100000:
         ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 
-    # Save figure
-    fign = dirname + '/LHC_noise_FB_bl.png'
-    plt.savefig(fign)
-    plt.clf()
+    # Output plot
+    if show_plot:
+        plt.show()
+    else:
+        fign = dirname +'/LHC_noise_FB_bl.png'
+        plt.savefig(fign)
+    plt.clf()         
 
 
-def plot_LHCNoiseFB_FWHM_bbb(RFStation, LHCNoiseFB, h5data,
-                             output_freq=1, dirname='fig'):
+
+def plot_LHCNoiseFB_FWHM_bbb(RFStation, LHCNoiseFB, h5data, 
+                             output_freq = 1, dirname = 'fig', show_plot = False):
+    
     """
     Plot of bunch-by-bunch FWHM bunch length used in LHCNoiseFB as a function 
     of time. For large amount of data, monitor with larger 'output_freq'.
@@ -437,8 +492,8 @@ def plot_LHCNoiseFB_FWHM_bbb(RFStation, LHCNoiseFB, h5data,
 
     # Plot
     fig = plt.figure(1)
-    fig.set_size_inches(8, 6)
-    ax = plt.axes([0.15, 0.1, 0.8, 0.8])
+    fig.set_size_inches(8,6)
+    ax = plt.axes()
     for i in range(nbunches):
         ax.plot(t, x[:, i], '.', color=cm.get_cmap('jet')(i / nbunches), label="Bunch %d" % i)
     ax.set_xlabel(r"No. turns [T$_0$]")
@@ -447,7 +502,10 @@ def plot_LHCNoiseFB_FWHM_bbb(RFStation, LHCNoiseFB, h5data,
         ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
     ax.legend()
 
-    # Save figure
-    fign = dirname + '/LHC_noise_FB_bl_bbb.png'
-    plt.savefig(fign)
-    plt.clf()
+    # Output plot
+    if show_plot:
+        plt.show()
+    else:
+        fign = dirname +'/LHC_noise_FB_bl_bbb.png'
+        plt.savefig(fign)
+    plt.clf()         
