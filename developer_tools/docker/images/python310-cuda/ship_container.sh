fb67f820cc5a22fed7c368309926068621fa3f5d
slauber <simon.fabian.lauber@cern.ch>
2024-11-01 14:09:08 +0100

    Fixed Dockerfile to have no admin privileges, fixed CI to not require admin privileges

--- developer_tools/docker/images/python310-cuda/ship_container.sh
+++ developer_tools/docker/images/python310-cuda/ship_container.sh
@@ -3,25 +3,11 @@
 # Exit immediately if a command fails
 set -e
 
-# Login to docker if not already
-# docker login gitlab-registry.cern.ch  # must have been executed already in order to work
-
 # Define variables
 IMAGE_NAME="gitlab-registry.cern.ch/blond/blond/python310"
 TAG=${1:-latest}
-#############################
-# Builds the container
-#############################
-# requires read api token !
-# 1. obtained token at https://gitlab.cern.ch/-/user_settings/personal_access_tokens
-# 2. export GITLAB_READ_TOKEN=<YOUR-TOKEN>
-# 3. Build your docker container
-echo "Building Docker image: $IMAGE_NAME:$TAG"
-docker build -t "$IMAGE_NAME:$TAG" . --secret id=gitlab_token,src=<(echo $GITLAB_READ_TOKEN)
-#############################
+
 
 # Push the container to gitlab
 echo "Pushing Docker image to: $IMAGE_NAME:$TAG"
 docker push "$IMAGE_NAME:$TAG"
-
-echo "Docker image $IMAGE_NAME:$TAG successfully pushed."
--- developer_tools/docker/images/python310-cuda/test_container.sh
+++ developer_tools/docker/images/python310-cuda/test_container.sh
@@ -3,10 +3,12 @@
 # Exit immediately if a command fails
 set -e
 
-# builds docker-image
-# requires read api token !
-# 1. obtained token at https://gitlab.cern.ch/-/user_settings/personal_access_tokens
-# 2. export GITLAB_READ_TOKEN=<YOUR-TOKEN>
-# 3. Build your docker container
-docker build -t python310-cuda-image . --secret id=gitlab_token,src=<(echo $GITLAB_READ_TOKEN)
-docker run --rm -it python310-cuda-image # opens shell to interactively work with the container
+# Login to docker if not already
+# docker login gitlab-registry.cern.ch  # must have been executed already in order to work
+
+# Define variables
+IMAGE_NAME="gitlab-registry.cern.ch/blond/blond/python310"
+TAG=${1:-latest}
+
+echo "Starting container locally: $IMAGE_NAME:$TAG"
+docker run -it "$IMAGE_NAME:$TAG" # opens shell to interactively work with the container
