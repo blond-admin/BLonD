76b4b78b4aa6e4a48b437e1863ba4f8c702f812e
Simon Albright <simon.albright@cern.ch>
2024-11-06 17:17:08 +0100

    updating test

--- unittests/beams/test_beam_object.py
+++ unittests/beams/test_beam_object.py
@@ -290,7 +290,7 @@ class testBeamClass(unittest.TestCase):
 
         self.assertEqual(self.beam.id[2100000:2100100].tolist(), [0] * 100,
                          msg="particle ids not applied correctly")
-        
+
         self.assertEqual(self.beam.id[-1], 2100200,
                          msg='particle ids not applied correctly')
 
@@ -328,6 +328,37 @@ class testBeamClass(unittest.TestCase):
 
         with self.assertRaises(TypeError, msg='Wrong type should raise exception'):
             self.beam.add_beam(([1], [2]))
+
+    def test_beam_info_setting(self):
+
+        with self.assertRaises(ValueError):
+            self.beam._set_beam_info()
+            self.beam._set_beam_info(None, 1, 1)
+
+    # def test_ratio_property(self):
+    #     beam1 = Beam(Ring=self.general_params, n_macroparticles=1, intensity=1)
+    #     self.assertEqual(beam1.ratio, 1)
+
+    # def test_ratio_setter_ratio(self):
+    #     beam1 = Beam(Ring=self.general_params, n_macroparticles=1, intensity=1)
+    #     beam1.ratio = 2  # rat
+    #     self.assertEqual(2.0, beam1.ratio)
+    #     self.assertEqual(1.0, beam1.n_macroparticles)
+    #     self.assertEqual(2.0, beam1.intensity)
+
+    # def test_ratio_setter_intensity(self):
+    #     beam1 = Beam(Ring=self.general_params, n_macroparticles=1, intensity=1)
+    #     beam1.intensity = 2  # rat
+    #     self.assertEqual(2,0, beam1.ratio)
+    #     self.assertEqual(1.0, beam1.n_macroparticles)
+    #     self.assertEqual(2.0, beam1.intensity)
+
+    # def test_ratio_setter_n_macroparticles(self):
+    #     beam1 = Beam(Ring=self.general_params, n_macroparticles=1, intensity=1)
+    #     beam1.n_macroparticles = 2  # rat
+    #     self.assertEqual(0.5, beam1.ratio)
+    #     self.assertEqual(2.0, beam1.n_macroparticles)
+    #     self.assertEqual(1.0, beam1.intensity, 1)
 
 
 if __name__ == '__main__':
