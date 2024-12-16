7b75a5cb0f68e3300f64c2c32f17a54fe9d11027
Elleanor Rose Lamb <elamb@pcsy107.dyndns.cern.ch>
2024-12-16 14:29:00 +0100

    remove parameter table from test folder

--- unittests/impedances/test_impedance.py
+++ unittests/impedances/test_impedance.py
@@ -123,7 +123,7 @@ class  BaseTestInducedVoltageResonator(unittest.TestCase):
 
     def test_init(self):
         # TODO Improve testcases
-        
+    
         ivr = InducedVoltageResonator(beam=self.beam, profile=self.profile, resonators=self.resonator)
 
     def test_init_mtw(self):
@@ -133,6 +133,7 @@ class  BaseTestInducedVoltageResonator(unittest.TestCase):
                                       )
         
     def test_mtw_false_induced_volt(self):
+        # TODO Improve testcases
         ivr = InducedVoltageResonator(beam=self.beam, profile=self.profile, resonators=self.resonator,
                                       multi_turn_wake=False)
     
@@ -143,7 +144,7 @@ class  BaseTestInducedVoltageResonator(unittest.TestCase):
 
 
     def test_multi_rf_station(self):
-    
+        # TODO Improve testcases
         ivr = InducedVoltageResonator(beam=self.beam, profile=self.profile, resonators=self.resonator, 
                                       rf_station=self.rf_station, multi_turn_wake=False)
         ivr.process()
@@ -165,18 +166,9 @@ class  BaseTestInducedVoltageResonator(unittest.TestCase):
 
 
 
-        
-
-                                      
-                        
-
-
-
-
 class TestInducedVoltageResonatorRf1(BaseTestInducedVoltageResonator, unittest.TestCase):
 
     def setUp(self):
-        import os
         import numpy as np
         from blond.beam.beam import Beam, Proton
         from blond.beam.distributions import bigaussian
@@ -185,7 +177,6 @@ class TestInducedVoltageResonatorRf1(BaseTestInducedVoltageResonator, unittest.T
         from blond.input_parameters.rf_parameters import RFStation
         from blond.input_parameters.ring import Ring
         import math 
-        this_directory = os.path.dirname(os.path.realpath(__file__)) + '/'
         
         gamma_transition = 1 / np.sqrt(0.00192)  
         mom_compaction = 1 / gamma_transition ** 2
@@ -223,10 +214,32 @@ class TestInducedVoltageResonatorRf1(BaseTestInducedVoltageResonator, unittest.T
                                  rf_station=self.rf_station, cuts_unit='rad')
         self.profile = Profile(beam, cut_options,
                                FitOptions(fit_option='gaussian'))
-        table = np.loadtxt(this_directory + './EX_05_new_HQ_table.dat', comments='!')
-        self.R_shunt = table[:, 2] * 10 ** 6
-        self.f_res = table[:, 0] * 10 ** 9
-        self.Q_factor = table[:, 1]
+        self.R_shunt = np.array([3.88e+05, 6.00e+02, 6.98e+04, 8.72e+04, 2.04e+04, 1.09e+05,
+       9.10e+03, 1.80e+03, 7.50e+03, 2.60e+03, 2.80e+04, 2.90e+04,
+       1.20e+05, 7.90e+04, 4.80e+04, 1.45e+04, 5.00e+03, 1.05e+04,
+       1.25e+04, 1.00e+04, 4.30e+04, 4.50e+04, 1.15e+04, 4.50e+03,
+       6.90e+03, 1.54e+04, 1.10e+04, 4.70e+03, 8.50e+03, 1.70e+04,
+       5.00e+03, 6.33e+05, 4.99e+05, 3.64e+05, 1.77e+05, 6.14e+04,
+       2.99e+04, 3.79e+05, 2.43e+05, 1.74e+04, 5.88e+05, 6.10e+04,
+       7.71e+05, 1.87e+05, 8.14e+05, 2.81e+05, 1.34e+05, 4.20e+04,
+       6.80e+04, 5.20e+04, 4.50e+04, 4.10e+04])
+        self.f_res = np.array([6.290e+08, 8.400e+08, 1.066e+09, 1.076e+09, 1.608e+09, 1.884e+09,
+       2.218e+09, 2.533e+09, 2.547e+09, 2.782e+09, 3.008e+09, 3.223e+09,
+       3.284e+09, 3.463e+09, 3.643e+09, 3.761e+09, 3.900e+09, 4.000e+09,
+       4.080e+09, 4.210e+09, 1.076e+09, 1.100e+09, 1.955e+09, 2.075e+09,
+       2.118e+09, 2.199e+09, 2.576e+09, 2.751e+09, 3.370e+09, 5.817e+09,
+       5.817e+09, 1.210e+09, 1.280e+09, 1.415e+09, 1.415e+09, 1.415e+09,
+       1.415e+09, 1.395e+09, 1.401e+09, 1.570e+09, 1.610e+09, 1.620e+09,
+       1.861e+09, 1.890e+09, 2.495e+09, 6.960e+08, 9.100e+08, 1.069e+09,
+       1.078e+09, 1.155e+09, 1.232e+09, 1.343e+09])
+        self.Q_factor = np.array([ 500.,   10.,  500.,  500.,   40.,  500.,   15.,  384.,  340.,
+         20.,  450.,  512.,  600.,  805., 1040.,  965.,   50., 1300.,
+        600.,  200.,  700.,  700.,  450.,  600.,  600.,  750., 1000.,
+        500.,   30., 1000.,   10.,  315.,  200.,   75.,  270.,   75.,
+        270.,  200., 1100.,   55.,  980.,   60.,  810.,  175., 1190.,
+       7400., 8415., 7980., 7810., 6660., 5870., 7820.])
+        
+
         self.resonator = Resonators(self.R_shunt, self.f_res, self.Q_factor)
 
 
@@ -244,7 +257,6 @@ class TestInducedVoltageResonatorRf2(BaseTestInducedVoltageResonator, unittest.T
         from blond.input_parameters.rf_parameters import RFStation
         from blond.input_parameters.ring import Ring
         import math 
-        this_directory = os.path.dirname(os.path.realpath(__file__)) + '/'
         
         gamma_transition = 1 / np.sqrt(0.00192)  
         mom_compaction = 1 / gamma_transition ** 2
@@ -286,10 +298,30 @@ class TestInducedVoltageResonatorRf2(BaseTestInducedVoltageResonator, unittest.T
                                  rf_station=self.rf_station, cuts_unit='rad')
         self.profile = Profile(beam, cut_options,
                                FitOptions(fit_option='gaussian'))
-        table = np.loadtxt(this_directory + './EX_05_new_HQ_table.dat', comments='!')
-        self.R_shunt = table[:, 2] * 10 ** 6
-        self.f_res = table[:, 0] * 10 ** 9
-        self.Q_factor = table[:, 1]
+        self.R_shunt = np.array([3.88e+05, 6.00e+02, 6.98e+04, 8.72e+04, 2.04e+04, 1.09e+05,
+       9.10e+03, 1.80e+03, 7.50e+03, 2.60e+03, 2.80e+04, 2.90e+04,
+       1.20e+05, 7.90e+04, 4.80e+04, 1.45e+04, 5.00e+03, 1.05e+04,
+       1.25e+04, 1.00e+04, 4.30e+04, 4.50e+04, 1.15e+04, 4.50e+03,
+       6.90e+03, 1.54e+04, 1.10e+04, 4.70e+03, 8.50e+03, 1.70e+04,
+       5.00e+03, 6.33e+05, 4.99e+05, 3.64e+05, 1.77e+05, 6.14e+04,
+       2.99e+04, 3.79e+05, 2.43e+05, 1.74e+04, 5.88e+05, 6.10e+04,
+       7.71e+05, 1.87e+05, 8.14e+05, 2.81e+05, 1.34e+05, 4.20e+04,
+       6.80e+04, 5.20e+04, 4.50e+04, 4.10e+04])
+        self.f_res = np.array([6.290e+08, 8.400e+08, 1.066e+09, 1.076e+09, 1.608e+09, 1.884e+09,
+       2.218e+09, 2.533e+09, 2.547e+09, 2.782e+09, 3.008e+09, 3.223e+09,
+       3.284e+09, 3.463e+09, 3.643e+09, 3.761e+09, 3.900e+09, 4.000e+09,
+       4.080e+09, 4.210e+09, 1.076e+09, 1.100e+09, 1.955e+09, 2.075e+09,
+       2.118e+09, 2.199e+09, 2.576e+09, 2.751e+09, 3.370e+09, 5.817e+09,
+       5.817e+09, 1.210e+09, 1.280e+09, 1.415e+09, 1.415e+09, 1.415e+09,
+       1.415e+09, 1.395e+09, 1.401e+09, 1.570e+09, 1.610e+09, 1.620e+09,
+       1.861e+09, 1.890e+09, 2.495e+09, 6.960e+08, 9.100e+08, 1.069e+09,
+       1.078e+09, 1.155e+09, 1.232e+09, 1.343e+09])
+        self.Q_factor = np.array([ 500.,   10.,  500.,  500.,   40.,  500.,   15.,  384.,  340.,
+         20.,  450.,  512.,  600.,  805., 1040.,  965.,   50., 1300.,
+        600.,  200.,  700.,  700.,  450.,  600.,  600.,  750., 1000.,
+        500.,   30., 1000.,   10.,  315.,  200.,   75.,  270.,   75.,
+        270.,  200., 1100.,   55.,  980.,   60.,  810.,  175., 1190.,
+       7400., 8415., 7980., 7810., 6660., 5870., 7820.])
         self.resonator = Resonators(self.R_shunt, self.f_res, self.Q_factor)
              
  
