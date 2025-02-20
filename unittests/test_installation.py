76469ffad80da54d2db058543759f5b3580d694b
slauber <simon.fabian.lauber@cern.ch>
2025-02-20 13:17:25 +0100

    Added reviewer suggestions

--- unittests/test_installation.py
+++ unittests/test_installation.py
@@ -1,49 +1,41 @@
-import os
 import pathlib
-import shutil
 import subprocess
 import sys
 import unittest
-import blond
 
 this_directory = pathlib.Path(__file__).parent.resolve()
-blond_path = pathlib.Path(blond.__file__).parent.resolve()
-
 
 
 def execute_command(commands):
-    print(commands)
     command = f"bash -i -c '{'; '.join(commands)}'"
-    prc = subprocess.Popen(
+    print(f"Running command in subprocess: {command}")
+    # Use subprocess.run to execute the command
+    result = subprocess.run(
         command,
         cwd=this_directory,
         stdout=sys.stdout,
         stderr=sys.stderr,
         shell=True
     )
-    prc.communicate()
-    assert prc.returncode == 0, f"{prc.returncode=}"
 
+    # This will raise a CalledProcessError if the command failed (returns non-zero returncode).
+    # If you actually want a specific string returned when failing, using assert as you did before also works fine.
+    result.check_returncode()
 
 
 venv_tmp = "venv_tmp"
 
 
 class TestInstallation(unittest.TestCase):
-    def test_setup(self):
-        commands = ["pip install --upgrade pip", f'python -m venv {venv_tmp}']
-        execute_command(commands)
-
-        commands = [f"source {venv_tmp}/bin/activate",  # activate virtual environment to install fresh blond
+    def test_installation_in_venv(self):
+        """Integration test: run pip install in a temporary directory and ensure it works."""
+        commands = ["pip install --upgrade pip",
+                    f'python -m venv {venv_tmp}',
+                    f"source {venv_tmp}/bin/activate",  # activate virtual environment to install fresh blond
                     f"cd ..",  # # change to BLonD folder
-                    "pip install .[all]"  # run installation of BLonD
+                    "pip install .[all]",  # run installation of BLonD
+                    'python -c "from blond import test; test()"',
                     ]
-        execute_command(commands)
-
-        commands = [
-            f"source {venv_tmp}/bin/activate",  # activate virtual environment to install fresh blond
-            'python -c "from blond import test; test()"',
-        ]
         execute_command(commands)
 
     def tearDown(self):
