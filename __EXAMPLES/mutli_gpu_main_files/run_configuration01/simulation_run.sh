b48462013e197c2121c6f11f389eeaf6fd298f09
slauber <simon.fabian.lauber@cern.ch>
2025-03-10 11:27:54 +0100

    Minor fix

--- __EXAMPLES/mutli_gpu_main_files/run_configuration01/simulation_run.sh
+++ __EXAMPLES/mutli_gpu_main_files/run_configuration01/simulation_run.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 # Glue code to connect a python file with HTCondor arguments and point to virtual environment
 
-# Calculate the absolute path of the script's directory
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-
 # Activate the virtual environment
-source "/afs/cern.ch/work/s/slauber/SimulationProjects/blond/.venv/bin/activate"
+source "/afs/cern.ch/work/s/slauber/SimulationProjects/BLonD/.venv/bin/activate"
 
 # Runs simulation ./blond_simulation_template/main
 # You might want to adapt these lines
-python3 "/afs/cern.ch/work/s/slauber/SimulationProjects/blond/EX_01_Acceleration.py"
+python3 "/afs/cern.ch/work/s/slauber/SimulationProjects/BLonD/EX_01_Acceleration.py"
 
