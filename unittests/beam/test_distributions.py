1e2b717327098ac6ca477cd257d5477a768f791e
lvalle <lina.valle@cern.ch>
2025-04-09 09:53:38 +0200

    Added test files for the Haissinki distribution function

---
+++ unittests/beam/test_distributions.py
@@ -0,0 +1,12 @@
+import unittest
+
+from blond.beam.distributions import haissinki_distribution
+
+
+class TestHaissinkiDistribution(unittest.TestCase):
+    def test_something(self):
+        haissinki_distribution()
+
+
+if __name__ == '__main__':
+    unittest.main()
