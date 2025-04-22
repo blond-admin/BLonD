1df7fcce24815509dbd7cb27c547564e6adcc82f
lvalle <lina.valle@cern.ch>
2025-04-22 10:10:38 +0200

    animated tracking being written

--- simu_acceleration/plots_theory.py
+++ simu_acceleration/plots_theory.py
@@ -4,6 +4,7 @@ from scipy.constants import c, e
 from blond.trackers.utilities import hamiltonian
 import matplotlib.animation as ani
 import matplotlib.pyplot as plt
+import time
 
 
 def get_hamiltonian(ring, rfstation, beam, X, Y, k = int(0)):
@@ -65,6 +66,8 @@ def plot_hamiltonian(ring, rfstation, beam, dt, dE, k = int(0), hamiltonian_ener
 def animated_plot_tracking(ring, rfcav, beam, map_, dt, dE, n_points = 1001, saving_file='animated_tracking_Z_mode_electrons', option = ''):
     global C, scat
     fig, ax = plt.subplots()
+    props = dict(boxstyle='round', facecolor='wheat')
+    timelabel = ax.text(0.9, 0.9, "", transform=ax.transAxes, ha="right", bbox=props)
     ax.set_xlim([-dt*1e9, dt*1e9/2])
     ax.set_ylim([-2.0, 2.0])
     scat = ax.scatter(-beam.dt*1e9,beam.dE/1e9, s = 0.2, color = 'blue')
@@ -74,22 +77,33 @@ def animated_plot_tracking(ring, rfcav, beam, map_, dt, dE, n_points = 1001, sav
     Z, hamiltonian_energy = get_hamiltonian(ring, rfcav, beam, X, Y, k=0)
     C = ax.contour(X*1e9, Y/1e9, Z, [hamiltonian_energy], colors='red')
 
+    hE_t = lambda DE, i: c * np.pi / (ring.ring_circumference * beam.beta * beam.energy) * \
+                      (1 / 2 * ring.eta_0[0, i] * DE ** 2 + 1 / 3 * ring.eta_1[0, i] * DE ** 3 + 1 / 4 *
+                       ring.eta_2[0, i] * DE ** 4)
+
+    hphi_DE = lambda phi, i: c * beam.beta * ring.Particle.charge * rfcav.voltage[0, i] / (
+            rfcav.harmonic[0, i] * ring.ring_circumference) * \
+                          (np.cos(phi) - np.cos(rfcav.phi_s[i]) + (phi - rfcav.phi_s[i]) * np.sin(
+                              rfcav.phi_s[i]))
+    t = np.ones(ring.n_turns+1) * time.time()
     def animate(i, ax):
         for m in map_:
             m.track()
-        beam.statistics()
-        if (i%10)==0:
-            for coll in ax.collections:
-                coll.remove()
-            Z, hamiltonian_energy = get_hamiltonian(ring, rfcav, beam, X, Y, k=i)
-            ax.scatter(-beam.dt * 1e9, beam.dE / 1e9, s=0.2, color='cyan')
-            ax.contour(X*1e9, Y/1e9, Z, [hamiltonian_energy], colors='red')
-            #scat.set_offsets(np.column_stack([-beam.dt*1e9, beam.dE/1e9]))
-            #ax.scatter(-beam.dt * 1e9, beam.dE / 1e9, s=0.2, color = 'blue')
-            fig.suptitle(f'Turn: {i}')
+        for coll in ax.collections:
+            coll.remove()
+        Xt = rfcav.omega_rf[0, i] * X + rfcav.phi_rf_d[0, i]
+        Z = hphi_DE(Xt,i) + hE_t(Y,i)
+        ax.scatter(-beam.dt * 1e9, beam.dE / 1e9, s=0.2, color='cyan')
+        ax.contour(X*1e9, Y/1e9, Z, [hphi_DE(np.pi - rfcav.phi_s[i],i)], colors='red')
+        #scat.set_offsets(np.column_stack([-beam.dt*1e9, beam.dE/1e9]))
+        #ax.scatter(-beam.dt * 1e9, beam.dE / 1e9, s=0.2, color = 'blue')
+        t[1:] = t[0:-1]
+        t[0] = time.time()
+        fig.suptitle(f'Turn: {i}')
+        timelabel.set_text("{:.3f} fps".format(-1. / np.diff(t).mean()))
         return ax
 
-    animated_fig = ani.FuncAnimation(fig, animate, fargs = {ax},repeat=True, frames = ring.n_turns+1, interval = ring.t_rev[0]*1e3/10)
+    animated_fig = ani.FuncAnimation(fig, animate, fargs = {ax},repeat=True, frames = int(np.floor(ring.n_turns/10)), interval = 10)
     plt.show()
     writer = ani.PillowWriter(fps=15,
                                      metadata=dict(artist='Me'),
