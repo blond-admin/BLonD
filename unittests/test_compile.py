3b18e078de3dfec7c9eaea9243f6bc536cf94d27
slauber <simon.fabian.lauber@cern.ch>
2025-06-02 18:24:01 +0200

    Added test of compile.py

--- unittests/test_compile.py
+++ unittests/test_compile.py
@@ -1,29 +1,46 @@
+import os
 import unittest
+from pathlib import Path
+from unittest.mock import patch, MagicMock
 
-from blond.compile import compile_cpp_library, compile_cuda_library, main, run_compile
+from blond.compile import main
+
+this_directory = Path(os.path.dirname(os.path.realpath(__file__)))
+blond_project_dir = this_directory.parent
+print(f"{blond_project_dir=}")
 
 
 class TestFunctions(unittest.TestCase):
-    @unittest.skip
-    def test_compile_cpp_library(self):
-        # TODO: implement test for `compile_cpp_library`
-        compile_cpp_library(
-            args=None, cflags=None, float_flags=None, libs=None, cpp_files=None
-        )
-
-    @unittest.skip
-    def test_compile_cuda_library(self):
-        # TODO: implement test for `compile_cuda_library`
-        compile_cuda_library(
-            args=None, nvccflags=None, float_flags=None, cuda_files=None, nvcc=None
-        )
-
-    @unittest.skip
     def test_main(self):
-        # TODO: implement test for `main`
-        main()
+        """--parallel - -optimize - -gpu"""
+        # mock argparse so that it behaves like it
+        # was called by cli
+        mock_args = MagicMock()
+        options = {
+            "parallel": True,
+            "boost": None,
+            "compiler": "g++",
+            "with_fftw": False,
+            "with_fftw_threads": False,
+            "with_fftw_omp": False,
+            "with_fftw_lib": None,
+            "with_fftw_header": None,
+            "flags": "",
+            "libs": "",
+            "libname": str(blond_project_dir / "blond/cpp_routines/libblond"),
+            "optimize": True,
+            "no_cpp": False,
+            "gpu": "discover",
+            "cuda_libname": str(blond_project_dir / "/blond/gpu/kernels"),
+        }
+        for key, value in options.items():
+            mock_args.__setattr__(key, value)
 
-    @unittest.skip
-    def test_run_compile(self):
-        # TODO: implement test for `run_compile`
-        run_compile(command=None, libname=None)
+        with patch(
+            "blond.compile.argparse.ArgumentParser.parse_args", return_value=mock_args
+        ):
+            main()
+
+
+if __name__ == "__main__":
+    unittest.main()
