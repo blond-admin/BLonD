e1485079b8a5a994acee4cb0e6607161088976d1
Simon <simon.albright@cern.ch>
2025-06-11 11:53:35 +0200

    Change BarrierBucket class to ArbitraryWaveform class
    Removed the barrier calculation features, which are better suited to the
    BarrierGenerator class.  Purpose is to simplify application of completely
    arbitrary waveforms to the beam.  The `track` method interpolates the
    given waveform at the beam coordinates and modifies the particle energy
    accordingly.

--- blond/llrf/barrier_bucket.py
+++ blond/llrf/barrier_bucket.py
@@ -28,57 +28,19 @@ if TYPE_CHECKING:
     from blond.beam.beam import Beam
 
 
-class BarrierBucket:
+class ArbitraryWaveform:
 
-    def __init__(self, t_center: float | Iterable[float],
-                 t_width: float | Iterable[float],
-                 peak: float | Iterable[float],
-                 beam: Beam, bin_centers: NDArray):
+    def __init__(self, beam: Beam):
 
-        self.t_center = t_center
-        self._cent_is_iter = hasattr(t_center, "__iter__")
-        self.t_width = t_width
-        self._width_is_iter = hasattr(t_width, "__iter__")
-        self.peak = peak
-        self._peak_is_iter = hasattr(peak, "__iter__")
+        self.waveform = bm.array([[0], [0]])
 
         self._beam = beam
-        self._bin_centers = bin_centers.copy()
-        self._barrier_waveform = bm.zeros_like(self._bin_centers)
-
-    def compute_barrier(self, turn: int):
-
-        if self._cent_is_iter:
-            cent = self.t_center[turn]
-        else:
-            cent = self.t_center
-
-        if self._width_is_iter:
-            width = self.t_width[turn]
-        else:
-            width = self.t_width
-
-        if self._peak_is_iter:
-            peak = self.peak[turn]
-        else:
-            peak = self.peak
-
-        self._barrier_waveform *= 0
-
-        low_bin = bm.where(self._bin_centers >= cent-width/2)[0][0]
-        high_bin = bm.where(self._bin_centers >= cent+width/2)[0][0]
-
-        b_time = self._bin_centers[low_bin:high_bin] - self._bin_centers[low_bin]
-
-        barrier = peak * bm.sin(2*np.pi * b_time/self.t_width)
-
-        self._barrier_waveform[low_bin:high_bin] = barrier
-
 
     def track(self):
 
-        self._beam.dE += bm.interp(self._beam.dt, self._bin_centers,
-                                   self._barrier_waveform)
+        self._beam.dE += (self._beam.Particle.charge
+                          * bm.interp(self._beam.dt, self.waveform[0],
+                                      self.waveform[1]))
 
     def to_gpu(self, recursive: bool = True):
 
