377772740da8174801d79afc46dde8dfc7ea1874
lvalle <lina.valle@cern.ch>
2025-07-15 17:29:49 +0200

    SR module updated

--- blond3/physics/synchrotron_radiation.py
+++ blond3/physics/synchrotron_radiation.py
@@ -142,7 +142,7 @@ class WigglerMagnet(BeamPhysicsRelevant):
                                 * self.I3 / (self.jz * self.I2))
 
         # check validity
-        beam.write_partial_dE() += (- U0_wiggler - 2.0 / tau_z  * beam._dE - 2.0 *
+        beam.write_partial_dE += (- U0_wiggler - 2.0 / tau_z  * beam._dE - 2.0 *
                       self.sigma_dE /
                               np.sqrt(self.tau_z * self.n_kicks)
                               * self.beam.energy * np.random.normal
@@ -154,8 +154,7 @@ class WigglerMagnet(BeamPhysicsRelevant):
             self.update_beam_energy(beam)
         pass
 
-
-class SynchrotronRadiation(BeamPhysicsRelevant):
+class SynchrotronRadiation(BeamPhysicsRelevant, section_i=None):
     def __init__(
         self,
         section_index: int = 0,
@@ -166,9 +165,8 @@ class SynchrotronRadiation(BeamPhysicsRelevant):
             section_index=section_index, name=name,
         )
         self.is_isomagnetic = is_isomagnetic
-        raise NotImplementedError("For Lina")
+        self.synchrotron_radiation_integrals = self.init_synchrotron_radiation_integrals()
         self._simulation: LateInit[Simulation] = None
-        self.energy_loss_per_turn : LateInit[NumpyArray | CupyArray] = None
         self.longitudinal_damping_time : LateInit[NumpyArray | CupyArray] = None
         self.natural_energy_spread : LateInit[NumpyArray | CupyArray] = None
 
@@ -185,11 +183,16 @@ class SynchrotronRadiation(BeamPhysicsRelevant):
         if self.is_isomagnetic:
             bending_radius = self._simulation.ring.bending_radius
             self.rho = bending_radius
-            self.I2 = 2.0 * np.pi / self.rho
-            self.I3 = 2.0 * np.pi / self.rho ** 2.0
-            self.I4 = (self.ring.ring_circumference *
+            I1 = (self._simulation.ring._circumference *
+                  self._simulation)
+            I2 = 2.0 * np.pi / self.rho
+            I3 = 2.0 * np.pi / self.rho ** 2.0
+            I4= (self.ring.ring_circumference *
                        self.ring.alpha_0[0, 0] / self.rho ** 2.0)
+            I5 = I1 * I2 # disregarding the effect of quadrupoles
             self.jz = 2.0 + self.I4 / self.I2
+
+        return np.array([I1, I2, I3, I4, I5])
 
 
     def on_init_simulation(self, simulation: Simulation) -> None:
@@ -209,20 +212,9 @@ class SynchrotronRadiation(BeamPhysicsRelevant):
             self.natural_energy_spread = np.empty(n_turns)
         pass
 
-    def generate_children(self, ring, simulation):
-        from ...physics.drifts import DriftBaseClass
-        drifts = ring.get_elements(DriftBaseClass)
 
 
-    @cached_property
-    def energy_loss_per_turn(self, beam:BeamBaseClass) -> float:
-        """Beam reference fraction of speed of light (v/c0) []"""
-        energy_loss_per_turn = (
-            beam.particle_type.sands_radiation_constant *
-            beam.reference_total_energy *4
-            *self.synchrotron_radiation_integrals[1] /
-            self._simulation.ring.circumference)
-        return energy_loss_per_turn
+
 
     def calculate_synchrotron_radiation_parameters(self, beam: BeamBaseClass):
         _DI_wE = self._contribution_to_synchrotron_radiation_integrals_without_energy
@@ -241,19 +233,19 @@ class SynchrotronRadiation(BeamPhysicsRelevant):
         # Ideally: the energy losses should be computed per particle,
         # and not per beam, to closely match the effect of SR onto each
         # particle. To be checked with theory
-        _DI_wE = self._contribution_to_synchrotron_radiation_integrals_without_energy
-
-        U0_wiggler = (beam.particle_type.c_gamma() * _DI_wE[1]
-                      / (2.0 * np.pi)) * (beam. reference_total_energy +
-                                          beam.read_partial_dE())** 4.0 # in
-        # eV per turn
-        jz = 2 + _DI_wE[3]/_DI_wE[1] # longitudinal damping number
-        tau_z = 2.0 / jz * beam._dE / U0_wiggler # longitudinal damping
+        DI_wE = self._contribution_to_synchrotron_radiation_integrals_without_energy
+        particles_total_energy = beam. reference_total_energy + beam.read_partial_dE()
+        energy_lost_from_synchrotron_radiation = beam.particle_type.sands_radiation_constant * DI_wE[1]/ (2.0 *np.pi) * particles_total_energy** 4.0
+        longitudinal_damping_partition_number = 2 + DI_wE[3]/DI_wE[1] #
+        # longitudinal damping number
+        longitudinal_damping_time = (2.0 / longitudinal_damping_partition_number *
+                                     beam.read_partial_dE()/energy_lost_from_synchrotron_radiation) #
+        # longitudinal damping
         # time in turns #
 
 
         # sigma_dE0 = np.sqrt(beam.particle_type.c_q() * (beam._dE/m_e)**2.0 *
-        #                     _DI_wE[2] / (jz * _DI_wE[1]))
+        #                     DI_wE[2] / (jz * DI_wE[1]))
         # # check validity
         # beam._dE += - U0_wiggler # energy lost through the damping wiggler
         #             - 2.0 / tau_z  * beam._dE
@@ -264,3 +256,77 @@ class SynchrotronRadiation(BeamPhysicsRelevant):
 
     # def track(self, beam: BeamBaseClass) -> None:
     #     pass
+    @energy_loss_per_turn.setter
+    def energy_loss_per_turn(self, value):
+        self._energy_loss_per_turn = value
+
+
+class SynchrotronRadiationMaster(BeamPhysicsRelevant):
+    """ Master class for handling synchrotron radiation along the ring.
+    To be described better #fixme
+    """
+    def __init__(
+        self,
+        section_index: int = 0,
+        name: Optional[str] = None,
+        is_isomagnetic: Optional[bool] = False,
+    ):
+        super().__init__(
+            section_index=section_index, name=name,
+        )
+        self.is_isomagnetic = is_isomagnetic
+        self.synchrotron_radiation_integrals = self.init_synchrotron_radiation_integrals()
+        self._simulation: LateInit[Simulation] = None
+        self.longitudinal_damping_time : LateInit[NumpyArray | CupyArray] = None
+        self.natural_energy_spread : LateInit[NumpyArray | CupyArray] = None
+
+    @cached_property
+    def energy_loss_per_turn(self, beam: BeamBaseClass) -> float:
+        """Beam reference fraction of speed of light (v/c0) []"""
+        energy_loss_per_turn = (
+                beam.particle_type.sands_radiation_constant *
+                beam.reference_total_energy * 4
+                * self.synchrotron_radiation_integrals[1] /
+                self._simulation.ring.circumference)
+        return energy_loss_per_turn
+
+    @cached_property
+    def damping_times(self, beam: BeamBaseClass) -> float:
+        """Beam reference fraction of speed of light (v/c0) []"""
+        energy_loss_per_turn = (
+                beam.particle_type.sands_radiation_constant *
+                beam.reference_total_energy * 4
+                * self.synchrotron_radiation_integrals[1] /
+                self._simulation.ring.circumference)
+        return energy_loss_per_turn
+
+    def on_init_simulation(self, simulation: Simulation) -> None:
+        self._simulation = simulation
+        self.init_synchrotron_radiation_integrals_ring()
+
+    def on_run_simulation(
+        self,
+        simulation: Simulation,
+        n_turns: int,
+        turn_i_init: int,
+    ) -> None:
+        if simulation.get_sr_info_tbt:
+            self.energy_loss_per_turn = np.empty(n_turns)
+            self.longitudinal_damping_time = np.empty(n_turns)
+            self.natural_energy_spread = np.empty(n_turns)
+        pass
+
+    def generate_children(self, element_type = 'drift', location: Optional[str] ='after'):
+        """ Function to generate and insert synchrotron radiation trackers
+        along the ring, either before or after the element type specified."""
+        if element_type == 'drift':
+            from drifts import DriftBaseClass
+            elements_list = self._simulation.ring.get_elements(DriftBaseClass)
+        elif element_type == 'cavity':
+            from cavities import CavityBaseClass
+            elements_list = self._simulation.ring.get_elements(CavityBaseClass)
+
+        # for element in elements_list:
+
+
+
