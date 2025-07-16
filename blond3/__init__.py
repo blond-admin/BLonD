4b186489b1443230cf95be3fdc0262f3f8dfbfed
lvalle <lina.valle@cern.ch>
2025-07-16 10:00:50 +0200

    Updated blond3 init for testing

--- blond3/__init__.py
+++ blond3/__init__.py
@@ -1,5 +1,12 @@
 from ._core.beam.beams import Beam
-from ._core.beam.particle_types import proton, electron, positron
+from ._core.beam.particle_types import (
+    proton,
+    uranium_29,
+    electron,
+    positron,
+    mu_plus,
+    mu_minus,
+)
 from ._core.ring.ring import Ring
 from ._core.simulation.simulation import Simulation
 from .beam_preparation.bigaussian import BiGaussian
@@ -20,7 +27,3 @@ from .physics.impedances.base import WakeField
 from .physics.losses import BoxLosses
 from .physics.profiles import StaticProfile
 from .physics.synchrotron_radiation import SynchrotronRadiationMaster, WigglerMagnet
-from .acc_math.analytic.synchrotron_radiation.synchrotron_radiation_maths import \
-    calculate_natural_energy_spread, calculate_damping_times_in_turn, \
-    calculate_energy_loss_per_turn, \
-    gather_longitudinal_synchrotron_radiation_parameters
