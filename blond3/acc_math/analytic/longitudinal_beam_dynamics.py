f6f5f85ca5c54ddd84efa63f054379a874c7db54
lvalle <lina.valle@cern.ch>
2025-07-18 17:16:12 +0200

    added and updated standard functions for synchrotron radiation parameter calculation + longitudinal beam dynamics functions (to be discussed) with the calculation of the linear synchrotron frequency

--- blond3/acc_math/analytic/longitudinal_beam_dynamics.py
+++ blond3/acc_math/analytic/longitudinal_beam_dynamics.py
@@ -7,13 +7,15 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from numpy.typing import NDArray as NumpyArray
 
-def get_synchrotron_frequency(energy: float | NumpyArray,
-                              voltage: float | NumpyArray,
-                              harmonic_number: float | NumpyArray,
-                              synchronous_phase: float | NumpyArray,
-                              slip_factor :float | NumpyArray,
-                              revolution_frequency: float|NumpyArray,
-                              ):
+
+def get_synchrotron_frequency(
+    energy: float | NumpyArray,
+    voltage: float | NumpyArray,
+    harmonic_number: float | NumpyArray,
+    synchronous_phase: float | NumpyArray,
+    slip_factor: float | NumpyArray,
+    revolution_frequency: float | NumpyArray,
+):
     """
     Calculation of the linear synchrotron angular frequency
 
@@ -31,7 +33,16 @@ def get_synchrotron_frequency(energy: float | NumpyArray,
 
     """
 
-    return np.pi * revolution_frequency * np.sqrt((harmonic_number * e *
-                                                      voltage * np.abs(
-                                             slip_factor * np.cos(synchronous_phase))) / (
-                                                 2 * np.pi * energy))
+    return (
+        np.pi
+        * revolution_frequency
+        * np.sqrt(
+            (
+                harmonic_number
+                * e
+                * voltage
+                * np.abs(slip_factor * np.cos(synchronous_phase))
+            )
+            / (2 * np.pi * energy)
+        )
+    )
