81023e27428c1f7da7e34f73c163d5678937ed56
lvalle <lina.valle@cern.ch>
2025-07-18 16:31:51 +0200

    added and updated standard functions for synchrotron radiation parameter calculation + longitudinal beam dynamics functions (to be discussed) with the calculation of the linear synchrotron frequency

---
+++ blond3/acc_math/analytic/longitudinal_beam_dynamics.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+import numpy as np
+from scipy.constants import e
+from _core.beam.particle_types import ParticleType
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray as NumpyArray
+
+def get_synchrotron_frequency(energy: float | NumpyArray,
+                              voltage: float | NumpyArray,
+                              harmonic_number: float | NumpyArray,
+                              synchronous_phase: float | NumpyArray,
+                              slip_factor :float | NumpyArray,
+                              revolution_frequency: float|NumpyArray,
+                              ):
+    """
+    Calculation of the linear synchrotron angular frequency
+    :param energy:
+    :param voltage:
+    :param synchronous_phase:
+    :param slip_factor:
+    :return:
+    """
+
+    return np.pi * revolution_frequency * np.sqrt((harmonic_number * e *
+                                                      voltage * np.abs(
+                                             slip_factor * np.cos(synchronous_phase))) / (
+                                                 2 * np.pi * energy))
