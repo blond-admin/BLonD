31f45f64c022a0059f977c42b7651c12ca1ecaad
slauber <simon.fabian.lauber@cern.ch>
2025-07-29 14:21:20 +0200

    As discussed with elamb

--- unittests_blond3/examples/test_EX_MuonCollider.py
+++ unittests_blond3/examples/test_EX_MuonCollider.py
@@ -1,7 +1,6 @@
 import unittest
 
 
-@unittest.skip("Not Implemented")
 class TestFunctions(unittest.TestCase):
     def test_my_callback(self):
         from blond3.examples import EX_MuonCollider  # NOQA will run the
