8c120d31450b98cd440b999e76244616595225d2
lthiele <leonard.thiele@cern.ch>
2025-08-20 18:58:18 +0200

    agreement with IndVoltTime is given, IndVoltFreq under certain conditions and IndVoltRes gives different results

--- unittests_blond3/physics/impedances/compare_with_legacy/test_integration_InducedVoltageResonator.py
+++ unittests_blond3/physics/impedances/compare_with_legacy/test_integration_InducedVoltageResonator.py
@@ -23,11 +23,11 @@ from blond3.physics.impedances.solvers import (
 
 from .test_integration_InducedVoltageFreq import f_res, Q_factor, R_shunt
 
-DEV_PLOT = True
+DEV_PLOT = False
 
 
 class Blond2:
-    def __init__(self):
+    def __init__(self, n_macroparticles=int(1e6), n_slices=256, bunch_length=1e-9 / 4):
         from blond.beam.beam import Beam, Proton
         from blond.beam.distributions import bigaussian
         from blond.beam.profile import CutOptions, Profile
@@ -41,22 +41,24 @@ class Blond2:
         from blond.input_parameters.rf_parameters import RFStation
         from blond.input_parameters.ring import Ring
 
+        self.induced_voltage = []
+
         for solver in (
-            InducedVoltageTime,
-            # InducedVoltageFreq,
-            # InducedVoltageResonator,
+                InducedVoltageTime,
+                # InducedVoltageResonator,
+                # InducedVoltageFreq,
         ):
             ring = Ring(6911.56, 1 / (1 / np.sqrt(0.00192)) ** 2, 25.92e9, Proton(), 10)
             rf_station = RFStation(ring, [4620], [0.9e6], [0.0], 1)
-            beam = Beam(ring, 1e6, 1e10)
-            bigaussian(ring, rf_station, beam, 2e-9 / 4, seed=1)
+            beam = Beam(ring, n_macroparticles, 1e10)
+            bigaussian(ring, rf_station, beam, bunch_length, seed=1)
             self.dt = beam.dt
             self.dE = beam.dE
 
             cut_options = CutOptions(
                 cut_left=0,
                 cut_right=2 * np.pi,
-                n_slices=64,
+                n_slices=n_slices,
                 rf_station=rf_station,
                 cuts_unit="rad",
             )
@@ -64,22 +66,22 @@ class Blond2:
             self.profile = profile
 
             profile.track()
-            R_shunt, f_res, Q_factor = 5e5, 1e9, 10e10
+            # R_shunt, f_res, Q_factor = 5e5, 1e9, 10e10
             resonator = Resonators(R_shunt, f_res, Q_factor)
             self.resonator = resonator
 
             if solver == InducedVoltageTime:
-                ind_volt = InducedVoltageTime(beam, profile, [resonator])
+                ind_volt = InducedVoltageTime(beam, profile, [resonator], )
             elif solver == InducedVoltageFreq:
                 ind_volt = InducedVoltageFreq(beam, profile, [resonator], 1e5)
             elif solver == InducedVoltageResonator:
-                ind_volt = InducedVoltageResonator(beam, profile, resonator)
+                ind_volt = InducedVoltageResonator(beam, profile, resonator, )
             else:
                 raise Exception
             tot_vol = TotalInducedVoltage(beam, profile, [ind_volt])
 
             tot_vol.induced_voltage_sum()
-            self.induced_voltage = tot_vol.induced_voltage
+            self.induced_voltage.append(tot_vol.induced_voltage)
 
             if DEV_PLOT:
                 plt.figure(1)
@@ -94,8 +96,8 @@ class Blond2:
 
 
 class Blond3:
-    def __init__(self):
-        blond2 = Blond2()
+    def __init__(self, n_macroparticles=int(1e6), n_slices=256, bunch_length=1e-9 / 4):
+        blond2 = Blond2(n_macroparticles=n_macroparticles, n_slices=n_slices, bunch_length=bunch_length)
         self.blond2 = blond2
 
         ring = Ring(circumference=6911.56)
@@ -110,7 +112,7 @@ class Blond3:
         cavity1.harmonic = 4620
         drift = DriftSimple()
         drift.transition_gamma = 1 / (1 / np.sqrt(0.00192)) ** 2
-        R_shunt, f_res, Q_factor = 5e5, 1e9, 10e10
+        # R_shunt, f_res, Q_factor = 5e5, 1e9, 10e10
         resonators = Resonators(
             shunt_impedances=R_shunt,
             center_frequencies=f_res,
@@ -152,28 +154,61 @@ class TestBothBlonds(unittest.TestCase):
         self.blond3 = Blond3()
 
     def close_in_norm(self, arr_a, arr_b, rtol=1e-5, atol=1e-8):
+        norm=np.inf
         a = np.asarray(arr_a)
         b = np.asarray(arr_b)
         diff = a - b
-        den = max(np.linalg.norm(a), np.linalg.norm(b), atol / rtol if rtol > 0 else 1.0)
-        return (np.linalg.norm(diff) <= rtol * den + atol * np.sqrt(diff.size))
+        den = max(np.linalg.norm(a, norm), np.linalg.norm(b, norm), atol / rtol if rtol > 0 else 1.0)
+        return (np.linalg.norm(diff, norm) <= rtol * den + atol * np.sqrt(diff.size))
 
-    def test___init__(self):
-        if DEV_PLOT:
-            plt.show()
-        plt.plot(self.blond3.blond2.induced_voltage)
-        plt.plot(self.blond3.induced_voltage, ls="--")
+    def test_integration(self):
+        n_macroparticles = int(1e6)
+        n_slices = 256
+        bunch_length = 1e-9
+        self.blond3 = Blond3(n_macroparticles, n_slices, bunch_length)
+
+        plt.title(f"{n_macroparticles} {n_slices} {bunch_length}")
+        plt.plot(self.blond3.blond2.induced_voltage[0], label="blond2 ind_volt time")
+        # plt.plot(self.blond3.blond2.induced_voltage[1], label="blond2 ind volt res", ls=":")
+        # plt.plot(self.blond3.blond2.induced_voltage[2], label="blond2 ind volt freq", ls="--")
+        plt.plot(self.blond3.induced_voltage, label="blond3", ls="dashdot")
+        plt.legend()
         plt.show()
-        # plt.plot(self.blond3.blond2.induced_voltage)
-        # plt.plot(self.blond3.induced_voltage, ls="--")
-        # plt.xlim(390*2, 395*2)
-        # plt.ylim(-1e-7, 1e-7)
-        # plt.show()
 
-        assert self.close_in_norm(self.blond3.blond2.induced_voltage, self.blond3.induced_voltage)
+        for blond2_ind_volt in self.blond3.blond2.induced_voltage:
+            try:
+                assert self.close_in_norm(blond2_ind_volt, self.blond3.induced_voltage)
+            except AssertionError:
+                np.testing.assert_allclose(
+                    blond2_ind_volt, self.blond3.induced_voltage, rtol=1e-6
+                )
 
-        # np.testing.assert_allclose(
-        #     self.blond3.blond2.induced_voltage, self.blond3.induced_voltage, rtol=1e-6
-        # )
+    def diff_params(self):
+        resgrid = np.zeros((3, 4, 4, 3))
+        for mac_ind, n_macroparticles in enumerate([int(1e4), int(1e6), int(1e8)]):
+            # for slic_ind, n_slices in enumerate([64, 128, 256, 512]):
+            for slic_ind, n_slices in enumerate([32]):
+                for b_ind, bunch_length in enumerate([1e-9 / 4, 1e-9, 4e-9]):
+                    self.blond3 = Blond3(n_macroparticles, n_slices, bunch_length)
+                    plt.title(f"{n_macroparticles} {n_slices} {bunch_length}")
+                    plt.plot(self.blond3.blond2.induced_voltage[0], label="blond2 ind_volt time")
+                    plt.plot(self.blond3.blond2.induced_voltage[1], label="blond2 ind volt freq", ls=":")
+                    # plt.plot(self.blond3.blond2.induced_voltage[2], label="blond2 ind volt res", ls="--")
+                    plt.plot(self.blond3.induced_voltage, label="blond3", ls="dashdot")
+                    plt.legend()
+                    plt.savefig(f"{n_macroparticles}_{n_slices}_{bunch_length}.png", dpi=400)
+                    plt.show()
 
+                    # for blond2_ind_volt in self.blond3.blond2.induced_voltage:
+                    resgrid[mac_ind, slic_ind, b_ind, 0] = self.close_in_norm(self.blond3.blond2.induced_voltage[0],
+                                                                              self.blond3.induced_voltage)
+                    resgrid[mac_ind, slic_ind, b_ind, 1] = self.close_in_norm(self.blond3.blond2.induced_voltage[1],
+                                                                              self.blond3.induced_voltage)
+                    resgrid[mac_ind, slic_ind, b_ind, 2] = self.close_in_norm(self.blond3.blond2.induced_voltage[2],
+                                                                              self.blond3.induced_voltage)
 
+                    #     assert self.close_in_norm(blond2_ind_volt, self.blond3.induced_voltage)
+                    # except AssertionError:
+                    #     np.testing.assert_allclose(
+                    #         blond2_ind_volt, self.blond3.induced_voltage, rtol=1e-6
+                    #     )
