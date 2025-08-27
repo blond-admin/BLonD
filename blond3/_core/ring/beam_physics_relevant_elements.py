b75aeb11b5b3268aa6f0145afc01b36ad86b7a14
slauber <simon.fabian.lauber@cern.ch>
2025-08-27 18:47:57 +0200

    Formatted

--- blond3/_core/ring/beam_physics_relevant_elements.py
+++ blond3/_core/ring/beam_physics_relevant_elements.py
@@ -48,9 +48,9 @@ class BeamPhysicsRelevantElements(Preparable):
 
         elem_section_indices = [e.section_index for e in self.elements]
         assert min(elem_section_indices) == 0, "section_index=0 must be set"
-        assert np.all(
-            np.diff(elem_section_indices) >= 0
-        ), f"Section indices must be increasing, but got {elem_section_indices}"
+        assert np.all(np.diff(elem_section_indices) >= 0), (
+            f"Section indices must be increasing, but got {elem_section_indices}"
+        )
         cavities = self.get_elements(CavityBaseClass)
         cav_section_indices = [c.section_index for c in cavities]
         all_different = len(cav_section_indices) == len(set(cav_section_indices))
@@ -154,10 +154,11 @@ class BeamPhysicsRelevantElements(Preparable):
         else:
             self.elements.append(element)
 
-    def check_section_index_compatibility(self, element:
-    BeamPhysicsRelevant, insert_at: int):
+    def check_section_index_compatibility(
+        self, element: BeamPhysicsRelevant, insert_at: int
+    ):
         """
-        Internal method to check the element is inserted in the defined 
+        Internal method to check the element is inserted in the defined
         section.
 
         Parameters
@@ -175,25 +176,33 @@ class BeamPhysicsRelevantElements(Preparable):
             insertion.
             If insert_at is not within [0:len(ring.elements.elements)]
         """
-        try :
+        try:
             if (insert_at != 0) and (insert_at != len(self.elements)):
-                assert (self.elements[insert_at - 1].section_index <=
-                        element.section_index <= self.elements[
-                            insert_at].section_index)
+                assert (
+                    self.elements[insert_at - 1].section_index
+                    <= element.section_index
+                    <= self.elements[insert_at].section_index
+                )
             elif insert_at == 0:
-                assert (element.section_index ==
-                        self.elements[insert_at].section_index)
+                assert element.section_index == self.elements[insert_at].section_index
             elif insert_at == len(self.elements):
-                assert (self.elements[insert_at - 1].section_index <=
-                        element.section_index <=
-                        self.elements[insert_at - 1].section_index + 1)
+                assert (
+                    self.elements[insert_at - 1].section_index
+                    <= element.section_index
+                    <= self.elements[insert_at - 1].section_index + 1
+                )
             else:
-                raise AssertionError(f'The element must be inserted within ['
-                                 f'0:{len(self.elements)}] indexes. ')
+                raise AssertionError(
+                    f"The element must be inserted within ["
+                    f"0:{len(self.elements)}] indexes. "
+                )
         except:
-            raise AssertionError('The element section index is incompatible '
-                                 'with the requested location. Please allow '
-                                 'overwrite for automatic handling.')
+            raise AssertionError(
+                "The element section index is incompatible "
+                "with the requested location. Please allow "
+                "overwrite for automatic handling."
+            )
+
     def insert(self, element: BeamPhysicsRelevant, insert_at: int) -> None:
         """
         Insert a beam physics-relevant element to the container at the
@@ -217,8 +226,7 @@ class BeamPhysicsRelevantElements(Preparable):
             If insert_at is not within [0:len(ring.elements.elements)]
         """
         assert isinstance(element.section_index, int)
-        self.check_section_index_compatibility(element = element,
-                                               insert_at= insert_at)
+        self.check_section_index_compatibility(element=element, insert_at=insert_at)
         self.elements.insert(insert_at, element)
 
     @property  # as readonly attributes
@@ -283,7 +291,10 @@ class BeamPhysicsRelevantElements(Preparable):
             class_=class_,
             section_i=section_i,
         )
-        assert len(elements) == 1, f"{len(elements)=}"
+        assert len(elements) == 1, (
+            f"Expected exactly one `{class_.__name__}`,"
+            f" but found {len(elements)} of this type."
+        )
         return elements[0]
 
     def reorder(self):
