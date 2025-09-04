1307ccb0f41da7235b53dd9dd45203f4764aecaa
lthiele <leonard.thiele@cern.ch>
2025-09-04 17:34:15 +0200

    readded pinned tests

--- unittests/physics/impedances/test_sources.py
+++ unittests/physics/impedances/test_sources.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import Mock
 
 import numpy as np
+from examples.EX_MuonCollider import local_res
 from matplotlib import pyplot as plt
 from scipy.constants import pi
 from scipy.constants import speed_of_light as c0
@@ -78,11 +79,51 @@ class TestInductiveImpedance(unittest.TestCase):
             simulation=simulation,
             beam=beam,
         )
+
+        pinned_freq_y = np.array(
+            # this might need to change if found that
+            # get_impedance physics is incorrect
+            [
+                0j,
+                1.327867777722188e16j,
+                2.64016751983167e16j,
+                3.9215137116993096e16j,
+                5.156883740771454e16j,
+                6.331794022966821e16j,
+                7.432469809457386e16j,
+                8.446006683004016e16j,
+                9.360521850449973e16j,
+                1.0165293457606154e17j,
+                1.0850886293188341e17j,
+                1.1409262408042843e17j,
+                1.1833875352751144e17j,
+                1.2119746928763856e17j,
+                1.22635255532271e17j,
+                1.22635255532271e17j,
+                1.2119746928763856e17j,
+                1.1833875352751144e17j,
+                1.1409262408042845e17j,
+                1.0850886293188341e17j,
+                1.0165293457606154e17j,
+                9.360521850449973e16j,
+                8.446006683004018e16j,
+                7.432469809457386e16j,
+                6.331794022966825e16j,
+                5.156883740771458e16j,
+                3.921513711699308e16j,
+                2.640167519831673e16j,
+                1.3278677777221898e16j,
+                -39.500437208149435j,
+            ]
+        )
         DEV_DEBBUG = False
         if DEV_DEBBUG:
             plt.plot(freq_x, np.abs(freq_y))
             plt.show()
-        # TODO PIN VALUE!
+        # This is NOT a test if the physics is correct!
+        # It should just allow to change internals of `get_impedance`
+        # and guarantee that the result did not change
+        np.testing.assert_allclose(freq_y, pinned_freq_y)
 
 
 class TestResonators(unittest.TestCase):
@@ -125,6 +166,65 @@ class TestResonators(unittest.TestCase):
                 center_frequencies=np.array([400e6]),
                 quality_factors=np.array([0.49]),
             )
+
+    def test_get_impedance_pinned(self):
+        simulation = Mock(Simulation)
+        beam = Mock(BeamBaseClass)
+        freq_x = np.linspace(0, 1e9, 30)
+        local_res = Resonators(
+            shunt_impedances=np.array([1, 2, 3]),
+            center_frequencies=np.array([400e6, 600e6, 1.2e9]),
+            quality_factors=np.array([1, 2, 3]),
+        )
+        freq_y = local_res.get_impedance(
+            freq_x=freq_x, simulation=simulation, beam=beam
+        )
+
+        pinned_freq_y = np.array(
+            # this might need to change if found that
+            # get_impedance physics is incorrect
+            [
+                0j,
+                (0.00942358651488795 + 0.17257266539124913j),
+                (0.03847829128074856 + 0.34598502512183227j),
+                (0.08951350665665338 + 0.5204706751495055j),
+                (0.1663642473536873 + 0.694876830308083j),
+                (0.27394129680743806 + 0.8655824712339767j),
+                (0.4169903786734268 + 1.0252003539257513j),
+                (0.597367508904515 + 1.1617714626882443j),
+                (0.8098132299529293 + 1.260286679759775j),
+                (1.0387297231581394 + 1.3088371527047062j),
+                (1.2617888995045035 + 1.3082130088125705j),
+                (1.4634874229891084 + 1.2759901511026408j),
+                (1.649396831946504 + 1.2354615637236905j),
+                (1.8457486837443287 + 1.194271207370826j),
+                (2.080707633067911 + 1.1284228930251694j),
+                (2.3538572306159256 + 0.9836550243814897j),
+                (2.606598464998768 + 0.7073482747932069j),
+                (2.7338902681213844 + 0.3126019580772236j),
+                (2.6698698182119367 - 0.09491115416842288j),
+                (2.4546067744675466 - 0.39781425368699297j),
+                (2.184014624168805 - 0.5539547699273479j),
+                (1.931984543412328 - 0.5867513809204155j),
+                (1.731407279490519 - 0.5372728771383196j),
+                (1.5898770746147266 - 0.4387931168944583j),
+                (1.505515550177968 - 0.31291770276423536j),
+                (1.47515159586377 - 0.17301463290083663j),
+                (1.4974580247385165 - 0.028301503021182173j),
+                (1.5737339298195334 + 0.11277261150134943j),
+                (1.7074625414604356 + 0.2395805248705769j),
+                (1.902597177481868 + 0.33642772231686235j),
+            ]
+        )
+        DEV_DEBBUG = True
+        if DEV_DEBBUG:
+            plt.plot(freq_x, np.abs(freq_y))
+            plt.show()
+
+        # This is NOT a test if the physics is correct!
+        # It should just allow to change internals of `get_impedance`
+        # and guarantee that the result did not change
+        np.testing.assert_allclose(freq_y, pinned_freq_y)
 
     def test_get_wake(self):
         freq, q_factor, shut_imp = (
