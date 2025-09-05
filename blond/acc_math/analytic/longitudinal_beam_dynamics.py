a9d8c80d5970dca1cff819aadc453a52ae1970e4
lvalle <lina.valle@cern.ch>
2025-09-05 16:14:52 +0200

    Transferring synchrotron radiation functions and tests

---
+++ blond/acc_math/analytic/longitudinal_beam_dynamics.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import numpy as np
+from scipy.constants import e
+
+from ..._core.beam.particle_types import ParticleType
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray as NumpyArray
+
+
+def get_synchrotron_frequency(
+    energy: float | NumpyArray,
+    voltage: float | NumpyArray,
+    harmonic_number: float | NumpyArray,
+    synchronous_phase: float | NumpyArray,
+    slip_factor: float | NumpyArray,
+    revolution_frequency: float | NumpyArray,
+):
+    """
+    Calculation of the linear synchrotron angular frequency
+
+    Parameters
+    ----------
+    energy
+    voltage
+    harmonic_number
+    synchronous_phase
+    slip_factor
+    revolution_frequency
+
+    Returns
+    -------
+
+    """
+
+    return (
+        np.pi
+        * revolution_frequency
+        * np.sqrt(
+            (
+                harmonic_number
+                * e
+                * voltage
+                * np.abs(slip_factor * np.cos(synchronous_phase))
+            )
+            / (2 * np.pi * energy)
+        )
+    )
