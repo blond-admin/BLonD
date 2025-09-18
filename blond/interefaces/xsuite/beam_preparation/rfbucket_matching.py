a0a63437d87d783d9e75ec2852c73e9776730306
slauber <simon.fabian.lauber@cern.ch>
2025-09-18 12:47:57 +0200

    Reworked type hints, added comments

--- blond/interefaces/xsuite/beam_preparation/rfbucket_matching.py
+++ blond/interefaces/xsuite/beam_preparation/rfbucket_matching.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Optional, Type
+from typing import TYPE_CHECKING, Type, Union
 
 import numpy as np
 from scipy.constants import c, e
 from xpart.longitudinal.rf_bucket import RFBucket
-from xpart.longitudinal.rfbucket_matching import ThermalDistribution  # default
 from xpart.longitudinal.rfbucket_matching import (
     RFBucketMatcher,
 )
@@ -13,16 +12,38 @@ from xpart.longitudinal.rfbucket_matching import (
 from blond import SingleHarmonicCavity
 from blond.beam_preparation.base import MatchingRoutine
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
+    from typing import Optional, Tuple
+
+    from xpart.longitudinal.rfbucket_matching import (
+        ParabolicDistribution,
+        QGaussianDistribution,
+        StationaryDistribution,
+        ThermalDistribution,
+        WaterbagDistribution,
+    )
+
     from blond._core.beam.base import BeamBaseClass
     from blond._core.simulation.simulation import Simulation
+
+    # TODO tests should cover all generators, if included in type hint
+    distribution_hints = Type[
+        Union[
+            ThermalDistribution,
+            ThermalDistribution,
+            QGaussianDistribution,
+            ParabolicDistribution,
+            WaterbagDistribution,
+            StationaryDistribution,  # general, in case xsuite was extended
+        ]
+    ]
 
 
 class XsuiteRFBucketMatcher(MatchingRoutine):
     def __init__(
         self,
         n_macroparticles: int | float,
-        distribution_type: ThermalDistribution,
+        distribution_type: distribution_hints,
         cavity: Optional[SingleHarmonicCavity] = None,
         sigma_z: Optional[float] = None,
         energy_init: Optional[float] = None,
@@ -57,8 +78,8 @@ class XsuiteRFBucketMatcher(MatchingRoutine):
             beam=beam,
         )
 
-        drifts: DriftSimple = simulation.ring.elements.get_elements(
-            DriftSimple
+        drifts: Tuple[DriftSimple, ...] = (
+            simulation.ring.elements.get_elements(DriftSimple)
         )
         for _drift in drifts:
             _drift.apply_schedules(
@@ -66,12 +87,15 @@ class XsuiteRFBucketMatcher(MatchingRoutine):
                 reference_time=0,
             )
         rf_element = self.cavity
+        # TODO what  if rf_element is None
         rf_element.apply_schedules(turn_i=0, reference_time=0)
 
         energy = self.energy_init
         rest_mass = beam.particle_type.mass
+        # TODO what  if energy is None
         gamma = energy / rest_mass
         transition_gamma = drifts[0].transition_gamma
+        # TODO what  if transition_gamma is None
         alpha_c = 1 / transition_gamma**2 - 1 / gamma**2
         mass_kg = beam.particle_type.mass * e / c**2
         charge_coulomb = beam.particle_type.charge * e
@@ -83,6 +107,7 @@ class XsuiteRFBucketMatcher(MatchingRoutine):
             mass_kg=mass_kg,
             charge_coulomb=charge_coulomb,
             alpha_array=np.atleast_1d(alpha_c),
+            # TODO what  if cavity is None
             harmonic_list=np.atleast_1d(self.cavity.harmonic),
             voltage_list=np.atleast_1d(self.cavity.voltage),
             phi_offset_list=np.atleast_1d(self.cavity.phi_rf),
