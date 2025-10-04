40ad7ed8a16b66d264018bc59b3bd236065f6ddc
lthiele <leonard.thiele@cern.ch>
2025-10-04 17:39:31 +0200

    tests fixed

--- blond/examples/EX_MuonCollider.py
+++ blond/examples/EX_MuonCollider.py
@@ -3,13 +3,9 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
-import psutil
+
+# import psutil
 from scipy.constants import pi
-
-p = psutil.Process(os.getpid())
-p.cpu_affinity(([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]))
-
-import json
 
 from blond import (
     Beam,
