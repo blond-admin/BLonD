a69d9708311e9e2056c4ef286e424d68a2d074a6
lthiele <leonard.thiele@cern.ch>
2025-10-04 16:18:36 +0200

    fixed simulation without intensity effects

--- blond/examples/EX_MuonCollider.py
+++ blond/examples/EX_MuonCollider.py
@@ -1,8 +1,5 @@
 import os
-from copy import deepcopy
-from os import PathLike
 from pathlib import Path
-from typing import TYPE_CHECKING
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -44,22 +41,7 @@ from blond.specifics.muon_collider.beam_matching.beam_matching_rountine import (
 backend.change_backend(
     Numpy32Bit
 )  # TODO: without these lines, it does not work, default should be set somewhere to be Numpy64bit python
-backend.set_specials("python")
-#
-# import ctypes
-#
-# import numba
-#
-# print(
-#     "Numba threading layer:", numba.config.THREADING_LAYER
-# )  # or numba.threading_layer()
-# mods = [b"libiomp5md.dll", b"libomp.dll", b"vcomp140.dll", b"libgomp-1.dll"]
-# print(
-#     {
-#         m.decode(): bool(ctypes.windll.kernel32.GetModuleHandleA(m))
-#         for m in mods
-#     }
-# )
+backend.set_specials("numba")
 
 
 # phi_s = 128 * pi / 180  # deg
@@ -77,21 +59,19 @@ def setup_and_run(int_eff=False):
     alpha_p = 8.986e-4
     Q_factor = 1.76e6
     bunch_intensity = 2.4e12
+    n_stations = 8
+    circumference = 5990
+    harmonic = 25928
+    voltage_per_cavity = 31140000.0
 
     energy_gain_per_turn = (ejection_energy - inj_energy) / n_turns
-    total_voltage = energy_gain_per_turn / np.sin(
-        phi_s
-    )  # different from BlonD2
-    n_cavities = total_voltage / 31140000.0
-    n_stations = 8
-    cav_per_station = (
-        n_cavities / n_stations
-    )  # TODO: this is currently a float --> just scaling
+    total_voltage = energy_gain_per_turn / np.sin(phi_s)
+    voltage_per_station = total_voltage / n_stations
+    n_cavities = int(np.ceil(total_voltage / voltage_per_cavity))
+    cav_per_station = n_cavities / n_stations
 
     R_over_Q = 518 if int_eff else 0
     gamma_transition = 1 / np.sqrt(alpha_p)
-    circumference = 5990
-    harmonic = 25928
 
     ring = Ring(circumference=circumference)
     magnetic_cycle = MagneticCyclePerTurn(
@@ -104,19 +84,22 @@ def setup_and_run(int_eff=False):
     )
     one_turn_model = []
     profile_list = []
+    t_rf = (
+        magnetic_cycle.get_t_rev_init(
+            ring.circumference,
+            turn_i_init=0,
+            t_init=0,
+            particle_type=mu_plus,
+        )
+        / harmonic
+    )
     for cavity_i in range(n_stations):
         profile_list.append(
             StaticProfile.from_rad(
-                0,
+                1e-10 * 2 * pi / t_rf,
                 2 * np.pi,
                 2**9,
-                magnetic_cycle.get_t_rev_init(
-                    ring.circumference,
-                    turn_i_init=0,
-                    t_init=0,
-                    particle_type=mu_plus,
-                )
-                / harmonic,  # this does not track anzthing?
+                t_rf,
                 section_index=cavity_i,
             )
         )
@@ -130,7 +113,7 @@ def setup_and_run(int_eff=False):
             [
                 profile_list[-1],
                 SingleHarmonicCavity(
-                    voltage=total_voltage / n_stations,
+                    voltage=voltage_per_station,
                     phi_rf=0,
                     harmonic=harmonic,
                     local_wakefield=WakeField(
@@ -142,7 +125,7 @@ def setup_and_run(int_eff=False):
                     else None,
                     section_index=cavity_i,
                 ),
-                profile_list[-1],  # for CR beam
+                # profile_list[-1],  # for CR beam
                 DriftSimple(  # for symmetry's sake for the CR bunch, we need to inject in the middle of a drift
                     transition_gamma=gamma_transition,
                     orbit_length=circumference / n_stations,
@@ -192,7 +175,7 @@ def setup_and_run(int_eff=False):
     #     beam=beam_CR,
     # )
     sim.run_simulation(
-        beams=(beam, beam_CR),
+        beams=([beam]),
         turn_i_init=0,
         n_turns=None,
         observe=(
@@ -226,15 +209,14 @@ def plot_and_compare(
     energy_centroid_blond2 = np.array(jdict["energy_mean"])
 
     plt.title("bunch length")
-    plt.plot(bunch_observation.turns_array, bunch_observation.sigma_dt * 1e12)
+    plt.plot(bunch_observation.sigma_dt * 1e12)
     # plt.plot(
     #     bunch_observation_CR.turns_array,
     #     bunch_observation_CR.sigma_dt * 1e12,
     #     label="CR",
     # )
     plt.plot(
-        bunch_observation_CR.turns_array,
-        bunch_length_blond2[:-1] * 1e12,
+        bunch_length_blond2[1:] * 1e12,
         label="blond2",
     )
     plt.ylabel("bunch length [ps]")
@@ -245,7 +227,7 @@ def plot_and_compare(
     plt.title("bunch centroid")
     plt.plot(bunch_observation.mean_dt * 1e9)
     # plt.plot(bunch_observation_CR.mean_dt * 1e9, label="CR")
-    plt.plot(bunch_centroid_blond2 * 1e9, label="blond2")
+    plt.plot(bunch_centroid_blond2[1:] * 1e9, label="blond2")
     plt.ylabel("bunch centroid [ns]")
     plt.legend()
     plt.show()
@@ -253,7 +235,7 @@ def plot_and_compare(
     plt.title("energy spread")
     plt.plot(bunch_observation.sigma_dE / 1e9)
     # plt.plot(bunch_observation_CR.sigma_dE / 1e9, label="CR")
-    plt.plot(energy_spread_blond2 / 1e9, label="blond2")
+    plt.plot(energy_spread_blond2[1:] / 1e9, label="blond2")
     plt.ylabel("energy spread [GeV]")
     plt.legend()
     plt.show()
@@ -261,25 +243,25 @@ def plot_and_compare(
     plt.title("energy centroid")
     plt.plot(bunch_observation.mean_dE)
     # plt.plot(bunch_observation_CR.mean_dE, label="CR")
-    plt.plot(energy_centroid_blond2, label="blond2")
+    plt.plot(energy_centroid_blond2[1:], label="blond2")
     plt.legend()
     plt.show()
 
     plt.title("emittance")
     plt.plot(bunch_observation.emittance_stat, label="emittance")
     # plt.plot(bunch_observation_CR.emittance_stat, label="CR")
-    plt.plot(emittance_blond2, label="blond2")
+    plt.plot(emittance_blond2[1:], label="blond2")
     plt.ylabel("statistical emittance (eVs)")
     plt.legend()
     plt.show()
 
-    plt.title("sigma t * sigma E")
-    plt.plot(bunch_observation.sigma_dE * bunch_observation.sigma_dt)
-    # plt.plot(
-    #     bunch_observation_CR.sigma_dE * bunch_observation_CR.sigma_dt, label="CR"
-    # )
-    plt.legend()
-    plt.show()
+    # plt.title("sigma t * sigma E")
+    # plt.plot(bunch_observation.sigma_dE * bunch_observation.sigma_dt)
+    # # plt.plot(
+    # #     bunch_observation_CR.sigma_dE * bunch_observation_CR.sigma_dt, label="CR"
+    # # )
+    # plt.legend()
+    # plt.show()
 
     # profiles = profile_observation.hist_y
     # turn_arr = profile_observation.turns_array
