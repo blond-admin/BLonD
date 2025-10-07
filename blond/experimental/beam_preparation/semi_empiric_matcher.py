Fixed WakeField calculation to work with different beams and scale by beam intensity only. (before, it also scaled with the array size because ratio was used)

blond/experimental/beam_preparation/semi_empiric_matcher.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
 
+import math
 from typing import Callable  # NOQA
 from typing import TYPE_CHECKING
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-from blond import AllowPlotting, backend
+from blond import AllowPlotting, WakeField, backend
 from blond.beam_preparation.base import MatchingRoutine
 
 from ..._core.helpers import int_from_float_with_warning
+from ...physics.profiles import ProfileBaseClass
 from .helpers import populate_beam
 
 if TYPE_CHECKING:  # pragma: no cover
@@ -230,6 +232,7 @@ class SemiEmpiricMatcher(MatchingRoutine):
         self.verbose = verbose
 
         self._previous_potential_well: NumpyArray | CupyArray | None = None
+        self._preprevious_potential_well: NumpyArray | CupyArray | None = None
 
     def prepare_beam(
         self,
@@ -251,6 +254,17 @@ class SemiEmpiricMatcher(MatchingRoutine):
         # iterate solution with intensity effects
         intensity_org = beam.intensity
         hist_y_previous = None
+
+        # Get decimal places from the tolerance (e.g., 1e-6 → 6)
+        tolerance_decimal_places = abs(
+            int(math.floor(math.log10(self.tolerance)))
+        )
+        if self.animate:
+            plt.figure("SemiEmpiricMatcher")
+            plt.clf()
+            self._plot_current_state(beam, 0, 0, ts)
+            plt.draw()
+            plt.pause(0.1)
         if simulation.intensity_effect_manager.has_wakefields():
             for i in range(self.maxiter_intensity_effects):
                 # Change the strength of intensity effects to allow
@@ -285,8 +299,8 @@ class SemiEmpiricMatcher(MatchingRoutine):
                     beam.read_partial_dt(),
                     bins=self.internal_grid_shape[0],
                     range=self.time_limit,
-                    density=True,
                 )
+                hist_y = hist_y / np.max(hist_y)
 
                 if self.animate:
                     plt.figure("SemiEmpiricMatcher")
@@ -302,7 +316,9 @@ class SemiEmpiricMatcher(MatchingRoutine):
                         np.sqrt(np.mean((hist_y - hist_y_previous) ** 2))
                     )
                     if self.verbose:
-                        print(f"Iteration {i}, {error=}")
+                        print(
+                            f"Iteration: {i:<5} | Error: {error:.{tolerance_decimal_places}f}"
+                        )
                     if (
                         error < self.tolerance
                         and i
@@ -314,8 +330,27 @@ class SemiEmpiricMatcher(MatchingRoutine):
 
             simulation.intensity_effect_manager.set_wakefields(active=True)
             simulation.intensity_effect_manager.set_profiles(active=True)
+            beam.intensity = intensity_org
 
-            beam.intensity = int(intensity_org)
+            simulation.run_simulation(
+                beams=(beam,),
+                n_turns=1,
+                turn_i_init=0,
+                show_progressbar=False,
+            )
+            plt.figure("mega_debug")
+            plt.subplot(2, 1, 1)
+            prof = simulation.ring.elements.get_element(WakeField).profile
+            plt.plot(prof.hist_x, prof.hist_y)
+            plt.subplot(2, 1, 2)
+            simulation.intensity_effect_manager.set_profiles(active=False)
+
+            potential_well, factor = simulation.get_potential_well_empiric(
+                ts=ts,
+                particle_type=beam.particle_type,
+                intensity=beam.intensity,
+            )
+            plt.plot(ts, beam.intensity * potential_well * factor)
 
     def _match_beam(
         self,
@@ -341,13 +376,21 @@ class SemiEmpiricMatcher(MatchingRoutine):
         ts
             Time coordinate, in [s] for observation of the potential well.
         """
-        potential_well = simulation.get_potential_well_empiric(
+        potential_well, factor = simulation.get_potential_well_empiric(
             ts=ts, particle_type=beam.particle_type, intensity=beam.intensity
         )
+        potential_well = potential_well * factor
+        self._preprevious_potential_well = self._previous_potential_well
         self._previous_potential_well = potential_well  # for debugging
+        if self._preprevious_potential_well is None:
+            avg_pot_well = potential_well
+        else:
+            avg_pot_well = (
+                potential_well + self._preprevious_potential_well
+            ) / 2
         deltaE_grid, time_grid, hamilton_2D = get_hamiltonian_semi_analytic(
             ts=ts,
-            potential_well=potential_well,
+            potential_well=avg_pot_well,
             reference_total_energy=beam.reference_total_energy,
             beta=beam.reference_beta,
             eta=float(
@@ -395,6 +438,15 @@ class SemiEmpiricMatcher(MatchingRoutine):
 
 
         """
+        plt.figure("mega_debug")
+        plt.subplot(2, 1, 1)
+        plt.cla()
+        plt.hist(
+            beam.read_partial_dt(),
+            bins=self.internal_grid_shape[0],
+            range=self.time_limit,
+        )
+        plt.figure("SemiEmpiricMatcher")
         with AllowPlotting():
             plt.subplot(2, 1, 1)
             plt.title(f"Iteration {i}, Intensity strength {scalar * 100} %")
@@ -411,5 +463,7 @@ class SemiEmpiricMatcher(MatchingRoutine):
             plt.axhline(self.hamilton_max, c="C1", linestyle="--")
             if self._previous_potential_well is not None:
                 plt.plot(ts, self._previous_potential_well)
+            if self._preprevious_potential_well is not None:
+                plt.plot(ts, self._preprevious_potential_well)
             plt.xlabel("Time (s)")
             plt.ylabel("Potential (arb. unit)")

blond/physics/impedances/solvers.py
@@ -83,7 +83,10 @@ class InductiveImpedanceSolver(WakeFieldSolver):
         factor = -backend.float(
             (beam.particle_type.charge * e)
             / (2 * np.pi)
-            * beam.ratio
+            * (
+                beam.intensity  # this used to be ratio
+                * self._parent_wakefield.profile.histy_to_density_factor
+            )
             * (self._simulation.ring.circumference / beam.reference_velocity)
             / self._parent_wakefield.profile.hist_step
         )
@@ -327,9 +330,11 @@ class PeriodicFreqSolver(WakeFieldSolver):
 
         _factor = backend.float(
             (-1 * beam.particle_type.charge * e)
-            * (
-                # TODO this might be a problem with MPI
-                beam.ratio
+            *
+            # TODO this might be a problem with MPI
+            (
+                beam.intensity  # this used to be ratio
+                * self._parent_wakefield.profile.histy_to_density_factor
             )
         )
 
@@ -514,9 +519,14 @@ class TimeDomainFftSolver(WakeFieldSolver):
             self._wake_imp_y_needs_update = True
         self._update_impedance_sources(beam=beam)
 
-        _factor = (-1 * beam.particle_type.charge * e) * (
+        _factor = (
+            (-1 * beam.particle_type.charge * e)
+            *
             # TODO this might be a problem with MPI
-            beam.ratio
+            (
+                beam.intensity  # this used to be ratio
+                * self._parent_wakefield.profile.histy_to_density_factor
+            )
         )
         # Calculate the convolution of the wake and the beam
         # Usually this would be np.convolve(wake, beam).

unittests/_core/simulation/test_simulation.py
@@ -130,7 +130,7 @@ class TestSimulation(unittest.TestCase):
         sim = SimulationTwoRfStations()
         ts = np.linspace(-2e-9, 2e-9, 100)
 
-        potential_well = sim.simulation.get_potential_well_empiric(
+        potential_well, factor = sim.simulation.get_potential_well_empiric(
             ts=ts,
             particle_type=proton,
         )
@@ -246,7 +246,7 @@ class TestSimulation(unittest.TestCase):
             beam_beta=self.beam.reference_beta,
             ring_circumference=self.simulation.ring.circumference,
         )
-        potential_well = self.simulation.get_potential_well_empiric(
+        potential_well, factor = self.simulation.get_potential_well_empiric(
             ts, particle_type=particle_type
         )
         DEV_PLOT = True  # TODO set false
@@ -259,14 +259,11 @@ class TestSimulation(unittest.TestCase):
             * (np.cos(phis) - np.cos(phi_s) + (phis - phi_s) * np.sin(phi_s))
         )
         if DEV_PLOT:
-            plt.subplot(2, 1, 1)
             plt.plot(
                 potential_well,
                 label="potential_well",
             )
-            plt.legend()
 
-            plt.subplot(2, 1, 2)
             plt.plot(
                 potential_well_analytic,
                 "--",

unittests/experimental/beam_preparation/test_semi_empiric_matcher.py
@@ -3,11 +3,19 @@ import unittest
 import matplotlib.pyplot as plt
 import numpy as np
 
-from blond import Beam, Simulation, backend
+from blond import (
+    Beam,
+    MultiHarmonicCavity,
+    Simulation,
+    SingleHarmonicCavity,
+    WakeField,
+    backend,
+)
 from blond.experimental.beam_preparation.semi_empiric_matcher import (
     SemiEmpiricMatcher,
     get_hamiltonian_semi_analytic,
 )
+from blond.physics.profiles import ProfileBaseClass
 
 
 class TestSemiEmpiricMatcher(unittest.TestCase):
@@ -29,12 +37,16 @@ class TestSemiEmpiricMatcher(unittest.TestCase):
         sim = SimulationTwoRfStations()
         self._test_matching(sim)
 
-        DEV_PLOT = False
+        DEV_PLOT = True
         if DEV_PLOT:
 
             def my_callback(simulation: Simulation, beam: Beam):
+                if simulation.turn_i.value % 10 != 0:
+                    return
                 plt.clf()
-                beam.plot_hist2d()
+                beam.plot_hist2d(range=((0.7e-9, 1.8e-9), (-3.5e8, 3.5e8)))
+                plt.axhline(beam._dE.mean())
+                plt.axvline(beam._dt.mean())
                 plt.draw()
                 plt.pause(0.1)
 
@@ -62,6 +74,47 @@ class TestSemiEmpiricMatcher(unittest.TestCase):
 
         sim = SimulationTwoRfStationsWithWake()
         self._test_matching(sim)
+        DEV_PLOT = True
+        if DEV_PLOT:
+
+            def my_callback(simulation: Simulation, beam: Beam):
+                if simulation.turn_i.value == 0:
+                    ts = np.linspace(0, 2.5e-9, 50)
+
+                    plt.figure("mega_debug")
+                    plt.subplot(2, 1, 1)
+                    prof = simulation.ring.elements.get_element(
+                        WakeField
+                    ).profile
+                    plt.plot(prof.hist_x, prof.hist_y)
+                    plt.subplot(2, 1, 2)
+                    simulation.intensity_effect_manager.set_profiles(
+                        active=False
+                    )
+
+                    potential_well, factor = (
+                        simulation.get_potential_well_empiric(
+                            ts=ts,
+                            particle_type=beam.particle_type,
+                            intensity=beam.intensity,
+                        )
+                    )
+                    plt.plot(ts, beam.intensity * potential_well * factor)
+                    plt.show()
+                if simulation.turn_i.value % 10 != 0:
+                    return
+                plt.clf()
+                beam.plot_hist2d(range=((0.7e-9, 1.8e-9), (-3.5e8, 3.5e8)))
+                plt.axhline(beam._dE.mean())
+                plt.axvline(beam._dt.mean())
+                plt.draw()
+                plt.pause(0.1)
+
+            sim.simulation.turn_i.value = 0
+            my_callback(simulation=sim.simulation, beam=sim.beam1)
+            sim.simulation.run_simulation(
+                beams=(sim.beam1,), callback=my_callback
+            )
         raise Exception()  # FIXME
 
     def _test_matching(self, sim):
@@ -79,6 +132,13 @@ class TestSemiEmpiricMatcher(unittest.TestCase):
             )
             / 36540
         )
+        print(ts.min(), ts.max())
+        # actively change the harmonic off the revolution time.
+        # matching should still work
+        cav = sim.simulation.ring.elements.get_element(MultiHarmonicCavity)
+        cav.harmonic = 33000 * np.ones(len(cav.harmonic), backend.float)
+        cav = sim.simulation.ring.elements.get_element(SingleHarmonicCavity)
+        cav.harmonic = 33000
 
         sim.simulation.prepare_beam(
             beam=sim.beam1,
@@ -86,11 +146,12 @@ class TestSemiEmpiricMatcher(unittest.TestCase):
                 time_limit=(ts.min(), ts.max()),
                 hamilton_max=50,
                 n_macroparticles=1e6,
-                internal_grid_shape=(512 - 1, 512 - 1),
+                internal_grid_shape=(1024 - 1, 1024 - 1),
                 density_modifier=5,
-                increment_intensity_effects_until_iteration_i=40,
-                maxiter_intensity_effects=50,
-                animate=False,
+                increment_intensity_effects_until_iteration_i=0,
+                maxiter_intensity_effects=20,
+                tolerance=0.001,
+                animate=True,
             ),
         )
 