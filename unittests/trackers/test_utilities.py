bb17855d883cdfa1f626cd821edb8d4024a808f9
lvalle <lina.valle@cern.ch>
2025-10-13 16:19:27 +0200

    removed empty tests

--- unittests/trackers/test_utilities.py
+++ unittests/trackers/test_utilities.py
@@ -5,53 +5,40 @@ from blond.trackers.utilities import minmax_location, potential_well_cut
 
 
 class TestUtilities(unittest.TestCase):
-    def _total_voltage(self):
-        pass
-    def _hamiltonian(self):
-        pass
-    def _separatrix(self):
-        pass
-    def _test_is_in_separatrix(self):
-        pass
-    def _test_minmax_location(self):
-        pass
     def test_potential_well_cut(self):
         time_potential = np.linspace(0, 1, 50)
         potential_array = np.array([np.sin(k * np.pi / 51) for k in range(10)])
-        with self.assertRaises(ValueError,
-                               msg=f"{len(time_potential)} != "
-                                   f"{len(potential_array)}"):
-            potential_well_cut(time_potential=time_potential,
-                               potential_array=potential_array
-                               )
+        with self.assertRaises(
+            ValueError, msg=f"{len(time_potential)} != {len(potential_array)}"
+        ):
+            potential_well_cut(
+                time_potential=time_potential, potential_array=potential_array
+            )
         # No minimum
         potential_array = np.array([np.sin(k * np.pi / 51) for k in range(50)])
-        with self.assertRaises(RuntimeError,
-                               msg="'The potential well has no minima...'"):
-            potential_well_cut(time_potential=time_potential,
-                               potential_array=potential_array
-                               )
-        #n_minima > n_maxima == 1:
+        with self.assertRaises(
+            RuntimeError, msg="'The potential well has no minima...'"
+        ):
+            potential_well_cut(
+                time_potential=time_potential, potential_array=potential_array
+            )
+        # n_minima > n_maxima == 1:
 
-        #No maximum
-        potential_array = np.array([1/np.sin((k+1) * np.pi / 50) for k in
-                                    range(50)])
+        # No maximum
+        potential_array = np.array(
+            [1 / np.sin((k + 1) * np.pi / 50) for k in range(50)]
+        )
 
-        with self.assertWarns(UserWarning,
-                               msg="The maximum of the potential well could not be found... \
+        with self.assertWarns(
+            UserWarning,
+            msg="The maximum of the potential well could not be found... \
                 You may reconsider the options to calculate the potential well \
                 as the main harmonic is probably not the expected one. \
                 You may also increase the percentage of margin to compute \
-                the potentiel well. The full potential well will be taken'"):
+                the potentiel well. The full potential well will be taken'",
+        ):
             time_potential_sep, potential_well_sep = potential_well_cut(
-                time_potential=time_potential,
-                               potential_array=potential_array
-                               )
+                time_potential=time_potential, potential_array=potential_array
+            )
         np.testing.assert_equal(time_potential_sep, time_potential)
         np.testing.assert_equal(potential_well_sep, potential_array)
-    def _test_phase_modulo_above_transition(self):
-        pass
-    def _test_phase_modulo_below_transition(self):
-        pass
-    def _time_modulo(self):
-        pass
