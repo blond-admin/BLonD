a50c8a85d0b41a43fb4917695d5aa7cefb125174
lvalle <lina.valle@cern.ch>
2025-10-16 11:40:43 +0200

    added input length checks in get_linear_angular_synchrotron_frequency function

--- blond/acc_math/analytic/longitudinal_beam_dynamics.py
+++ blond/acc_math/analytic/longitudinal_beam_dynamics.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import inspect
 from typing import TYPE_CHECKING
 
 import numpy as np
@@ -16,7 +17,7 @@ def get_linear_angular_synchrotron_frequency(
     voltage: float | NumpyArray,
     harmonic_number: float | NumpyArray,
     synchronous_phase: float | NumpyArray,
-    slip_factor: float | NumpyArray,
+    phase_slip_factor: float | NumpyArray,
     revolution_frequency: float | NumpyArray,
 ) -> float | NumpyArray:
     """
@@ -31,9 +32,9 @@ def get_linear_angular_synchrotron_frequency(
     harmonic_number
         Harmonic number, from the synchronous condition
     synchronous_phase
-        Phase of the synchronous particle
-    slip_factor
-        Slip factor, [dimensionless]
+        Phase of the synchronous particle, in [rad]
+    phase_slip_factor
+        Phase slip factor, [dimensionless]
     revolution_frequency
         Revolution frequency, in [Hz]
 
@@ -42,6 +43,19 @@ def get_linear_angular_synchrotron_frequency(
     angular_synchrotron_frequency
             Angular synchrotron frequency, in [rad]
     """
+    args = locals()
+    keys = inspect.signature(
+        get_linear_angular_synchrotron_frequency
+    ).parameters.keys()
+    input_lengths = {
+        len(args.get(arg)) if isinstance(args.get(arg), np.ndarray) else 1
+        for arg in keys
+    }
+    if len(input_lengths) > 2:
+        raise ValueError(
+            "Input arrays of more than one element have different lengths."
+        )
+
     return (
         np.pi
         * revolution_frequency
@@ -50,7 +64,7 @@ def get_linear_angular_synchrotron_frequency(
                 harmonic_number
                 * e
                 * voltage
-                * np.abs(slip_factor * np.cos(synchronous_phase))
+                * np.abs(phase_slip_factor * np.cos(synchronous_phase))
             )
             / (2 * np.pi * energy)
         )
