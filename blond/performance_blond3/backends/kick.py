f4ae4bfcaeec000e6bf870b0e4f0ca7891b914fb
slauber <simon.fabian.lauber@cern.ch>
2025-11-04 10:10:33 +0100

    Improved speed of unittests by preventing recompilation of the numba backend via caching.

--- blond/performance_blond3/backends/kick.py
+++ blond/performance_blond3/backends/kick.py
@@ -58,7 +58,9 @@ def main():  # pragma: no cover
     from blond._core.backends.cpp.callables import CppSpecials
     from blond._core.backends.cuda.callables import CudaSpecials
     from blond._core.backends.fortran.callables import FortranSpecials
-    from blond._core.backends.numba.callables import NumbaSpecials
+    from blond._core.backends.numba.callables import recompile_numba_backend
+
+    NumbaSpecials = recompile_numba_backend(backend.float)
 
     functions = (
         NumbaSpecials().kick_multi_harmonic,
