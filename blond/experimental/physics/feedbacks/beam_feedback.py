230bebbc0e89f689f6ec2aa5e684dfd86a8cf848
lthiele <leonard.thiele@cern.ch>
2025-11-04 13:11:23 +0100

    docm fix

--- blond/experimental/physics/feedbacks/beam_feedback.py
+++ blond/experimental/physics/feedbacks/beam_feedback.py
@@ -144,7 +144,6 @@ class Blond2BeamFeedback(LocalFeedback):
 
         self.delay = delay
 
-        #: | *Band-pass filter window coefficient for beam phase calculation.*
         self.alpha = window_coefficient
 
         self.time_offset = time_offset
