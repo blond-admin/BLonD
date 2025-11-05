15602c49eca3d3bde6bf7e44c47928612ef6dde4
slauber <simon.fabian.lauber@cern.ch>
2025-11-05 08:41:44 +0100

    Added testcase.

--- unittests/_core/beam/test_beams.py
+++ unittests/_core/beam/test_beams.py
@@ -135,6 +135,18 @@ class TestBeam(unittest.TestCase):
         Beam.plot_hist2d(beam)
         plt.gcf().clf()
 
+    def test_plot_hist_executes_gpu(self) -> None:
+        try:
+            import cupy as cp  # type: ignore
+        except ModuleNotFoundError:
+            self.skipTest("Cupy not available")
+        beam = Mock(Beam)
+        beam._dE = cp.ones(10)
+        beam._dt = cp.ones(10)
+        Beam.plot_hist(beam, axis=0)
+        plt.gcf().clf()
+
+
     def test_setup_beam(self) -> None:
         with self.assertRaises(AssertionError):
             self.beam.setup_beam(dE=np.ones(10), dt=np.ones(11))
