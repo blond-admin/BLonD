95c92d1558da0ff2939a3404e3942fc657a0f533
lthiele <leonard.thiele@cern.ch>
2025-11-10 18:16:47 +0100

    rollback of particle type changes

--- blond/_core/beam/particle_types.py
+++ blond/_core/beam/particle_types.py
@@ -86,22 +86,22 @@ class ParticleType:
         return self._quantum_radiation_constant
 
 
-proton = ParticleType(
+proton: ParticleType = ParticleType(
     mass=m_p * c**2 / e,
     charge=1,
 )
 
-uranium_29 = ParticleType(
+uranium_29: ParticleType = ParticleType(
     mass=238 * m_p * c**2 / e,  # approximate mass-energy in eV
     charge=29,
 )
 
-electron = ParticleType(
+electron: ParticleType = ParticleType(
     mass=m_e * c**2 / e,
     charge=-1,
 )
 
-positron = ParticleType(
+positron: ParticleType = ParticleType(
     mass=m_e * c**2 / e,
     charge=1,
 )
