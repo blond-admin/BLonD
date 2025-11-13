b357f5f307afe51c2760766db60105e69ae901a3
slauber <simon.fabian.lauber@cern.ch>
2025-11-13 15:14:50 +0100

    Hardened find_instances_with_method against mocking

--- blond/testing/mocks.py
+++ blond/testing/mocks.py
@@ -2,12 +2,22 @@
 
 from unittest.mock import Mock
 
-from blond import Beam, DriftSimple, Simulation, StaticProfile, WakeField
+from blond import (
+    Beam,
+    ConstantMagneticCycle,
+    DriftSimple,
+    Simulation,
+    SingleHarmonicRfStation,
+    StaticProfile,
+    WakeField,
+)
 
 simulation_mock = Mock(Simulation)
 
 beam_mock = Mock(Beam)
 static_profile_mock = Mock(StaticProfile)
 wakefield_profile_mock = Mock(WakeField)
+cycle_const_mock = Mock(ConstantMagneticCycle)
 
 drift_simple_mock = Mock(DriftSimple)
+single_harmonic_rf_station_mock = Mock(SingleHarmonicRfStation)
