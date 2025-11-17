9fe06f69414fc7a306629ed31729e10c6c864be2
slauber <simon.fabian.lauber@cern.ch>
2025-11-17 10:56:02 +0100

    Fixed filename

--- unittests/_core/backends/numba/test_callables.py
+++ unittests/_core/backends/numba/test_callables.py
@@ -1,11 +1,11 @@
-from unittest import TestCase
+import unittest
 
 import numpy as np
 
 from blond._core.backends.numba.callables import enforce_precision
 
 
-class TestCallables(TestCase):
+class TestCallables(unittest.TestCase):
     def test_enforce_precision(self):
         for floattype in (np.float32, np.float64):
 
@@ -15,3 +15,7 @@ class TestCallables(TestCase):
 
             res1 = foo(10.0, 20.0)
             self.assertEqual(type(res1), floattype)
+
+
+if __name__ == "__main__":
+    unittest.main()
