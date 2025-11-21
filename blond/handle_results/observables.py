adjusted masking to not append anymore but instead set to existing array, corrected test

blond/handle_results/observables.py
@@ -230,11 +230,6 @@ class ObservablesEndOfTurnBase(ObservablesBaseClass):
             self._turns_array[turn - turn_i_init] = turn + section_lengths
         self._turns_array = self._turns_array.flatten()
 
-    def assert_lateinit(self):
-        for parameter, value in self.__dict__.items():
-            if value is None:  # uninitialized
-                assert value is not None, f"`{parameter}` was not initialized."
-
 
 class BeamObservationEndOfTurn(ObservablesEndOfTurnBase):
     """Observe the bunch coordinates during simulation execution.
@@ -660,15 +655,15 @@ class MultiBunchObservationMetaParams(ObservablesEndOfTurnBase):
 
         if simulation.section_i.value in self._section_indices_to_observe:
             if self.recompute_mask or len(self._mask[0]) == 0:
-                self._mask = []
+                self._mask = np.zeros(
+                    (self.n_bunches, self._beam.common_array_size), dtype=bool
+                )
                 for bucket in range(self.n_bunches):
-                    self._mask.append(
-                        (self._beam._dt < self.t_rf * (bucket + 1))
-                        & (self._beam._dt > self.t_rf * bucket)
-                    )
+                    self._mask[bucket] = (
+                        self._beam._dt < self.t_rf * (bucket + 1)
+                    ) & (self._beam._dt > self.t_rf * bucket)
 
                 self._mask = np.array(self._mask, dtype=bool)
-
             backend.specials.meta_params_multibunch(
                 self._beam._dt,
                 self._beam._dE,

unittests/handle_results/test_observables.py
@@ -279,7 +279,7 @@ class TestBunchObservation(unittest.TestCase):
 class TestMultiBunchObservationMetaParams(unittest.TestCase):
     def setUp(self) -> None:
         self.multi_bunch_observation_meta_params = MultiBunchObservationMetaParams(
-            t_rf=1,
+            t_rf=2,
             each_turn_i=1,
             folder=callers_relative_path("results/", stacklevel=1),
             beam=beam,
@@ -287,13 +287,13 @@ class TestMultiBunchObservationMetaParams(unittest.TestCase):
 
     def test___init__(self) -> None:
         self.multi_bunch_observation_meta_params = MultiBunchObservationMetaParams(
-            t_rf=1,
+            t_rf=2,
             each_turn_i=1,
             folder=callers_relative_path("results/", stacklevel=1),
             beam=beam,
         )
 
-    def test_from_disk(self) -> None:
+    def test_from_disk_single_bunch(self) -> None:
         self.multi_bunch_observation_meta_params.on_init_simulation(
             simulation=simulation,
         )
@@ -308,6 +308,20 @@ class TestMultiBunchObservationMetaParams(unittest.TestCase):
         )
         self.multi_bunch_observation_meta_params.to_disk()
         self.multi_bunch_observation_meta_params.from_disk()
+
+        emittance = np.sqrt(np.mean(beam._dE ** 2)
+                              * np.mean(beam._dt ** 2)
+                              - np.mean(beam._dE * beam._dt) ** 2)
+        assert np.isclose(self.multi_bunch_observation_meta_params.emittance_stat,
+                          emittance)
+        assert np.isclose(self.multi_bunch_observation_meta_params.sigma_dE,
+                          np.std(beam._dE))
+        assert np.isclose(self.multi_bunch_observation_meta_params.sigma_dt,
+                          np.std(beam._dt))
+        assert np.isclose(self.multi_bunch_observation_meta_params.mean_dE,
+                          np.mean(beam._dE))
+        assert np.isclose(self.multi_bunch_observation_meta_params.mean_dt,
+                          np.mean(beam._dt))
 
 
 class TestCavityPhaseObservation(unittest.TestCase):