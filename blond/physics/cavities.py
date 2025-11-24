f676a672731580ce04f2af3df29221d09e40f2a5
slauber <simon.fabian.lauber@cern.ch>
2025-11-24 16:24:05 +0100

    Fixed LICENCE file format

--- blond/physics/cavities.py
+++ blond/physics/cavities.py
@@ -1,6 +1,6 @@
 # Copyright CERN. This software is distributed under the
 # terms of the GNU General Public Licence version 3 (GPL Version 3),
-# copied verbatim in the file LICENCE.md.
+# copied verbatim in the file LICENCE.txt.
 # In applying this licence, CERN does not waive the privileges and immunities
 # granted to it by virtue of its status as an Intergovernmental Organization or
 # submit itself to any jurisdiction.
