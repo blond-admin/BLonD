881aad7313a3a7a04c158a07b2577775d62139fb
slauber <simon.fabian.lauber@cern.ch>
2025-11-24 17:27:21 +0100

    Fixed docstrings.

--- blond/generals/hashing_.py
+++ blond/generals/hashing_.py
@@ -6,6 +6,13 @@
 # submit itself to any jurisdiction.
 # Project website: http://blond.web.cern.ch/
 
+"""Functions that help with hashing.
+
+Authors
+-------
+Simon Lauber
+"""
+
 from __future__ import annotations
 
 import hashlib
