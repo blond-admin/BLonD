1623cd45e47a52b0c76a782a93d2d82291fac7b7
slauber <simon.fabian.lauber@cern.ch>
2025-11-24 15:08:29 +0100

    Added copyright notes.

--- blond/core/backends/fortran/kick_32.f90
+++ blond/core/backends/fortran/kick_32.f90
@@ -1,3 +1,11 @@
+! Copyright CERN. This software is distributed under the
+! terms of the GNU General Public Licence version 3 (GPL Version 3),
+! copied verbatim in the file LICENCE.md.
+! In applying this licence, CERN does not waive the privileges and immunities
+! granted to it by virtue of its status as an Intergovernmental Organization or
+! submit itself to any jurisdiction.
+! Project website: http://blond.web.cern.ch/
+
 ! kick.f90
 subroutine kick_single_harmonic(dt, dE, voltage, omega_rf, phi_rf, charge, acceleration_kick, n)
    implicit none
--- blond/core/base.py
+++ blond/core/base.py
@@ -1,3 +1,11 @@
+# Copyright CERN. This software is distributed under the
+# terms of the GNU General Public Licence version 3 (GPL Version 3),
+# copied verbatim in the file LICENCE.md.
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization or
+# submit itself to any jurisdiction.
+# Project website: http://blond.web.cern.ch/
+
 """Internal mix-ins to define all `Simulation` relevant classes."""
 
 from __future__ import annotations
