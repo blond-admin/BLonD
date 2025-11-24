bf0955126082727beb93d4afc7bc01b20ebe8723
Simon Lauber <simon.fabian.lauber@cern.ch>
2025-11-24 16:53:31 +0100

    Added copyright notice

--- blond/core/backends/fortran/drift_32.f90
+++ blond/core/backends/fortran/drift_32.f90
@@ -1,3 +1,11 @@
+! Copyright CERN. This software is distributed under the
+! terms of the GNU General Public Licence version 3 (GPL Version 3),
+! copied verbatim in the file LICENCE.txt.
+! In applying this licence, CERN does not waive the privileges and immunities
+! granted to it by virtue of its status as an Intergovernmental Organization or
+! submit itself to any jurisdiction.
+! Project website: http://blond.web.cern.ch/
+
 ! drift.f90
 subroutine drift_simple(dt, dE, T, eta_0, beta, energy, n)
    implicit none
--- blond/examples/EX_05_Wake_impedance.py
+++ blond/examples/EX_05_Wake_impedance.py
@@ -1,12 +1,12 @@
-# pragma: no cover
-
-# Copyright 2014-2017 CERN. This software is distributed under the
+# Copyright CERN. This software is distributed under the
 # terms of the GNU General Public Licence version 3 (GPL Version 3),
-# copied verbatim in the file LICENCE.md.
+# copied verbatim in the file LICENCE.txt.
 # In applying this licence, CERN does not waive the privileges and immunities
 # granted to it by virtue of its status as an Intergovernmental Organization or
 # submit itself to any jurisdiction.
 # Project website: http://blond.web.cern.ch/
+
+# pragma: no cover
 
 """SPS simulation with intensity effects in time and frequency domains using
 a table of resonators. The input beam has been cloned to show that the two
