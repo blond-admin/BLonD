94aa691b60568ae3edd15f8883249c8d47173608
lthiele <leonard.thiele@cern.ch>
2025-11-27 21:54:35 +0100

    added lead ions

--- blond/core/beam/particle_types.py
+++ blond/core/beam/particle_types.py
@@ -106,6 +106,11 @@ uranium_29: ParticleType = ParticleType(
     charge=29,
 )
 
+lead_82: ParticleType = ParticleType(
+    mass=207.93 * m_p * c**2 / e,  # approximate mass-energy in eV
+    charge=82,
+)
+
 electron: ParticleType = ParticleType(
     mass=m_e * c**2 / e,
     charge=-1,
