75bf1b1a2f01e9f73dccafd66ae4932a3ea4e4a7
phickers <paula.hickersberger@cern.ch>
2025-11-28 11:46:21 +0100

    Delete moved function in test_profile

--- unittests/physics/test_profiles.py
+++ unittests/physics/test_profiles.py
@@ -12,14 +12,12 @@ import numpy as np
 from scipy.stats import norm
 
 from blond import Beam, backend, uranium_29
+from blond.acc_math.empiric.empiric import gauss_fit, multi_gauss_fit
 from blond.physics.profiles import (
     DynamicProfileConstCutoff,
     DynamicProfileConstNBins,
     ProfileBaseClass,
     StaticProfile,
-    gauss,
-    gauss_fit,
-    multi_gauss_fit,
 )
 
 
@@ -238,49 +236,6 @@ class TestDynamicProfileConstNBins(unittest.TestCase):
             np.zeros(10),
             self.dynamic_profile_const_cutoff.hist_y,
         )
-
-
-def test_gauss_fit():
-    x = np.arange(-4,4,0.001)
-    p = [0.4,0,1]
-
-    gauss_test = norm.pdf(x, p[1], p[2])
-
-    fit = gauss_fit( x, gauss_test)
-
-    np.testing.assert_almost_equal(
-        fit,
-        p,
-        decimal=2
-    )
-
-def test_multi_gauss_fit():
-    x = np.arange(-4,12,0.001)
-    p = np.array([[0.4,0,1],
-            [0.4,8,1]])
-
-    gauss_test = norm.pdf(x, p[0,1], p[0,2]) +  norm.pdf(x, p[1,1], p[1,2])
-
-    fit = multi_gauss_fit(x, gauss_test, n_bunches = 2)
-
-    np.testing.assert_almost_equal(
-        fit,
-        p,
-        decimal=2
-    )
-
-def test_gauss():
-    x = np.arange(-4,4,0.001)
-    p = [0.4,0,1]
-
-    gauss_func = gauss(x,p[0],p[1], p[2])
-    gauss_test = norm.pdf(x, p[1], p[2])
-
-    np.testing.assert_almost_equal(
-        gauss_func,
-        gauss_test,
-        decimal=2
-    )
 
 if __name__ == "__main__":
     unittest.main()
