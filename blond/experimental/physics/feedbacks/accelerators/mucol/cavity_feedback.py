283b661f4bee914996b8bbfbae31e16220503375
lthiele <leonard.thiele@cern.ch>
2025-12-05 16:11:34 +0100

    first iteration at init of mucol cavity feedback

--- blond/experimental/physics/feedbacks/accelerators/mucol/cavity_feedback.py
+++ blond/experimental/physics/feedbacks/accelerators/mucol/cavity_feedback.py
@@ -8,25 +8,98 @@
 
 from warnings import warn
 
+from blond import StaticProfile
 from blond.experimental.physics.feedbacks.cavity_feedback import (
     IQCavityFeedback,
 )
-from blond.physics.profiles import ProfileBaseClass
 
 
 class PassiveCavity(IQCavityFeedback):
+    """
+    Passive Cavity, implementing the beam-cavity interaction formulas without a feedback involved.
 
+            Parameters
+        ----------
+        profile
+            profile on which the feedback should act
+        R_over_Q
+            shunt impedance over quality factor of one cavity [$$\Omega$$]
+        Q_L
+            Loaded quality factor of one cavity [1]
+        f_center
+            center frequency of the cavity [Hz]
+        f_detuning
+            detuning of the cavity [Hz]
+        n_cavities
+            number of cavities
+        generator_current
+            given in [A]
+        generator_phase
+            given in [rad]
+        injection_phase
+            In :func:`xxxx` the cavity will optimise the phase at injection towards
+            this value by adjusting the initial parameters at the beginning of the internal tracking given in [rad]
+        injection_voltage
+            In :func:`xxxx` the cavity will optimise the voltage at injection towards
+            this value by adjusting the initial parameters at the beginning of the internal tracking given in [V]
+        harmonic_index
+            only the default of 0 is allowed
+        n_periods_coarse
+            number of RF periods, one coarse grid corresponds to
+        section_index
+            section which the feedback belongs to
+        use_lowpass_filter
+            Used in :func:xxx
+        name
+            If not given, is automatically chosen
+    """
     def __init__(self,
-                 profile: ProfileBaseClass,  # is this stricly necessary?
+                 profile: StaticProfile,  # is this stricly necessary?
+                 R_over_Q: float,
+                 Q_L: float,
+                 f_center: float,
+                 f_detuning: float,
+                 n_cavities: int,
+                 generator_current: float,
+                 generator_phase: float = 0,
+                 injection_phase: float = -1,
+                 injection_voltage: float = -1,
                  harmonic_index: int = 0,
+                 n_periods_coarse: int = 1,
                  section_index: int = 0,
                  use_lowpass_filter: bool = False,
                  name: str | None = None) -> None:
+        if harmonic_index != 0:
+            raise NotImplementedError("harmonic indices other than 0 are not supported with this module")
+
+        assert R_over_Q >= 0, "R_over_Q must be >= 0"
+        self.R_over_Q = R_over_Q
+
+        assert Q_L >= 0.5, "Q_L must be >= 0.5"
+        self.Q_L = Q_L
+
+        assert f_center >= 0, "f_center must be >= 0"  # TODO: does this make sense here?
+        self.f_center = f_center
+
+        assert f_detuning >= 0, "fset must be >= 0"
+        self.f_detuning = f_detuning
+
+        assert n_cavities > 0, "n_cavities must be > 0"
+        self.n_cavities = n_cavities
+
+        assert generator_current > 0, "i_g must be > 0"
+        self.generator_current = generator_current
+
+        self.generator_phase = generator_phase
+        self.injection_phase = injection_phase
+        self.injection_voltage = injection_voltage
+
         if use_lowpass_filter:
             warn("lowpass filter is not used in this class")
 
         super().__init__(profile=profile,
-                         _parent_cavity=None,
-                         n_cavities=None,
-                         section_index=section_index,
-                         name=name)
+                         n_cavities=n_cavities,
+                         section_index=section_index,  # TODO: this should not be necessary or? The parent cavity already has this information
+                         name=name,
+                         n_periods_coarse=n_periods_coarse,
+                         harmonic_index=harmonic_index)
