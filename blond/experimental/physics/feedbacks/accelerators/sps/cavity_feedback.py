232114b01c822a0031c07afd38977e9ef63e2d71
Birk Emil Karlsen-Baeck <birk.beck@cern.ch>
2025-12-11 20:29:06 +0100

    [blond][experimental][physics][feedbacks][accelerators][sps][cavity feedback] Minor fixes the SPS 200 MHz cavity controller model

--- blond/experimental/physics/feedbacks/accelerators/sps/cavity_feedback.py
+++ blond/experimental/physics/feedbacks/accelerators/sps/cavity_feedback.py
@@ -308,7 +308,7 @@ class SPSOneTurnFeedback(IQCavityFeedback):
         # Initialize moving average
         self.n_mov_av = round(
             self.TWC.tau
-            / self._parent_rf_station.get_main_harmonic_t_rf_current()
+            / self._parent_rf_station.get_main_harmonic_t_rf_actual()
         )
         self.DV_MOV_AVG = np.zeros(2 * self.n_coarse, dtype=complex)
         self.logger.debug("Moving average over %d points", self.n_mov_av)
@@ -724,7 +724,7 @@ class SPSOneTurnFeedback(IQCavityFeedback):
         # TODO REMWORK/REMOVE
         t_rev = float(
             (2 * np.pi * self._parent_rf_station.harmonic[self.harmonic_index])
-            / self._parent_rf_station.omega_rf[self.harmonic_index]
+            / self._parent_rf_station.omega_rf_actual[self.harmonic_index]
         )
         # TODO REMWORK/REMOVE
         t_rf = t_rev / float(
