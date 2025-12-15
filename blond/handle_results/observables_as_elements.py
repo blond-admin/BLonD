4ceeb5a64c4583ad3e70151ba5bda841faed1ba6
slauber <simon.fabian.lauber@cern.ch>
2025-12-15 14:16:35 +0100

    Bugfix

--- blond/handle_results/observables_as_elements.py
+++ blond/handle_results/observables_as_elements.py
@@ -315,11 +315,11 @@ class BunchObservationMetaParams(BeamObservationElement, ObservablesBaseClass):
             Beam class to interact with this element.
         """
         if self._beam_id_filter is None or self._beam_id_filter == id(beam):
-            self._sigma_dt.write(np.std(beam.read_partial_dt()))
-            self._sigma_dE.write(np.std(beam.read_partial_dE()))
-            self._mean_dt.write(np.mean(beam.read_partial_dt()))
-            self._mean_dE.write(np.mean(beam.read_partial_dE()))
-            self._rms_emittance.write(
+            self._sigma_dt.write(np.std(beam._dt.std()))
+            self._sigma_dE.write(np.std(beam._dE.std()))
+            self._mean_dt.write(beam._dt.mean())
+            self._mean_dE.write(beam._dE.mean())
+            self._rms_emittance.write(  # FIXME make distributed
                 np.sqrt(
                     np.average(beam.read_partial_dE() ** 2)
                     * np.average(
