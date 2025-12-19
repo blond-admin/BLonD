69a0c2039c3d75558be610792446d6e34302faf8
lvalle <lina.valle@cern.ch>
2025-12-19 15:53:02 +0100

    ParticleTypeClassicalRadius class

--- blond/core/beam/particle_types.py
+++ blond/core/beam/particle_types.py
@@ -97,6 +97,83 @@ class ParticleType:
         return self._mass_inv
 
 
+class ParticleTypeClassicalRadius(ParticleType):
+    """
+    Represent a particle type with physical constants with its classical radius.
+
+    Parameters
+    ----------
+    mass : float
+        Rest mass energy of the particle, in [eV].
+    charge : float
+        Number of electric charges of the particle, in [].
+    user_decay_rate : float, optional
+        Optional user-specified decay rate. Default is 0.0.
+    """
+
+    def __init__(
+        self, mass: float, charge: float, user_decay_rate: float = 0.0
+    ):
+        super().__init__(
+            mass=mass, charge=charge, user_decay_rate=user_decay_rate
+        )
+
+        radius_cl = (
+            0.25
+            / (np.pi * epsilon_0)
+            * e**2
+            * self.charge**2
+            / (self.mass * e)
+        )
+        self._classical_particle_radius = radius_cl
+
+        # Sand's radiation constant [m / eV^3]
+        c_gamma = (
+            4 * np.pi / 3 * self._classical_particle_radius / self.mass**3
+        )
+        self._sands_radiation_constant = c_gamma
+
+        # Quantum radiation constant [m]
+        c_q = 55.0 / (32.0 * np.sqrt(3.0)) * hbar * c / (self.mass * e)
+        self._quantum_radiation_constant = c_q
+
+    @property
+    def classical_particle_radius(self) -> float:
+        """
+        Classical particle radius [m].
+
+        Returns
+        -------
+        classical_particle_radius
+            Classical particle radius [m].
+        """
+        return self._classical_particle_radius
+
+    @property
+    def sands_radiation_constant(self) -> float:
+        """
+        Return Sand's radiation constant [ m / eV^3].
+
+        Returns
+        -------
+        sands_radiation_constant
+            Sand's radiation constant [ m / eV^3].
+        """
+        return self._sands_radiation_constant
+
+    @property
+    def quantum_radiation_constant(self) -> float32 | float64:
+        """
+        Quantum radiation constant [m].
+
+        Returns
+        -------
+        quantum_radiation_constant
+            Quantum radiation constant [m].
+        """
+        return self._quantum_radiation_constant
+
+
 proton: ParticleType = ParticleType(
     mass=m_p * c**2 / e,
     charge=1,
@@ -110,6 +187,16 @@ uranium_29: ParticleType = ParticleType(
 lead_82: ParticleType = ParticleType(
     mass=207.93 * m_p * c**2 / e,  # approximate mass-energy in eV
     charge=82,
+)
+
+electron: ParticleType = ParticleTypeClassicalRadius(
+    mass=m_e * c**2 / e,
+    charge=-1,
+)
+
+positron: ParticleType = ParticleTypeClassicalRadius(
+    mass=m_e * c**2 / e,
+    charge=1,
 )
 
 _muon_decay_rate = float(1 / 2.1969811e-6)
@@ -126,127 +213,3 @@ mu_minus: ParticleType = ParticleType(
     charge=-1,
     user_decay_rate=_muon_decay_rate,
 )
-
-
-class electron(ParticleType):
-    """Electron ParticleType class."""
-
-    def __init__(self):
-        super().__init__(mass=m_e * c**2 / e, charge=-1)
-
-        radius_cl = (
-            0.25
-            / (np.pi * epsilon_0)
-            * e**2
-            * self.charge**2
-            / (self.mass * e)
-        )
-        self._classical_particle_radius = radius_cl
-
-        # Sand's radiation constant [m / eV^3]
-        c_gamma = (
-            4 * np.pi / 3 * self._classical_particle_radius / self.mass**3
-        )
-        self._sands_radiation_constant = c_gamma
-
-        # Quantum radiation constant [m]
-        c_q = 55.0 / (32.0 * np.sqrt(3.0)) * hbar * c / (self.mass * e)
-        self._quantum_radiation_constant = c_q
-
-    @property
-    def classical_particle_radius(self) -> float:
-        """
-        Classical particle radius [m].
-
-        Returns
-        -------
-        classical_particle_radius
-            Classical particle radius [m].
-        """
-        return self._classical_particle_radius
-
-    @property
-    def sands_radiation_constant(self) -> float:
-        """
-        Return Sand's radiation constant [ m / eV^3].
-
-        Returns
-        -------
-        sands_radiation_constant
-            Sand's radiation constant [ m / eV^3].
-        """
-        return self._sands_radiation_constant
-
-    @property
-    def quantum_radiation_constant(self) -> float32 | float64:
-        """
-        Quantum radiation constant [m].
-
-        Returns
-        -------
-        quantum_radiation_constant
-            Quantum radiation constant [m].
-        """
-        return self._quantum_radiation_constant
-
-
-class positron(ParticleType):
-    """Positron ParticleType class."""
-
-    def __init__(self):
-        super().__init__(mass=m_e * c**2 / e, charge=1)
-
-        radius_cl = (
-            0.25
-            / (np.pi * epsilon_0)
-            * e**2
-            * self.charge**2
-            / (self.mass * e)
-        )
-        self._classical_particle_radius = radius_cl
-
-        # Sand's radiation constant [m / eV^3]
-        c_gamma = (
-            4 * np.pi / 3 * self._classical_particle_radius / self.mass**3
-        )
-        self._sands_radiation_constant = c_gamma
-
-        # Quantum radiation constant [m]
-        c_q = 55.0 / (32.0 * np.sqrt(3.0)) * hbar * c / (self.mass * e)
-        self._quantum_radiation_constant = c_q
-
-    @property
-    def classical_particle_radius(self) -> float:
-        """
-        Classical particle radius [m].
-
-        Returns
-        -------
-        classical_particle_radius
-            Classical particle radius [m].
-        """
-        return self._classical_particle_radius
-
-    @property
-    def sands_radiation_constant(self) -> float:
-        """
-        Return Sand's radiation constant [ m / eV^3].
-
-        Returns
-        -------
-        sands_radiation_constant
-            Sand's radiation constant [ m / eV^3].
-        """
-        return self._sands_radiation_constant
-
-    @property
-    def quantum_radiation_constant(self) -> float32 | float64:
-        """
-        Quantum radiation constant [m].
-
-        Returns
-        -------
-        quantum_radiation_constant
-            Quantum radiation constant [m].
-        """
-        return self._quantum_radiation_constant
