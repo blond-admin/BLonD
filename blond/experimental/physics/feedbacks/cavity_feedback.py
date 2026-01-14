fixed backend casting, fixed computation of carrier frequency

blond/experimental/physics/feedbacks/cavity_feedback.py
@@ -224,7 +224,7 @@ class IQCavityFeedback(LocalFeedback):
         t_rf = t_rev / float(harmonic)
 
         self.n_coarse = round(t_rev / self.T_s)  # TODO: round or ceil?
-        self.omega_carrier = omega_rf / self.n_coarse
+        self.omega_carrier = omega_rf / self.n_periods_coarse
         # FIXME NO REDECLARATION!
 
         self.omega_rf = float(omega_rf)
@@ -257,7 +257,7 @@ class IQCavityFeedback(LocalFeedback):
         t_rf = 2 * np.pi / omega_rf
 
         self.n_coarse = round(t_rev / self.T_s)
-        self.omega_carrier = omega_rf / self.n_coarse
+        self.omega_carrier = omega_rf / self.n_periods_coarse
         # FIXME NO REDECLARATION!
 
         self.omega_rf = float(omega_rf)
@@ -390,7 +390,7 @@ class IQCavityFeedback(LocalFeedback):
 
         # Present carrier frequency: main RF frequency
         self.omega_carrier_prev = self.omega_carrier
-        self.omega_carrier = self.omega_rf / self.n_coarse
+        self.omega_carrier = self.omega_rf / self.n_periods_coarse
 
         # Present sampling time
         self.T_s_prev = self.T_s

blond/physics/cavities.py
@@ -1616,9 +1616,9 @@ class MultiHarmonicRfStation(RfStationBaseClass):
         from blond.cycles.magnetic_cycle import ConstantMagneticCycle
 
         multi_harmonic_rf_station = MultiHarmonicRfStation(
-            harmonic=np.array(harmonic, dtype=backend.float),
-            voltage=np.array(voltage, dtype=backend.float),
-            phi_rf=np.array(phi_rf, dtype=backend.float),
+            harmonic=np.array(harmonic, dtype=float),
+            voltage=np.array(voltage, dtype=float),
+            phi_rf=np.array(phi_rf, dtype=float),
             n_harmonics=len(voltage),
             section_index=section_index,
             local_wakefield=local_wakefield,

unittests/physics/test_cavities.py
@@ -273,9 +273,9 @@ class TestMultiHarmonicCavity(unittest.TestCase):
 
         self.multi_harmonic_cavity = MultiHarmonicRfStation.headless(
             section_index=0,
-            voltage=np.array([1e6, 2e6], dtype=backend.float),
-            phi_rf=np.array([0.1 * np.pi, np.pi], dtype=backend.float),
-            harmonic=np.array([1, 5], dtype=backend.float),
+            voltage=np.array([1e6, 2e6], dtype=float),
+            phi_rf=np.array([0.1 * np.pi, np.pi], dtype=float),
+            harmonic=np.array([1, 5], dtype=float),
             circumference=456,
             local_wakefield=None,
             cavity_feedback=None,