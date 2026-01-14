85cf09434ed29b67a96b3467e0c4e91dc059e9d7
lthiele <leonard.thiele@cern.ch>
2026-01-14 13:22:46 +0100

    documentation, adjustment of import statements, added internal checks, added relative voltage corrections and phase corrections

--- blond/physics/feedbacks/base.py
+++ blond/physics/feedbacks/base.py
@@ -6,6 +6,16 @@
 # submit itself to any jurisdiction.
 # Project website: http://blond.web.cern.ch/
 
+"""
+Collection of implementations to model longitudinal feedbacks.
+
+Notes
+-----
+Authors:
+Birk Karlsen Baeck
+Leonard Thiele
+"""
+
 from __future__ import annotations
 
 from abc import abstractmethod
@@ -15,6 +25,8 @@ from blond.core.base import BeamPhysicsRelevant
 from blond.core.ring.helpers import requires
 
 if TYPE_CHECKING:  # pragma: no cover
+    from numpy.typing import NDArray as NumpyArray
+
     from blond.core.beam.base import BeamBaseClass
     from blond.core.simulation.simulation import Simulation
     from blond.physics.cavities import (
@@ -26,6 +38,17 @@ if TYPE_CHECKING:  # pragma: no cover
 
 
 class FeedbackBaseClass(BeamPhysicsRelevant):
+    """
+    Baseclass for implementation of feedback elements.
+
+    Parameters
+    ----------
+    section_index
+        Section index of the feedback.
+    name
+        Name of the feedback.
+    """
+
     def __init__(
         self,
         section_index: int = 0,
@@ -35,44 +58,104 @@ class FeedbackBaseClass(BeamPhysicsRelevant):
 
 
 class LocalFeedback(FeedbackBaseClass):
+    """
+    Baseclass for implementation of local feedback elements.
+
+    Baseclass for implementation of local feedback elements.
+    This will be attached to a specific multi- or single-harmonic
+    cavity, which will act on the beam.
+
+    Parameters
+    ----------
+    profile
+        Profile the feedback should act on.
+    name
+        Name of the feedback.
+
+    Attributes
+    ----------
+    phase_correction
+        Correction to the rf phase, has to be defined on the
+        profile time grid.
+    relative_voltage_correction
+        Relative correction to the setpoint voltage
+        stemming from the feedback,
+        has to be defined on the profile time grid.
+    """
+
     def __init__(
         self,
         profile: ProfileBaseClass,
-        section_index: int = 0,
         name: str | None = None,
     ):
         super().__init__(
-            section_index=section_index,
             name=name,
         )
         self._parent_rf_station: (
             SingleHarmonicRfStation | MultiHarmonicRfStation | None
         ) = None
+
+        self.relative_voltage_correction: NumpyArray | None = None
+        self.phase_correction: NumpyArray | None = None
+
         self.profile = profile
 
-    def set_parent_rf_station(self, rf_station: RfStationBaseClass):
+    def set_parent_rf_station(
+        self, rf_station: MultiHarmonicRfStation | SingleHarmonicRfStation
+    ) -> None:
+        """
+        Set the parent RF station on initialization of the rf_station.
+
+        Parameters
+        ----------
+        rf_station
+            Cavity to be the parent rf station.
+        """
+        from blond.physics.cavities import (  # no cyclic import
+            MultiHarmonicRfStation,
+            SingleHarmonicRfStation,
+        )
+
         assert self._parent_rf_station is None, (
             "This feedback has already one owner!"
         )
+        if not isinstance(
+            rf_station, SingleHarmonicRfStation | MultiHarmonicRfStation
+        ):
+            raise ValueError(
+                f"Local feedbacks can only be initialized with SingleHarmonicRfStation "
+                f"or MultiHarmonicRfStation but not {type(rf_station)}"
+            )
         self._parent_rf_station = rf_station
+        self._section_index = self._parent_rf_station.section_index
 
     @abstractmethod  # pragma: no cover
     def track(self, beam: BeamBaseClass) -> None:
         """
-        Main simulation routine to be called in the mainloop
+        Main simulation routine to be called in the mainloop.
 
         Parameters
         ----------
         beam
-            Beam class to interact with this element
+            Beam class to interact with this element.
         """
         pass
 
 
-RfFeedback = LocalFeedback  # just an alias name
-
-
 class GlobalFeedback(FeedbackBaseClass):
+    """
+    Baseclass for implementation of global feedback elements.
+
+    Parameters
+    ----------
+    profile
+        Profile on which the feedback should work, needs to be tracked before.
+    section_index
+        Section index of the feedback.
+    name
+        Name of the feedback.
+    """
+
     def __init__(
         self,
         profile: ProfileBaseClass,
@@ -88,33 +171,18 @@ class GlobalFeedback(FeedbackBaseClass):
 
     # Use `requires` to automatically sort execution order of
     # `element.on_init_simulation` for all elements
-    @requires(["SingleHarmonicRfStation"])
+    @requires(["RfStationBaseClass"])
     def on_init_simulation(self, simulation: Simulation) -> None:
         """
-        Lateinit method when `simulation.__init__` is called
+        Lateinit method when `simulation.__init__` is called.
 
+        Parameters
+        ----------
         simulation
-            `Simulation` context manager
+            `Simulation` context manager.
         """
+        from blond.physics.cavities import RfStationBaseClass
+
         self.cavities = simulation.ring.elements.get_elements(
-            SingleHarmonicRfStation
+            RfStationBaseClass,
         )
-
-
-BeamFeedback = GlobalFeedback  # just an alias name
-
-
-class GroupedFeedback(FeedbackBaseClass):
-    def __init__(
-        self,
-        profile: ProfileBaseClass,
-        cavities: list[SingleHarmonicRfStation | MultiHarmonicRfStation],
-        section_index: int = 0,
-        name: str | None = None,
-    ):
-        super().__init__(
-            section_index=section_index,
-            name=name,
-        )
-        self.profile = profile
-        self.cavities = cavities
