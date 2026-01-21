a9ab07ce0848ff3024cc67302a4c6420a17e4029
elamb <elamb@cern.ch>
2026-01-21 14:55:20 +0100

    added example code in docs

--- blond/experimental/beam_preparation/brute_force_matcher.py
+++ blond/experimental/beam_preparation/brute_force_matcher.py
@@ -58,6 +58,109 @@ class BruteForceMatcher(MatchingRoutine):
         Lower and upper bounds in energy deviation, in [eV]. Used to purge
         particles when ``purge=True``. If ``None``, no energy-based purging
         is applied.
+
+    Example
+    -------
+
+    >>> from blond import (
+    ...     Beam,
+    ...     ConstantMagneticCycle,
+    ...     DriftSimple,
+    ...     Ring,
+    ...     Simulation,
+    ...     SingleHarmonicRfStation,
+    ...     proton,
+    ... )
+    >>> from blond.experimental.beam_preparation.brute_force_matcher import (
+    ...     BruteForceMatcher,
+    ... )
+    >>> from blond.handle_results.observables_as_elements import (
+    ...     BeamObservationInRingElement,
+    ... )
+
+    >>> def main():
+    ...     # Simulation parameters -------------------------------------------------------
+    ...     p_s = 450.0e9  # Synchronous momentum [eV]
+    ...     harmonic_number = 35640  # Harmonic number
+    ...     voltage1 = 2e6  # RF voltage, station 1 [eV]
+    ...     voltage2 = 4e6  # RF voltage, station 2 [eV]
+    ...     phi_rf = 0  # Phase modulation/offset
+    ...     transition_gamma = 55.759505  # Transition gamma
+    ...
+    ...     energy_cycle = ConstantMagneticCycle(
+    ...         value=p_s,
+    ...         reference_particle=proton,
+    ...     )
+    ...     ring = Ring(
+    ...         circumference=26658.883,
+    ...     )
+    ...     beam = Beam(
+    ...         intensity=1.0e9,
+    ...         particle_type=proton,
+    ...     )
+    ...
+    ...     observation = BeamObservationInRingElement(
+    ...         each_turn_i=1,
+    ...         section_index=0,
+    ...         n_turns=10,
+    ...         folder="./",
+    ...     )
+    ...
+    ...     one_turn_execution_order = (
+    ...         DriftSimple(
+    ...             transition_gamma=transition_gamma,
+    ...             orbit_length=0.3 * ring.circumference,
+    ...             section_index=0,
+    ...         ),
+    ...         SingleHarmonicRfStation(
+    ...             harmonic=harmonic_number,
+    ...             phi_rf=phi_rf,
+    ...             voltage=voltage1,
+    ...             section_index=0,
+    ...         ),
+    ...         observation,
+    ...         DriftSimple(
+    ...             transition_gamma=transition_gamma,
+    ...             orbit_length=0.7 * ring.circumference,
+    ...             section_index=1,
+    ...         ),
+    ...         SingleHarmonicRfStation(
+    ...             harmonic=harmonic_number,
+    ...             phi_rf=phi_rf,
+    ...             voltage=voltage2,
+    ...             section_index=1,
+    ...         ),
+    ...     )
+    ...
+    ...     ring.add_elements(one_turn_execution_order, reorder=False)
+    ...     sim = Simulation(ring=ring, magnetic_cycle=energy_cycle)
+    ...
+    ...     sim.prepare_beam(
+    ...         preparation_routine=BruteForceMatcher(
+    ...             time_limit=[0.1e-9, 4e-9],
+    ...             energy_limit=[-4e8, 4e8],
+    ...             n_macroparticles=3000,
+    ...             n_iter=100,
+    ...             every_iter_to_plot=10,
+    ...             animate=True,
+    ...             purge_limit_time=[0.1e-9, 4e-9],
+    ...             purge_limit_energy=[-4e8, 4e8],
+    ...             purge=True,
+    ...         ),
+    ...         beam=beam,
+    ...     )
+    ...
+    ...     plt.show()
+    ...
+    ...     sim.run_simulation(
+    ...         n_turns=20,
+    ...         beams=(beam,),
+    ...     )
+    ...
+    ...     plt.close()
+    ...     plt.scatter(observation.dts[0], observation.dEs[0])
+    ...     plt.scatter(observation.dts[0], observation.dEs[0])
+    ...     plt.show()
     """
 
     def __init__(
