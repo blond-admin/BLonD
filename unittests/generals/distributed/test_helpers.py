afcbb35a9bb1602fd37aa31ac9841e91187b1d87
slauber <simon.fabian.lauber@cern.ch>
2026-01-23 12:48:42 +0100

    Added testcase

--- unittests/generals/distributed/test_helpers.py
+++ unittests/generals/distributed/test_helpers.py
@@ -6,7 +6,9 @@ import numpy as np
 import pytest
 
 from blond.generals.distributed.helpers import (
+    MPI_RANK,
     distributed_arange,
+    mpi_aware_random_generator_cpu,
     mpi_is_distributed,
     mpi_is_root,
 )
@@ -54,6 +56,33 @@ class TestCallablesWithMPI(unittest.TestCase):
             self.assertTrue(mpi_is_root())
         if da._rank == 1:
             self.assertFalse(mpi_is_root())
+
+    def test_mpi_aware_random_generator_cpu(self):
+        seed = 1
+        size_global = 12
+        size_local = 6  # assume `mpirun -n 2`
+        # not distributed
+        random_generator_not_distributed = np.random.default_rng(
+            seed=seed,
+        )
+        array_expected = random_generator_not_distributed.standard_normal(
+            size=size_global
+        )
+
+        # distributed
+        random_generator_distributed = mpi_aware_random_generator_cpu(
+            seed=seed, n_forward_per_rank=size_local
+        )
+        array_local = random_generator_distributed.standard_normal(
+            size=size_local
+        )
+
+        # compare
+
+        if MPI_RANK == 0:
+            np.testing.assert_equal(array_expected[0:6], array_local)
+        if MPI_RANK == 1:
+            np.testing.assert_equal(array_expected[6:12], array_local)
 
 
 class TestCallablesNoMPI(unittest.TestCase):
