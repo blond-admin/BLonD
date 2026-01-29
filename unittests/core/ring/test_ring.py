c5360c7959ef4da812affd8aad3a46a14f381c33
lvalle <lina.valle@cern.ch>
2026-01-29 09:50:43 +0100

    added a comparison test to fail, between the transition gamma and the average_transition_gamma for BLonD3 discussion

--- unittests/core/ring/test_ring.py
+++ unittests/core/ring/test_ring.py
@@ -516,6 +516,36 @@ class TestRing(unittest.TestCase):
 
         self.assertEqual(self.ring.momentum_compaction_factor, 0.002)
 
+    def test_transition_gamma(self):
+        self.ring._circumference = 100
+
+        from blond.testing.mocks import drift_simple_mock
+
+        drift_simple_mock.momentum_compaction_factor = 1e-3
+        drift_simple_mock.orbit_length = 50
+        drift_simple_mock.section_index = 0
+
+        self.ring.add_element(drift_simple_mock, deepcopy=True)
+        self.ring.add_element(drift_simple_mock, deepcopy=True)
+
+        self.assertEqual(self.ring.transition_gamma, np.sqrt(1 / 0.002))
+
+    def test_compare_average_transition_gamma_with_transition_gamma(self):
+        self.ring._circumference = 129
+        from blond.testing.mocks import drift_simple_mock
+
+        drift_simple_mock.momentum_compaction_factor = 1e-3
+        drift_simple_mock.transition_gamma = np.sqrt(1 / 1e-3)
+        drift_simple_mock.orbit_length = 12
+        drift_simple_mock.section_index = 0
+
+        self.ring.add_element(drift_simple_mock, deepcopy=True)
+        self.ring.add_element(drift_simple_mock, deepcopy=True)
+
+        self.assertEqual(
+            self.ring.transition_gamma, self.ring.average_transition_gamma
+        )
+
     def test_average_transition_gamma(self):
         self.ring._circumference = 129
         from blond.testing.mocks import drift_simple_mock
