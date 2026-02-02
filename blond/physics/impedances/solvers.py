acdd23a1818f82edc160eed0b1435bcbfb13b36c
lthiele <leonard.thiele@cern.ch>
2026-02-02 16:17:54 +0100

    added potential todo

--- blond/physics/impedances/solvers.py
+++ blond/physics/impedances/solvers.py
@@ -850,9 +850,9 @@ class MultiPassResonatorSolver(WakeFieldSolver):
             Simulation time at the moment of calling, has to be > self._last_reference_time.
         """
         delta_t = current_time - self._last_reference_time
-        assert delta_t > 0, (
-            f"delta t was not > 0({delta_t})"
-        )  # TODO: performance = ?
+        # assert delta_t > 0, (  # TODO: is this stricly necessary? This would prevent itner-cavity interaction, which is probably not well modelled by this, but should be fine for benchmarking
+        #     f"delta t was not > 0({delta_t})"
+        # )  # TODO: performance = ?
         for prof_ind, profile_time in enumerate(self._past_profile_times):
             profile_time += delta_t  # NOQA # TODO test PLW2901 `for` loop variable `profile_time` overwritten by assignment target
             self._wake_function_time[prof_ind] += delta_t
