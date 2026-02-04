d5c0d722c303d79312cd5f16fec84932fbdf262a
Simon Albright <simon.albright@cern.ch>
2026-02-04 10:31:57 +0100

    Expanded and parameterized casting unittests
    Added the @parameterized decorator on the casting unit tests to iterate
    over all backends.  Included both full and numpy only versions so that
    local testing without a gpu will still yield useful results.

--- unittests/core/backends/test_backend.py
+++ unittests/core/backends/test_backend.py
@@ -3,6 +3,7 @@ import warnings
 
 import numpy as np
 import pytest
+from parameterized import parameterized
 
 from blond.core.backends.backend import (
     Cupy32Bit,
@@ -17,7 +18,7 @@ from blond.core.backends.backend import (
 from blond.core.backends.numba.callables import recompile_numba_backend
 
 try:
-    import cupy as _  # type: ignore
+    import cupy as cp  # type: ignore
 
     cupy_available = True
 except ModuleNotFoundError:
@@ -905,9 +906,12 @@ class TestSpecials(unittest.TestCase):
                         err_msg=f"{special=} {dtype=}",
                     )
 
-    def test_cast_float_arr(self):
-        backend.change_backend(Numpy64Bit)
+    @pytest.mark.backend_mutation
+    @parameterized.expand([Numpy32Bit, Numpy64Bit])
+    def test_cast_float_arr_np_only(self, new_backend):
+        backend.change_backend(new_backend)
         target = backend.array([1, 2, 3], dtype=backend.float)
+
         for in_type in (tuple, list, np.array):
             cast = backend.cast_arr_float_if_needed(in_type(target))
             self.assertTrue(cast.dtype == backend.float)
@@ -923,21 +927,113 @@ class TestSpecials(unittest.TestCase):
         unchanged = backend.cast_arr_float_if_needed(target)
         self.assertTrue(target is unchanged)
 
-    def test_cast_complex_arr(self):
-        backend.change_backend(Numpy64Bit)
+    @pytest.mark.backend_mutation
+    @parameterized.expand([Numpy32Bit, Numpy64Bit, Cupy32Bit, Cupy64Bit])
+    def test_cast_float_arr_full(self, new_backend):
+        if not cupy_available:
+            self.skipTest(f"{cupy_available=}")
+
+        backend.change_backend(new_backend)
+        for in_type in (tuple, list, np.array, cp.array):
+            # Recreate the target for each loop, avoids issues with
+            # transferring back and forth between cupy and numpy.
+            target = backend.array([1, 2, 3], dtype=backend.float)
+
+            if backend.ndarray is cp.ndarray:
+                to_cast = in_type(target.get())
+            else:
+                to_cast = in_type(target)
+
+            cast = backend.cast_arr_float_if_needed(to_cast)
+            self.assertTrue(cast.dtype == backend.float)
+            self.assertIsInstance(cast, backend.ndarray)
+            if isinstance(backend, CupyBackend):
+                cast = cast.get()
+                target = target.get()
+            np.testing.assert_array_equal(cast, target)
+        for in_dtype in (np.int32, np.int64, np.float64, np.complex128):
+            # Recreate the target for each loop, avoids issues with
+            # transferring back and forth between cupy and numpy.
+            target = backend.array([1, 2, 3], dtype=backend.float)
+            to_cast = target.astype(in_dtype)
+            cast = backend.cast_arr_float_if_needed(to_cast)
+            self.assertTrue(cast.dtype == backend.float)
+            self.assertIsInstance(cast, backend.ndarray)
+
+            if isinstance(backend, CupyBackend):
+                cast = cast.get()
+                target = target.get()
+
+            np.testing.assert_array_equal(cast, target)
+
+        target = backend.array([1, 2, 3], dtype=backend.float)
+        unchanged = backend.cast_arr_float_if_needed(target)
+        self.assertTrue(target is unchanged)
+
+    @pytest.mark.backend_mutation
+    @parameterized.expand([Numpy32Bit, Numpy64Bit])
+    def test_cast_complex_arr_np_only(self, new_backend):
+        if not cupy_available and isinstance(new_backend, CupyBackend):
+            self.skipTest(f"{cupy_available=}")
+
+        backend.change_backend(new_backend)
         target = backend.array([1, 2, 3], dtype=backend.complex)
         for in_type in (tuple, list, np.array):
-            cast = backend.cast_arr_float_if_needed(in_type(target))
-            self.assertTrue(cast.dtype == backend.float)
+            cast = backend.cast_arr_complex_if_needed(in_type(target))
+            self.assertTrue(cast.dtype == backend.complex)
             self.assertIsInstance(cast, backend.ndarray)
             np.testing.assert_array_equal(cast, target)
 
         for in_dtype in (np.int32, np.int64, np.float64, np.complex256):
-            cast = backend.cast_arr_float_if_needed(target.astype(in_dtype))
-            self.assertTrue(cast.dtype == backend.float)
+            cast = backend.cast_arr_complex_if_needed(target.astype(in_dtype))
+            self.assertTrue(cast.dtype == backend.complex)
             self.assertIsInstance(cast, backend.ndarray)
             np.testing.assert_array_equal(cast, target)
 
+        target = backend.array([1, 2, 3], dtype=backend.complex)
+        unchanged = backend.cast_arr_complex_if_needed(target)
+        self.assertTrue(target is unchanged)
+
+    @pytest.mark.backend_mutation
+    @parameterized.expand([Numpy32Bit, Numpy64Bit, Cupy32Bit, Cupy64Bit])
+    def test_cast_complex_arr_full(self, new_backend):
+        if not cupy_available:
+            self.skipTest(f"{cupy_available=}")
+
+        backend.change_backend(new_backend)
+        for in_type in (tuple, list, np.array, cp.array):
+            # Recreate the target for each loop, avoids issues with
+            # transferring back and forth between cupy and numpy.
+            target = backend.array([1, 2, 3], dtype=backend.complex)
+
+            if backend.ndarray is cp.ndarray:
+                to_cast = in_type(target.get())
+            else:
+                to_cast = in_type(target)
+            cast = backend.cast_arr_complex_if_needed(to_cast)
+            self.assertTrue(cast.dtype == backend.complex)
+            self.assertIsInstance(cast, backend.ndarray)
+            if isinstance(backend, CupyBackend):
+                cast = cast.get()
+                target = target.get()
+            np.testing.assert_array_equal(cast, target)
+        for in_dtype in (np.int32, np.int64, np.float64, np.complex128):
+            # Recreate the target for each loop, avoids issues with
+            # transferring back and forth between cupy and numpy.
+            target = backend.array([1, 2, 3], dtype=backend.complex)
+            # Manually discard imaginary to prevent exception
+            # Needed for cupy array backends
+            to_cast = target.real.astype(in_dtype)
+            cast = backend.cast_arr_complex_if_needed(to_cast)
+            self.assertTrue(cast.dtype == backend.complex)
+            self.assertIsInstance(cast, backend.ndarray)
+            if isinstance(backend, CupyBackend):
+                cast = cast.get()
+                target = target.get()
+
+            np.testing.assert_array_equal(cast, target)
+
+        target = backend.array([1, 2, 3], dtype=backend.complex)
         unchanged = backend.cast_arr_complex_if_needed(target)
         self.assertTrue(target is unchanged)
 
