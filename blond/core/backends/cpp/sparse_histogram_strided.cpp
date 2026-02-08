2fd195f54d20daa59a3231c2a6cfbd4762132d0c
slauber <simon.fabian.lauber@cern.ch>
2026-02-08 20:47:50 +0100

    More work

--- blond/core/backends/cpp/sparse_histogram_strided.cpp
+++ blond/core/backends/cpp/sparse_histogram_strided.cpp
@@ -22,57 +22,75 @@ Project website: http://blond.web.cern.ch/
 #include "blond_common.h"
 #include "openmp.h"
 
-extern "C" void sparse_histogram_strided(const real_t * __restrict__ input,
-                real_t * __restrict__ output,
-               const real_t * __restrict__ cut_left_array,
-               const real_t * __restrict__ cut_right_array,
-               const real_t * __restrict__ bunch_indexes,
-               const int n_slices_bucket,
-               const int n_filled_buckets,
-               const int n_macroparticles,
-               const int stride){
-  // Constants init
-  const real_t inv_hist_dist = 1 / (cut_left_array[1] - cut_left_array[0]);
-  const real_t inv_bin_width = n_slices_bucket / (cut_right_array[0] - cut_left_array[0]);
-  const int total = n_filled_buckets * stride;
-  // memory alloc for per thread histo
-  real_t **histo = (real_t **)malloc(omp_get_max_threads() * sizeof(real_t *));
-  histo[0] =
-      (real_t *)malloc(omp_get_max_threads() * total * sizeof(real_t));
-  for (int i = 0; i < omp_get_max_threads(); i++)
-    histo[i] = (*histo + total * i);
+extern "C" void sparse_histogram_strided(
+    const real_t *__restrict__ input,
+    real_t *__restrict__ output,
+    const real_t *__restrict__ cut_left_array,
+    const real_t *__restrict__ cut_right_array,
+    const real_t *__restrict__ bunch_indexes,
+    const int n_slices_bucket,
+    const int n_filled_buckets,
+    const int n_macroparticles,
+    const int stride)
+{
+    const real_t cut_left0 = cut_left_array[0];
+    const real_t inv_hist_dist = real_t(1) / (cut_left_array[1] - cut_left0);
+    const real_t inv_bin_width =
+        real_t(n_slices_bucket) / (cut_right_array[0] - cut_left0);
+
+    const int total = n_filled_buckets * stride;
+
+    // Persistent storage
+    static real_t *histo_all = nullptr;
+    static int histo_threads = 0;
+    static int histo_size = 0;
+
+    const int max_threads = omp_get_max_threads();
+
+    if (histo_all == nullptr || histo_threads < max_threads || histo_size < total) {
+        free(histo_all);
+
+        histo_threads = max_threads;
+        histo_size = total;
+
+        histo_all = (real_t *)malloc(
+            sizeof(real_t) * histo_threads * histo_size);
+
+        if (!histo_all)
+            return;
+    }
 
 #pragma omp parallel
-  {
-    const int id = omp_get_thread_num();
-    const int threads = omp_get_num_threads();
-    memset(histo[id], 0, total * sizeof(real_t));
+    {
+        const int tid = omp_get_thread_num();
+        real_t *histo = histo_all + tid * histo_size;
 
-// main calculation
-#pragma omp for
-    for (int i = 0; i < n_macroparticles; i++) {
-      real_t a = input[i];
-      int hist_i = (int)((a - cut_left_array[0]) * inv_hist_dist);
+        // zero local histogram
+        memset(histo, 0, histo_size * sizeof(real_t));
 
-      real_t cut_left_loc = (cut_left_array[hist_i]);
-      real_t cut_right_loc = (cut_right_array[hist_i]);
-      if ((a < cut_left_loc) || (a > cut_right_loc))
-        continue;
-      int bin_idx_loc = (a - cut_left_array[hist_i]) * inv_bin_width;
-      if (bin_idx_loc <n_slices_bucket){
-          histo[id][hist_i * stride + bin_idx_loc] += 1;
-      }
+#pragma omp for schedule(static)
+        for (int i = 0; i < n_macroparticles; ++i) {
+            const real_t a = input[i];
+
+            const int hist_i = (int)((a - cut_left0) * inv_hist_dist);
+            if ((unsigned)hist_i >= (unsigned)n_filled_buckets)
+                continue;
+
+            const real_t cl = cut_left_array[hist_i];
+            if (a < cl || a >= cut_right_array[hist_i])
+                continue;
+
+            const int bin = (int)((a - cl) * inv_bin_width);
+            if ((unsigned)bin < (unsigned)n_slices_bucket)
+                histo[hist_i * stride + bin] += real_t(1);
+        }
+
+#pragma omp for schedule(static)
+        for (int i = 0; i < total; ++i) {
+            real_t sum = 0;
+            for (int t = 0; t < omp_get_num_threads(); ++t)
+                sum += histo_all[t * histo_size + i];
+            output[i] = sum;
+        }
     }
-
-// Reduce to a single histogram
-#pragma omp for
-    for (int i = 0; i < total; i++) {
-      output[i] = 0.;
-      for (int t = 0; t < threads; t++)
-        output[i] += histo[t][i];
-    }
-  }
-  // free memory
-  free(histo[0]);
-  free(histo);
 }
