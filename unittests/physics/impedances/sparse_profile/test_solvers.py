2c6562b3e5b0d921231045354190a705623e8141
slauber <simon.fabian.lauber@cern.ch>
2026-02-09 11:11:05 +0100

    Work for pre-commit hooks

--- unittests/physics/impedances/sparse_profile/test_solvers.py
+++ unittests/physics/impedances/sparse_profile/test_solvers.py
@@ -202,7 +202,7 @@ class MyTestCase(unittest.TestCase):
         return wakefield
 
     def multiturn(self, induced_voltage) -> WakeField:
-        backend.set_specials("cpp") # TODO remove
+        backend.set_specials("cpp")  # TODO remove
         ring = Ring(
             circumference=6911.56,
         )
