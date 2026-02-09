592ed180e25ab42df6eb43c5c86898c528ddc975
slauber <simon.fabian.lauber@cern.ch>
2026-02-09 12:35:54 +0100

    Fix

--- blond/core/backends/cpp/compile.py
+++ blond/core/backends/cpp/compile.py
@@ -140,7 +140,7 @@ def compile_cpp_library(  # NOQA:  PLR0915 PLR0912
     #                -mfma4 -fopenmp -ftree-vectorizer-verbose=1 '-ffast-math'
 
     cflags = [
-        "-Ofast",
+        "-O3",
         "-std=c++11",
         "-shared",
         "-funroll-loops",  # Aggressive loop unrolling
