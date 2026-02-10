afb33cc0ffcbd2316e4203d934748077a8ad3c7b
slauber <simon.fabian.lauber@cern.ch>
2026-02-10 11:18:57 +0100

    Fixes and more testing

---
+++ unittests/physics/impedances/sparse_profile/test_profile_minimal.py
@@ -0,0 +1,78 @@
+import unittest
+from copy import deepcopy
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from blond import Beam, backend, uranium_29
+from blond.core.beam.beams import ProbeBeam
+from blond.physics.profiles_sparse import EquidistantMultiProfile
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.multiprofile_equidistant = EquidistantMultiProfile.headless(
+            t_rev=5 * 10.0,
+            n_profiles=5,
+            width_per_profile=10.0,
+            bins_per_profile=4,
+            offset=0,
+        )
+        start = self.multiprofile_equidistant.profiles[0].hist_x[0]
+        stop = self.multiprofile_equidistant.profiles[-1].hist_x[-1]
+
+        base = np.linspace(
+            start,
+            stop,
+            5 * 4,
+            endpoint=True,
+        )
+        dt = np.concatenate([[b] * int(b) for b in base])
+        self.beam = ProbeBeam(
+            dt=dt,
+            particle_type=uranium_29,
+        )
+
+    def test___init__(self):
+        pass  # calls __init__ in  self.setUp
+
+    def test_track(self):
+        DEV_DRAW = False  # TODO false
+        backend.set_specials(
+            "cpp"  # TODO remove
+        )
+
+        independent_profiles = self.expected_exec(DEV_DRAW)
+        equidistant = self.actual_exec(DEV_DRAW)
+        if DEV_DRAW:
+            plt.show()
+        for i, profile_expected in enumerate(independent_profiles):
+            profile_actual = equidistant.profiles[i]
+            np.testing.assert_allclose(
+                profile_actual.hist_x, profile_expected.hist_x
+            )
+
+            np.testing.assert_allclose(
+                profile_actual.hist_y, profile_expected.hist_y
+            )
+
+    def actual_exec(self, DEV_DRAW):
+        equidistant = deepcopy(self.multiprofile_equidistant)
+        equidistant._bind_profiles()
+        equidistant.track(self.beam)
+        if DEV_DRAW:
+            equidistant.plot(linestyle="--")
+
+        return equidistant
+
+    def expected_exec(self, DEV_DRAW):
+        independent_profiles = deepcopy(self.multiprofile_equidistant.profiles)
+        for profile_expected in independent_profiles:
+            profile_expected.track(self.beam)
+            if DEV_DRAW:
+                profile_expected.plot()
+        return independent_profiles
+
+
+if __name__ == "__main__":
+    unittest.main()
