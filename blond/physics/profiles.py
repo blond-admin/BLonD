76ddb33862980ff05933865d8d450451f2630796
slauber <simon.fabian.lauber@cern.ch>
2026-02-13 16:32:42 +0100

    Returns artists

--- blond/physics/profiles.py
+++ blond/physics/profiles.py
@@ -28,6 +28,7 @@ if TYPE_CHECKING:  # pragma: no cover
     from typing import Any
 
     from cupy.typing import NDArray as CupyArray  # type: ignore
+    from matplotlib.lines import Line2D
     from numpy.typing import NDArray as NumpyArray
 
     from blond.core.beam.base import BeamBaseClass
@@ -102,7 +103,7 @@ class ProfileBaseClass(BeamPhysicsRelevant, HasPropertyCache):
         assert self._hist_y is not None
         self.invalidate_cache()
 
-    def plot(self, **kwargs_plot: dict[str, Any]) -> None:
+    def plot(self, **kwargs_plot: dict[str, Any]) -> list[Line2D]:
         """
         Plot the current histogram.
 
@@ -114,7 +115,8 @@ class ProfileBaseClass(BeamPhysicsRelevant, HasPropertyCache):
         from blond import AllowPlotting
 
         with AllowPlotting():
-            plt.plot(self.hist_x, self.hist_y, **kwargs_plot)
+            artists = plt.plot(self.hist_x, self.hist_y, **kwargs_plot)
+        return artists
 
     @property  # as readonly attributes
     def hist_x(self) -> NumpyArray | CupyArray:
