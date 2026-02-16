65cd25905fe5c8cb12c3d37bb388e4b9ca000571
slauber <simon.fabian.lauber@cern.ch>
2026-02-16 09:18:06 +0100

    Added NUMBA

--- blond/core/backends/numba/callables.py
+++ blond/core/backends/numba/callables.py
@@ -190,6 +190,14 @@ def recompile_numba_backend(  # NOQA PLR0915 # NOQA: D102
         sig_stop,
     )
 
+    sig_sparse_histogram = void(
+        sig_array_read,  # x
+        sig_array_write,  # out
+        nb_f[:],  # left_cuts
+        nb_f[:],  # right_cuts
+        numba.int32[:],  # bins_per_profile
+        numba.int32[:],  # start_indices
+    )
     sig_hist_x = nb_f[:]
     sig_hist_y = nb_f[:]
     sig_alpha = nb_f
@@ -551,6 +559,77 @@ def recompile_numba_backend(  # NOQA PLR0915 # NOQA: D102
                 ids=ids,
             )
             return n_new
+
+        @staticmethod
+        @enforce_precision(floattype)
+        @njit(
+            sig_sparse_histogram,
+            parallel=True,
+            fastmath=True,
+            cache=True,
+        )
+        def sparse_histogram(
+            x: CupyArray,
+            out: CupyArray,
+            left_cuts: CupyArray,
+            right_cuts: CupyArray,
+            bins_per_profile: CupyArray,
+            start_indices: CupyArray,
+        ) -> None:
+            """
+            Sparse histogram continuous memory layout.
+
+            Parameters
+            ----------
+            x
+                An array, e.g., the particle dt values.
+            out
+                Output histogram (n_filled_buckets * stride).
+            left_cuts
+                Start of each histogram.
+            right_cuts
+                Stop of each histogram.
+            bins_per_profile
+                Number of bins per histogram.
+            start_indices
+                Precomputed index to access out per histogram
+                ``out[start_indices[i]:start_indices[i] + bins_per_profile[i]]``
+            """
+            out[:] = 0
+            n_cuts = len(right_cuts)
+            n_threads = numba.get_num_threads()  # this prevents caching
+            array_tmp = np.zeros((n_threads, len(out)))
+            for i in prange(len(x)):
+                thread_i = numba.get_thread_id()
+                xi = x[i]
+                cut_left = 0.0
+                hist_i = -1
+                for j in range(n_cuts):
+                    cut_right = right_cuts[j]
+                    if xi <= cut_right:
+                        cut_left = left_cuts[j]
+                        hist_i = j
+                        break
+                if hist_i == -1:
+                    continue
+                if xi < cut_left:
+                    continue
+
+                start = cut_left
+                stop = cut_right
+                if xi == stop:
+                    write_idx = int(
+                        start_indices[hist_i] + bins_per_profile[hist_i] - 1
+                    )
+                    array_tmp[thread_i, write_idx] += 1
+                    continue
+                inv_bin_step = bins_per_profile[hist_i] / (
+                    cut_right - cut_left
+                )
+                idx = int((xi - start) * inv_bin_step)
+                write_idx = int(start_indices[hist_i] + idx)
+                array_tmp[thread_i, write_idx] += 1
+            out[:] = np.sum(array_tmp, axis=0)
 
     return NumbaSpecials
 
