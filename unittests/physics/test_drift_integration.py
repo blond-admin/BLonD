13669d86f12bbe6abeff1b1b32a356a177ff7fcc
lthiele <leonard.thiele@cern.ch>
2026-02-18 20:08:24 +0100

    rollback of parameter extraction

--- unittests/physics/test_drift_integration.py
+++ unittests/physics/test_drift_integration.py
@@ -34,18 +34,14 @@ class TestDriftIntegration(unittest.TestCase):
         logging.basicConfig(level=logging.INFO)
         ring = Ring(circumference=circumference)
 
-        cavity1 = SingleHarmonicRFStation(
-            section_index=0,
-            harmonic=35640,
-            voltage=6e6,
-            phi_rf=0,
-        )
-        cavity2 = SingleHarmonicRFStation(
-            section_index=1,
-            harmonic=35640,
-            voltage=6e6,
-            phi_rf=0,
-        )
+        cavity1 = SingleHarmonicRFStation(section_index=0)
+        cavity1.harmonic = 35640
+        cavity1.voltage = 6e6
+        cavity1.phi_rf_design = 0
+        cavity2 = SingleHarmonicRFStation(section_index=1)
+        cavity2.harmonic = 35640
+        cavity2.voltage = 6e6
+        cavity2.phi_rf_design = 0
 
         N_TURNS = int(1e3)
         energy_cycle = MagneticCyclePerTurn(
@@ -83,9 +79,10 @@ class TestDriftIntegration(unittest.TestCase):
         logging.basicConfig(level=logging.INFO)
         ring = Ring(circumference=circumference)
 
-        cavity1 = SingleHarmonicRFStation(
-            section_index=0, harmonic=35640, voltage=6e6, phi_rf=0
-        )
+        cavity1 = SingleHarmonicRFStation(section_index=0)
+        cavity1.harmonic = 35640
+        cavity1.voltage = 6e6
+        cavity1.phi_rf_design = 0
 
         N_TURNS = int(1e3)
         energy_cycle = ConstantMagneticCycle(
