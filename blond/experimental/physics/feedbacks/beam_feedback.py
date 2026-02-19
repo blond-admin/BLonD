dfaac8afdd69b3d4aaaaef5c661c7b0e27f20687
lthiele <leonard.thiele@cern.ch>
2026-02-19 14:07:18 +0100

    Deleted feedback functionality, will be reverted with for other MR

--- blond/experimental/physics/feedbacks/beam_feedback.py
+++ blond/experimental/physics/feedbacks/beam_feedback.py
@@ -23,23 +23,17 @@ from __future__ import annotations
 from abc import abstractmethod
 from typing import TYPE_CHECKING
 
-import numpy as np
-
 from blond import Simulation
-from blond.core.backends.backend import backend
 from blond.physics.feedbacks.base import (
     GlobalFeedback,
 )
 
 if TYPE_CHECKING:  # pragma: no cover
     from blond.core.beam.base import BeamBaseClass
-    from blond.physics.cavities import RFStationBaseClass
     from blond.physics.profiles import ProfileBaseClass
 
 
 class BeamFeedbackBase(GlobalFeedback):
-    _parent_rf_station: RFStationBaseClass
-
     def __init__(
         self,
         profile: ProfileBaseClass,
@@ -67,13 +61,7 @@ class BeamFeedbackBase(GlobalFeedback):
         n_turns: int,
         **kwargs,
     ) -> None:
-        if (
-            self.beam_current_threshold is None
-            and self.cavities[0].any_feedback_not_none
-        ):
-            raise RuntimeError(
-                "The filled slots in the machine is needed to compute the cavity sum phase"
-            )
+        pass
 
     @abstractmethod
     def get_beam_attribute(self, beam: BeamBaseClass):
@@ -86,96 +74,12 @@ class BeamFeedbackBase(GlobalFeedback):
         pass
 
     def beam_phase(self):
-        # Main RF frequency at the present turn
-        omega_rf = self.cavities[0].get_main_harmonic_omega_rf()
-        phi_rf = self.cavities[0].get_main_harmonic_phi_rf()
-
-        if self.time_offset is None:
-            coeff = backend.specials.beam_phase(
-                self.profile.hist_x,
-                self.profile.hist_y,
-                self.window_coefficient,
-                omega_rf,
-                phi_rf,
-                self.profile.hist_step,
-            )
-        else:
-            indexes = self.profile.hist_x >= self.time_offset
-            coeff = backend.specials.beam_phase(
-                self.profile.hist_x[indexes],
-                self.profile.hist_y,
-                self.window_coefficient,
-                omega_rf,
-                phi_rf,
-                self.profile.hist_step,
-            )
-
-        # Project beam phase to (pi/2,3pi/2) range
-        self.phi_beam = np.arctan(coeff) + np.pi
+        pass
 
     def phase_difference(
         self, beam: BeamBaseClass, RFnoise=None, noiseFB=None
     ):
-        """
-        *Phase difference between beam and RF phase of the main RF system.
-        Optional: add RF phase noise through dphi directly.*
-        """
-
-        # Correct for design stable phase
-        counter = self.cavities[0]._turn_i.value
-        self.dphi = self.phi_beam - self.cavities[0].calc_phi_s_main_harmonic(
-            beam
-        )
-
-        # Phase offset due to beam loading
-        if self.cavities[0].any_feedback_not_none:
-            filled_slots = (
-                np.abs(
-                    self.cavities[0]
-                    .cavity_feedback_list[0]
-                    .I_BEAM_COARSE[
-                        -self.cavities[0].cavity_feedback_list[0].n_coarse :
-                    ]
-                )
-                > self.beam_current_threshold
-            )
-
-            gap_phase_in_slots = (
-                self.cavities[0]
-                .cavity_feedback_list[0]
-                .gap_voltage_phase[filled_slots]
-            )
-            # voltage difference
-            if len(gap_phase_in_slots) > 0:
-                phi_mean = np.mean(gap_phase_in_slots)
-            else:
-                phi_mean = 0
-            self.dphi = self.dphi + phi_mean
-
-        # Possibility to add RF phase noise through the PL
-        if RFnoise is not None:
-            if noiseFB is not None:
-                self.dphi += noiseFB.x * RFnoise.dphi[counter]
-            else:
-                self.dphi += RFnoise.dphi[counter]
+        pass
 
     def _track(self, beam: BeamBaseClass):
-        self.get_beam_attribute(
-            beam=beam,
-        )
-        self.apply_corrections(
-            beam=beam,
-        )
-
-        if self.cavities[0]._turn_i.value >= self.delay:
-            # TODO incorrect for simulations that start later
-            # domega_rf is updated later
-            # this means domega_rf is effectively from last turn
-            omega_increment = (
-                self.domega_rf
-                * self.cavities[0].harmonic[:]
-                / self.cavities[
-                    0
-                ].get_main_harmonic()  # dynamically updated by `update_domega_rf`
-            )
-            self.cavities[0].delta_omega_rf = omega_increment
+        pass
--- blond/experimental/physics/feedbacks/cavity_feedback.py
+++ blond/experimental/physics/feedbacks/cavity_feedback.py
@@ -8,24 +8,14 @@
 
 from __future__ import annotations
 
-import warnings
-from abc import abstractmethod
 from typing import TYPE_CHECKING
 
-import numpy as np
-
-from blond.core.helpers import int_from_float_with_warning
-from blond.core.ring.helpers import requires
-from blond.physics.cavities import SingleHarmonicRFStation
 from blond.physics.feedbacks.base import LocalFeedback
 from blond.physics.profiles import StaticProfile
 
 if TYPE_CHECKING:  # pragma: no cover
-    from typing import Any
-
     from numpy.typing import NDArray as NumpyArray
 
-    from blond import Simulation
     from blond.core.beam.base import BeamBaseClass
 
 # TODO rewrite all docstrings
@@ -110,7 +100,6 @@ class IQCavityFeedback(LocalFeedback):
         n_periods_coarse: int | float,
         harmonic_index: int,
         use_lowpass_filter: bool = False,
-        section_index: int = 0,
         name: str | None = None,
     ):
         """
@@ -133,288 +122,16 @@ class IQCavityFeedback(LocalFeedback):
         name
             # TODO might be removed
         """
-        assert isinstance(profile, StaticProfile)
         super().__init__(
             profile=profile,
             name=name,
         )
 
-        # Number of cavities the feedback is working on
-        assert n_cavities > 0, f"{n_cavities=}, but must be bigger 0."
-        self.n_cavities = int_from_float_with_warning(
-            n_cavities,
-            warning_stacklevel=2,
-        )
-
-        # Apply a low-pass filter to the RF beam current
-        self.use_lowpass_filter = use_lowpass_filter
-
-        # The harmonic index the cavity feedback is working on
-        self.harmonic_index = int_from_float_with_warning(
-            harmonic_index,
-            warning_stacklevel=2,
-        )
-
-        # Ratio between rf periods and coarse grid sampling period
-        if not type(n_periods_coarse) is int:
-            warnings.warn(
-                "n_periods_coarse is not an integer; coupling between loops might break"
-            )
-        self.n_periods_coarse = n_periods_coarse
-        self.n_coarse: float | None = None
-
-        self.omega_carrier_prev: float | None = None
-        self.omega_carrier: float | None = None
-        self.omega_rf: float | None = None
-        self.t_rev: float | None = None
-
-        # Present sampling time
-        self.T_s_prev: float | None = None
-        self.T_s: float | None = None
-
-        # Present coarse grid and save previous turn coarse grid
-        self.rf_centers_prev: float | None = None
-
-        # Residual part of last turn entering the current turn due to non-integer harmonic number
-        self.dT: float | None = None
-
-        self.rf_centers: NumpyArray | None = None
-
         self.V_corr: NumpyArray | None = None
         self.alpha_sum: NumpyArray | None = None
         self.phi_corr: NumpyArray | None = None
-
-        self.V_SET: NumpyArray | None = None
-        self.I_BEAM_COARSE: NumpyArray | None = None
-        self.I_BEAM_FINE: NumpyArray | None = None
-        self.V_ANT_COARSE: NumpyArray | None = None
-        self.V_ANT_FINE: NumpyArray | None = None
-        self.I_GEN_COARSE: NumpyArray | None = None
-        self.I_GEN_FINE: NumpyArray | None = None
 
         self.gap_voltage_phase: NumpyArray | None = None
-
-    @requires(["RFStationBaseClass", "BeamBaseClass"])
-    def on_run_simulation(
-        self,
-        simulation: Simulation,
-        beam: BeamBaseClass,
-        n_turns: int,
-        **kwargs: dict[str, Any],
-    ) -> None:
-        super().on_run_simulation(simulation, beam, n_turns, **kwargs)
-
-        harmonic, omega_rf, phi_rf = self.get_harmonic_and_omega_rf_phi_rf()
-
-        self.T_s = (self.n_periods_coarse * 2 * np.pi) / float(omega_rf)
-        # TODO REMWORK/REMOVE
-        t_rev = float((2 * np.pi * harmonic) / float(omega_rf))
-        # TODO REMWORK/REMOVE
-        t_rf = t_rev / float(harmonic)
-
-        self.n_coarse = round(t_rev / self.T_s)  # TODO: round or ceil?
-        self.omega_carrier = omega_rf
-        # FIXME NO REDECLARATION!
-
-        self.omega_rf = float(omega_rf)
-        self.dT = 0
-
-        # The least amount of arrays needed to feedback to the tracker object
-        self.rf_centers = np.arange(self.n_coarse) * self.T_s + 0.5 * t_rf
-
-        self.V_SET = np.zeros(2 * self.n_coarse, dtype=complex)
-        self.I_BEAM_COARSE = np.zeros(2 * self.n_coarse, dtype=complex)
-        self.I_BEAM_FINE = np.zeros(self.profile.n_bins, dtype=complex)
-        self.V_ANT_COARSE = np.zeros(2 * self.n_coarse, dtype=complex)
-        self.V_ANT_FINE = np.zeros(self.profile.n_bins, dtype=complex)
-        self.I_GEN_COARSE = np.zeros(2 * self.n_coarse, dtype=complex)
-        self.I_GEN_FINE = np.zeros(self.profile.n_bins, dtype=complex)
-
-        self.gap_voltage_phase = np.zeros(self.n_coarse)
-
-    def set_hardware_commissioning(self, omega_rf: float, harmonic: int):
-        self.T_s = (self.n_periods_coarse * 2 * np.pi) / omega_rf
-        # TODO REMWORK/REMOVE
-        t_rev = float((2 * np.pi * harmonic) / omega_rf)
-        # TODO REMWORK/REMOVE
-        t_rf = 2 * np.pi / omega_rf
-
-        self.n_coarse = round(t_rev / self.T_s)
-        self.omega_carrier = float(omega_rf) / self.n_periods_coarse
-        # FIXME NO REDECLARATION!
-
-        self.omega_rf = float(omega_rf)
-        self.dT = 0
-
-        # The least amount of arrays needed to feedback to the tracker object
-        self.rf_centers = np.arange(self.n_coarse) * self.T_s + 0.5 * t_rf
-
-        self.V_SET = np.zeros(2 * self.n_coarse, dtype=complex)
-        self.I_BEAM_COARSE = np.zeros(2 * self.n_coarse, dtype=complex)
-        self.I_BEAM_FINE = np.zeros(self.profile.n_bins, dtype=complex)
-        self.V_ANT_COARSE = np.zeros(2 * self.n_coarse, dtype=complex)
-        self.V_ANT_FINE = np.zeros(self.profile.n_bins, dtype=complex)
-        self.I_GEN_COARSE = np.zeros(2 * self.n_coarse, dtype=complex)
-        self.I_GEN_FINE = np.zeros(self.profile.n_bins, dtype=complex)
-
-        # TODO REWORK LATEINIT
-        self.V_corr: NumpyArray | None = None
-        self.alpha_sum: NumpyArray | None = None
-        self.phi_corr: NumpyArray | None = None
-        self.omega_carrier_prev: float | None = None
-        self.T_s_prev: float | None = None
-        self.rf_centers_prev: float | None = None
-
-    @abstractmethod  # pragma: no cover
-    def update_fb_variables(self) -> None:
-        r"""
-        Method to update the variables specific to the feedback.
-
-        This is meant to be implemented in the child class by the user.
-        """
-        pass
-
-    def get_harmonic_and_omega_rf_phi_rf(
-        self,
-    ) -> tuple[float, float, float]:
-        """
-        Convenience function to get the actual values, currently acting on the RF station.
-
-        This function is necessary since the _parent_cavity can be either a multi or single harmonic cavity.
-        One of the holds the values for phi_rf omega_rf and as arrays and one as floats.
-
-        Returns
-        -------
-        harmonic
-            harmonic number for the harmonic index/only one
-        omega_rf
-            omega_rf for the harmonic index/only one
-        phi_rf
-            phi_rf for the harmonic index/only one
-
-        """
-
-        if isinstance(self._parent_rf_station, SingleHarmonicRFStation):
-            harmonic = self._parent_rf_station.harmonic
-            omega_rf = self._parent_rf_station.omega_rf
-            phi_rf = self._parent_rf_station.phi_rf
-        else:
-            harmonic = self._parent_rf_station.harmonic[self.harmonic_index]
-            omega_rf = self._parent_rf_station.omega_rf[self.harmonic_index]
-            phi_rf = self._parent_rf_station.phi_rf[self.harmonic_index]
-        return harmonic, omega_rf, phi_rf
-
-    def get_harmonic_and_omega_rf_phi_rf_design(
-        self,
-    ) -> tuple[float, float, float]:
-        """
-        Convenience function to get the design values of the RF station.
-
-        This function is necessary since the _parent_cavity can be either a multi or single harmonic cavity.
-        One of the holds the values for phi_rf omega_rf and as arrays and one as floats.
-
-        Returns
-        -------
-        harmonic
-            harmonic number for the harmonic index/only one
-        omega_rf_design
-            omega_rf_design for the harmonic index/only one
-        phi_rf_design
-            phi_rf_design for the harmonic index/only one
-
-        """
-        if isinstance(self._parent_rf_station, SingleHarmonicRFStation):
-            harmonic = self._parent_rf_station.harmonic
-            omega_rf_design = self._parent_rf_station.omega_rf_design
-            phi_rf_design = self._parent_rf_station.phi_rf_design
-        else:
-            harmonic = self._parent_rf_station.harmonic[self.harmonic_index]
-            omega_rf_design = self._parent_rf_station.omega_rf_design[
-                self.harmonic_index
-            ]
-            phi_rf_design = self._parent_rf_station.phi_rf_design[
-                self.harmonic_index
-            ]
-        return harmonic, omega_rf_design, phi_rf_design
-
-    def get_voltage_from_parent_rf_station(self) -> float:
-        """
-        Convenience function to get the voltage from the parent RF station.
-
-        Returns
-        -------
-        voltage
-            voltage from the parent RF station, either at harmonic_index or the only one
-
-        """
-        if isinstance(self._parent_rf_station, SingleHarmonicRFStation):
-            return self._parent_rf_station.voltage
-        else:
-            return self._parent_rf_station.voltage[self.harmonic_index]
-
-    def update_rf_variables(
-        self, omega_rf: float | None = None, harmonic: float | None = None
-    ) -> None:
-        r"""Updating variables from the other BLonD classes."""
-        if omega_rf is None or harmonic is None:
-            harmonic, omega_rf, phi_rf = (
-                self.get_harmonic_and_omega_rf_phi_rf()
-            )
-        else:
-            phi_rf = 0.0
-
-        # Present RF angular frequency
-        self.omega_rf = float(omega_rf)
-        t_rev = float(  # TODO REMWORK/REMOVE
-            2 * np.pi * harmonic / self.omega_rf
-        )
-        t_rf = float(2 * np.pi / self.omega_rf)
-
-        # Present carrier frequency: main RF frequency
-        self.omega_carrier_prev = self.omega_carrier
-        self.omega_carrier = self.omega_rf  # TODO: is this correct?
-
-        # Present sampling time
-        self.T_s_prev = self.T_s
-        self.T_s = self.n_periods_coarse * 2 * np.pi / self.omega_rf
-
-        # Update the coarse grid sampling
-        self.n_coarse = round(t_rev / self.T_s)
-
-        # Present coarse grid and save previous turn coarse grid
-        self.rf_centers_prev = np.copy(self.rf_centers)
-
-        # Residual part of last turn entering the current turn due to non-integer harmonic number
-        self.dT = -phi_rf / self.omega_rf
-
-        self.rf_centers = (
-            np.arange(self.n_coarse) * self.T_s + 0.5 * t_rf + self.dT
-        )
-
-    @abstractmethod  # pragma: no cover
-    def circuit_track(self, no_beam: bool = False) -> None:
-        r"""
-        Method to track circuit of the feedback.
-
-        Notes
-        -----
-        This is meant to be implemented in the child class by the user.
-        The only requirement for this method is that it has to update the
-        V_ANT_FINE and V_SET arrays turn-by-turn.
-
-        Parameters
-        ----------
-        no_beam
-            # TODO
-        """
-        pass
-
-    def track_no_beam(self, n_pretrack: int | None = 1) -> None:
-        r"""Tracking method of the cavity feedback without beam in the accelerator."""
-        self.update_rf_variables()  # TODO: commented out
-        self.update_fb_variables()
-        for i in range(n_pretrack):
-            self.circuit_track(no_beam=True)
 
     def _track(self, beam: BeamBaseClass) -> None:
         r"""
@@ -426,83 +143,4 @@ class IQCavityFeedback(LocalFeedback):
             Simulation `Beam` object
 
         """
-        from blond.physics.feedbacks.helpers import cartesian_to_polar
-
-        # Update parameters from rest of BLonD classes
-        self.update_rf_variables()
-        self.update_fb_variables()
-
-        # Get rf beam current
-        self.rf_beam_current(
-            beam=beam,
-            use_lowpass_filter=self.use_lowpass_filter,
-        )
-
-        # Tracking circuit model of feedback
-        self.circuit_track()
-
-        # Convert to amplitude and phase
-        self.V_corr, self.alpha_sum = cartesian_to_polar(
-            IQ_vector=self.V_ANT_FINE[-self.profile.n_bins :],
-        )
-
-        # Calculate OTFB correction w.r.t. RF voltage and phase in RFStation
-        self.V_corr /= self.get_voltage_from_parent_rf_station()
-        self.phi_corr = self.alpha_sum - np.mean(
-            np.angle(self.V_SET[-self.n_coarse :])
-        )
-
-        self.phase_correction = self.phi_corr
-        self.relative_voltage_correction = self.V_corr
-
-        self.gap_voltage_phase = np.angle(
-            self.V_ANT_COARSE[-self.n_coarse :] / self.V_SET[-self.n_coarse :]
-        )  # TODO: where is the interpolation gone?
-
-    def rf_beam_current(
-        self,
-        beam: BeamBaseClass,
-        use_lowpass_filter: bool = False,
-    ) -> None:
-        r"""Calculate RF beam current from beam profile"""
-        from blond.physics.feedbacks.helpers import rf_beam_current
-
-        harmonic, omega_rf_design, _ = (
-            self.get_harmonic_and_omega_rf_phi_rf_design()
-        )
-        t_rev = float(  # TODO REMWORK/REMOVE
-            (2 * np.pi * harmonic) / omega_rf_design
-        )
-        # Beam current from profile
-        self.I_BEAM_COARSE[: self.n_coarse] = self.I_BEAM_COARSE[
-            -self.n_coarse :
-        ]
-        self.I_BEAM_FINE, self.I_BEAM_COARSE[-self.n_coarse :] = (
-            rf_beam_current(
-                beam=beam,
-                profile=self.profile,
-                omega_c=self.omega_carrier,
-                T_rev=t_rev,
-                use_lowpass_filter=use_lowpass_filter,
-                downsample={"Ts": self.T_s, "points": self.n_coarse},
-                external_reference=True,
-                dT=self.dT,
-            )
-        )
-
-        # Convert RF beam currents to be in units of Amperes
-        self.I_BEAM_FINE = self.I_BEAM_FINE / self.profile.hist_step
-        self.I_BEAM_COARSE[-self.n_coarse :] = (
-            self.I_BEAM_COARSE[-self.n_coarse :] / self.T_s
-        )
-
-    def set_point_from_rfstation(self) -> NumpyArray:
-        r"""Computes the setpoint in I/Q based on the RF voltage in the RFStation"""
-        from blond.physics.feedbacks.helpers import polar_to_cartesian
-
-        V_set = polar_to_cartesian(
-            self.get_voltage_from_parent_rf_station() / self.n_cavities,
-            0,
-        )
-
-        return V_set * np.ones(self.n_coarse)
+        pass
