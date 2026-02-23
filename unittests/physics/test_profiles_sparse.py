946c86b49d21bc70c0d3c8244f4449fd2831e84d
slauber <simon.fabian.lauber@cern.ch>
2026-02-23 11:34:24 +0100

    Fixed GPU dependency

--- unittests/physics/test_profiles_sparse.py
+++ unittests/physics/test_profiles_sparse.py
@@ -17,8 +17,8 @@ class TestEquidistantMultiProfile(unittest.TestCase):
             bins_per_profile=4,
             offset=0,
         )
-        start = self.multiprofile_equidistant.profiles[0].hist_x[0]
-        stop = self.multiprofile_equidistant.profiles[-1].hist_x[-1]
+        start = float(self.multiprofile_equidistant.profiles[0].hist_x[0])
+        stop = float(self.multiprofile_equidistant.profiles[-1].hist_x[-1])
 
         base = np.linspace(
             start,
