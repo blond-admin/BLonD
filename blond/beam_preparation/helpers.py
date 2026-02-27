858249bc203db62a7fea4d56683685da8acb8739
slauber <simon.fabian.lauber@cern.ch>
2026-02-27 15:26:29 +0100

    Deleted

--- blond/beam_preparation/helpers.py
+++ blond/beam_preparation/helpers.py
@@ -12,8 +12,19 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import numpy as np
+from cupy.typing import NDArray as CupyArray
+from numpy._typing import NDArray as NumpyArray
+
+from blond.core.beam.base import BeamBaseClass
+from blond.generals.cupy.no_cupy_import import copy_to_cpu
+from blond.generals.distributed.helpers import (
+    mpi_aware_random_generator_cpu,
+    mpi_local_size,
+)
+
 if TYPE_CHECKING:
-    from blond import Beam
+    from blond import Beam, backend
 
 
 def make_multibunch_beam(
@@ -79,3 +90,119 @@ def make_multibunch_beam(
         mpi_mode="all-ranks",
     )
     return full_beam
+
+
+def generate_particle_coordinates(
+    time_grid: NumpyArray,
+    deltaE_grid: NumpyArray,
+    density_grid: NumpyArray,
+    n_macroparticles: int,
+    seed: int | None,
+) -> tuple[NumpyArray | CupyArray, NumpyArray | CupyArray]:
+    """
+    Fill bunch with macroparticles according to `density_distribution`.
+
+    Parameters
+    ----------
+    time_grid
+        2D grid of positions in time, in [s].
+    deltaE_grid
+        2D grid of energies, in [eV].
+    density_grid
+        2D grid of densities according to time vs. energy.
+    n_macroparticles
+        Number of macroparticles to distribute, according to the grid.
+    seed
+        Random seed, to make function with same seed
+        always return the same value.
+
+    Returns
+    -------
+    dt_local
+        Particle coordinates (on the local MPI node, if MPI is active).
+    dE_local
+        Particle coordinates (on the local MPI node, if MPI is active).
+    """
+    # Initialise the random number generator
+    # DEV NOTE (2025) It might be checked at a later time,
+    # if cupy and numpy provide for the exact same random generators.
+    n_macroparticles_local = mpi_local_size(
+        n_macroparticles, warning_hint="n_macroparticles"
+    )
+    random_generator_cpu = mpi_aware_random_generator_cpu(
+        seed=seed, n_forward_per_rank=n_macroparticles_local
+    )
+    # Generating particles randomly inside the grid cells according to the
+    # provided density_grid
+    indexes = random_generator_cpu.choice(
+        np.arange(0, np.size(density_grid)),
+        n_macroparticles_local,
+        p=copy_to_cpu(density_grid.flatten() / np.sum(density_grid)),
+    )
+    indexes = np.array(
+        indexes
+    )  # finally convert to the correct backend. See above why.
+    time_step = time_grid[0, 1] - time_grid[0, 0]
+    assert time_step > 0, f"{time_step=}"
+    deltaE_step = deltaE_grid[1, 0] - deltaE_grid[0, 0]
+    assert deltaE_step > 0, f"{deltaE_step=}"
+    # Randomize particles inside each grid cell (uniform distribution)
+    # ``backend.random.triangular`` has rotational symmetry, but is
+    # distributed within a square.
+    dt_local = backend.array(
+        time_grid.flatten()[indexes]
+        + random_generator_cpu.triangular(
+            left=-1, mode=0, right=1, size=n_macroparticles_local
+        )
+        * time_step
+    )
+    dE_local = backend.array(
+        deltaE_grid.flatten()[indexes]
+        + random_generator_cpu.triangular(
+            left=-1, mode=0, right=1, size=n_macroparticles_local
+        )
+        * deltaE_step
+    )
+    return dt_local, dE_local
+
+
+def populate_beam(
+    beam: BeamBaseClass,
+    time_grid: NumpyArray,
+    deltaE_grid: NumpyArray,
+    density_grid: NumpyArray,
+    n_macroparticles: int,
+    seed: int | None,
+) -> None:
+    """
+    Fill bunch with macroparticles according to `density_distribution`.
+
+    Parameters
+    ----------
+    beam
+        Simulation beam object.
+    time_grid
+        2D grid of positions in time, in [s].
+    deltaE_grid
+        2D grid of energies, in [eV].
+    density_grid
+        2D grid of densities according to time vs. energy.
+    n_macroparticles
+        Number of macroparticles to distribute, according to the grid.
+    seed
+        Random seed, to make function with same seed
+        always return the same value.
+
+    Notes
+    -----
+    The beam coordinate `dt` and `dE` will be overwritten.
+    """
+    dt_local, dE_local = generate_particle_coordinates(
+        time_grid=time_grid,
+        deltaE_grid=deltaE_grid,
+        density_grid=density_grid,
+        n_macroparticles=n_macroparticles,
+        seed=seed,
+    )
+
+    beam.setup_beam(dt=dt_local, dE=dE_local, mpi_mode="all-ranks")
--- blond/experimental/beam_preparation/semi_empiric_matcher.py
+++ blond/experimental/beam_preparation/semi_empiric_matcher.py
@@ -17,8 +17,8 @@ import numpy as np
 
 from blond import AllowPlotting, backend
 from blond.beam_preparation.base import MatchingRoutine
+from blond.beam_preparation.helpers import populate_beam
 from blond.core.helpers import int_from_float_with_warning
-from blond.experimental.beam_preparation.helpers import populate_beam
 from blond.generals.cupy.no_cupy_import import copy_to_cpu
 
 # Oversampling factor for potential well calculation
