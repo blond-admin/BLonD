9d4746d66da1acf364c234ddefa0ca9450c1a1b8
lvalle <lina.valle@cern.ch>
2026-02-27 17:17:56 +0100

    PArticle to particle

--- blond/trackers/tracker.py
+++ blond/trackers/tracker.py
@@ -628,7 +628,7 @@ class RingAndRFTracker:
                             dE=self.beam.dE,
                             voltage=self.total_voltage,
                             bin_centers=self.profile.bin_centers,
-                            charge=self.beam.Particle.charge,
+                            charge=self.beam.particle.charge,
                             acceleration_kick=self.acceleration_kick[turn],
                         )
 
