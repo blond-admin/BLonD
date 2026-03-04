a9bdae8984618e88439f0d1940bee711428ab405
Simon Albright <simon.albright@cern.ch>
2026-03-04 10:12:47 +0100

    Tests for BLonD2 style barrier bucket generator

---
+++ unittests/physics/test_barrier_bucket.py
@@ -0,0 +1,299 @@
+# General imports
+import unittest
+
+import numpy as np
+import numpy.testing as nptest
+
+# BLonD imports
+import blond.physics.barrier_bucket as bbuck
+
+
+class TestBarrierBucketFunctions(unittest.TestCase):
+    def test_simple_barrier(self):
+        cent = 500e-9
+        width = 100e-9
+        ampl = 1e3
+
+        centers = np.linspace(0, 1000e-9, 5000)
+
+        barrier = bbuck.compute_sin_barrier(
+            cent, width, ampl, centers, periodic=False
+        )
+
+        self.assertAlmostEqual(np.max(barrier), 1e3, places=1)
+
+        left_pts = np.where(centers < cent - width / 2)[0]
+        right_pts = np.where(centers > cent + width / 2)[0]
+
+        self.assertListEqual(
+            list(barrier[left_pts]), list(np.zeros_like(left_pts))
+        )
+        self.assertListEqual(
+            list(barrier[right_pts]), list(np.zeros_like(right_pts))
+        )
+
+        self.assertAlmostEqual(
+            np.max(barrier[left_pts[-1] : right_pts[0]]), 1e3, places=1
+        )
+        self.assertAlmostEqual(
+            np.min(barrier[left_pts[-1] : right_pts[0]]), -1e3, places=1
+        )
+
+    def test_periodic_barrier(self):
+        cent = 1000e-9
+        width = 100e-9
+        ampl = 1e3
+
+        centers = np.linspace(0, 1000e-9, 5000)
+
+        barrier = bbuck.compute_sin_barrier(
+            cent, width, ampl, centers, periodic=True
+        )
+
+        self.assertAlmostEqual(np.max(barrier), 1e3, places=1)
+
+        left_pts = np.where(centers < centers[0] + width / 2)[0]
+        right_pts = np.where(centers > centers[-1] - width / 2)[0]
+
+        self.assertListEqual(
+            list(barrier[left_pts[-1] : right_pts[0]]),
+            list(barrier[left_pts[-1] : right_pts[0]]),
+        )
+
+        self.assertAlmostEqual(np.max(barrier[left_pts]), 1e3, places=1)
+        self.assertAlmostEqual(np.min(barrier[right_pts]), -1e3, places=1)
+
+    def test_wide_barrier(self):
+        cent = 1000e-9
+        width = 1100e-9
+        ampl = 1e3
+
+        centers = np.linspace(0, 1000e-9, 5000)
+
+        with self.assertRaises(ValueError):
+            bbuck.compute_sin_barrier(cent, width, ampl, centers)
+
+    def test_fourier_series(self):
+        cent = 500e-9
+        width = 100e-9
+        ampl = 1e3
+
+        centers = np.linspace(0, 1000e-9, 5000)
+
+        barrier = bbuck.compute_sin_barrier(
+            cent, width, ampl, centers, periodic=False
+        )
+
+        amps, phases = bbuck.waveform_to_harmonics(barrier, np.arange(1, 13))
+
+        amps_exp = [
+            19.9,
+            39.0,
+            56.6,
+            72.1,
+            84.9,
+            94.6,
+            101.0,
+            103.9,
+            103.5,
+            100,
+            93.7,
+            85.0,
+        ]
+        phases_exp = [
+            3.14,
+            6.28,
+            3.14,
+            6.28,
+            3.14,
+            6.28,
+            3.14,
+            6.28,
+            3.14,
+            6.28,
+            3.13,
+            6.28,
+        ]
+
+        for a, a_exp, p, p_exp in zip(amps, amps_exp, phases, phases_exp):
+            self.assertAlmostEqual(a, a_exp, places=1)
+            self.assertAlmostEqual(p, p_exp, places=2)
+
+    def test_sinc_filter(self):
+        cent = 500e-9
+        width = 100e-9
+        ampl = 1e3
+
+        centers = np.linspace(0, 1000e-9, 5000)
+
+        barrier = bbuck.compute_sin_barrier(
+            cent, width, ampl, centers, periodic=False
+        )
+
+        amps, _ = bbuck.waveform_to_harmonics(barrier, np.arange(1, 13))
+        amps = bbuck.sinc_filtering(amps, m=1)
+
+        amps_exp = [
+            19.4,
+            35.3,
+            45.1,
+            47.4,
+            42.3,
+            31.6,
+            17.7,
+            3.6,
+            -8.2,
+            -16.0,
+            -19.3,
+            -18.4,
+        ]
+
+        for a, a_exp in zip(amps, amps_exp):
+            self.assertAlmostEqual(a, a_exp, places=1)
+
+    def test_waveform_harmonics(self):
+        harms = [1, 2, 3, 4, 5, 6, 7]
+        set_amps = [4e3, 0, 3e3, 0, 2e3, 0, 1e3]
+        set_phases = [0, 0, np.pi, 0, 0, 0, np.pi]
+
+        t_rev = 1e-6
+        centers = np.linspace(0, t_rev, 5000)
+        waveform = np.zeros_like(centers)
+
+        for h, a, p in zip(harms, set_amps, set_phases):
+            waveform += a * np.sin(2 * np.pi * h * centers / t_rev + p)
+
+        comp_amps, comp_phases = bbuck.waveform_to_harmonics(
+            waveform, range(1, 8)
+        )
+
+        for i in range(len(harms)):
+            self.assertAlmostEqual(comp_amps[i], set_amps[i], delta=1e1)
+
+            if set_amps[i] > 0:
+                # Use sin/cos comparison to avoid issues with 0 != 2pi
+                self.assertAlmostEqual(
+                    np.cos(comp_phases[i]), np.cos(set_phases[i]), places=1
+                )
+                self.assertAlmostEqual(
+                    np.sin(comp_phases[i]), np.sin(set_phases[i]), places=1
+                )
+
+        comp_wave = bbuck.harmonics_to_waveform(
+            centers, harms, set_amps, set_phases, t_rev
+        )
+
+        nptest.assert_array_almost_equal(waveform, comp_wave, decimal=2)
+
+    def test_negative_barrier(self):
+        cent = 500e-9
+        width = 100e-9
+        ampl = 1e3
+
+        centers = np.linspace(0, 1000e-9, 5000)
+
+        pbarrier = bbuck.compute_sin_barrier(cent, width, ampl, centers)
+        nbarrier = bbuck.compute_sin_barrier(cent, width, -ampl, centers)
+
+        nptest.assert_array_equal(pbarrier, -nbarrier)
+
+        pamps, pphases = bbuck.waveform_to_harmonics(pbarrier, range(1, 26))
+        namps, nphases = bbuck.waveform_to_harmonics(nbarrier, range(1, 26))
+
+        nptest.assert_array_equal(pamps, namps)
+
+        precreated = bbuck.harmonics_to_waveform(
+            centers, range(1, 26), pamps, pphases
+        )
+        nrecreated = bbuck.harmonics_to_waveform(
+            centers, range(1, 26), namps, nphases
+        )
+
+        nptest.assert_array_almost_equal(precreated, -nrecreated)
+
+
+class TestBarrierBucketGenerator(unittest.TestCase):
+    def test_fixed_barrier(self):
+        cent = 500e-9
+        width = 100e-9
+        ampl = 1e3
+
+        generator = bbuck.BarrierGenerator(cent, width, ampl)
+        bin_cents = np.linspace(0, 1000e-9, 1000)
+        wave = generator.waveform_at_time(1, bin_cents)
+
+        wave_pts = np.where(wave != 0)[0]
+        self.assertAlmostEqual(
+            bin_cents[wave_pts[-1]] - bin_cents[wave_pts[0]], width, places=2
+        )
+        self.assertAlmostEqual(np.mean(bin_cents[wave_pts]), cent, places=2)
+
+    def test_variable_barrier(self):
+        bin_cents = np.linspace(0, 1000e-9, 10000)
+
+        peak = [[0, 1], [1e3, 4e3]]
+        t_cent = [[0, 1], [200e-9, 800e-9]]
+        t_width = [[0, 1], [100e-9, 150e-9]]
+
+        generator = bbuck.BarrierGenerator(t_cent, t_width, peak)
+
+        for t in np.linspace(0, 1, 10):
+            peak_exp = np.interp(t, peak[0], peak[1])
+            cent_exp = np.interp(t, t_cent[0], t_cent[1])
+            width_exp = np.interp(t, t_width[0], t_width[1])
+            wave = generator.waveform_at_time(t, bin_cents)
+
+            self.assertAlmostEqual(np.max(wave), peak_exp, places=1)
+            self.assertAlmostEqual(np.min(wave), -peak_exp, places=1)
+
+            wave_pts = np.where(wave != 0)[0]
+            self.assertAlmostEqual(
+                bin_cents[wave_pts[-1]] - bin_cents[wave_pts[0]],
+                width_exp,
+                places=1,
+            )
+            self.assertAlmostEqual(
+                np.mean(bin_cents[wave_pts]), cent_exp, places=1
+            )
+
+    def test_to_fourier_series(self):
+        cent = 500e-9
+        width = 100e-9
+        ampl = 1e3
+
+        generator = bbuck.BarrierGenerator(cent, width, ampl)
+
+        times = np.linspace(0, 1, 10)
+        t_rev = np.zeros_like(times) + 1000e-9
+        harmonics = np.arange(1, 11)
+
+        # Unfiltered
+        harms, amps, phases = generator.to_fourier_series(
+            times, t_rev, harmonics, m=0
+        )
+
+        for a, p in zip(amps, phases):
+            self.assertListEqual(list(a[0]), list(times))
+            self.assertListEqual(list(p[0]), list(times))
+        self.assertListEqual(list(harms), list(harmonics))
+
+        bin_width = t_rev[0] / (10 * harmonics[-1])
+        n_bins = int(t_rev[0] / bin_width)
+        bin_cents = np.linspace(0, t_rev[0], n_bins)
+
+        barrier = bbuck.compute_sin_barrier(cent, width, ampl, bin_cents)
+        amps_exp, phases_exp = bbuck.waveform_to_harmonics(barrier, harmonics)
+
+        g_comp = bbuck._gain_compensation(
+            bin_cents, barrier, harms, amps_exp, phases_exp, t_rev[0]
+        )
+
+        amps_exp /= g_comp
+
+        for i, (a, p) in enumerate(zip(amps, phases)):
+            self.assertEqual(a[1, 0], amps_exp[i])
+            self.assertEqual(p[1, 0], phases_exp[i])
+
+
+if __name__ == "__main__":
+    unittest.main()
