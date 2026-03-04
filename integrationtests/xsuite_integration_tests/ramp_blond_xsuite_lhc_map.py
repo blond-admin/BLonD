fixed dt_shift calculation , removing phi_rf

integrationtests/xsuite_integration_tests/ramp_blond_xsuite_lhc_map.py
@@ -67,7 +67,7 @@ def run_simulation(n_turns: int, init_distribution: dict):
         section_index=1,
         voltage=rf_voltage,
         harmonic=harmonic,
-        phi_rf=0,
+        phi_rf=-np.pi,
         circumference=circumference,
         total_energy=None,
         is_below_transition=None,

integrationtests/xsuite_integration_tests/simple_xsuite_line.py
@@ -36,7 +36,7 @@ def run_simulation(n_turns):
     tab = line.get_table()
     tab_cav = tab.rows[tab.element_type == "Cavity"]
     for nn in tab_cav.name:
-        line[nn].lag = 3.141592653589793 / np.pi * 180
+        line[nn].lag = 180
         line[nn].frequency = 200266136.96284187
 
     N_TURNS = n_turns

integrationtests/xsuite_integration_tests/simple_xsuite_map.py
@@ -51,7 +51,7 @@ def run_simulation(n_turns):
     xsuite_cavity = xt.Cavity(
         voltage=rf_voltage,
         frequency=400788731.3867354,
-        lag=3.141592653589793 / np.pi * 180,
+        lag=180,
     )
     # Create line
     line = xt.Line(elements=[matrix], element_names={"matrix"})