f1ab3c2359563390660d76b30ae53df2bab4ad88
lthiele <leonard.thiele@cern.ch>
2026-03-06 14:24:34 +0100

    tests mostly working

--- unittests/physics/feedbacks/test_cavity_feedback.py
+++ unittests/physics/feedbacks/test_cavity_feedback.py
@@ -1,6 +1,3 @@
-import unittest
-from unittest.mock import Mock
-
 import numpy as np
 import pytest
 
@@ -8,12 +5,10 @@ from blond import (
     Beam,
     ConstantMagneticCycle,
     DriftSimple,
-    Resonators,
     Ring,
     Simulation,
     SingleHarmonicRFStation,
     StaticProfile,
-    WakeField,
     mu_plus,
 )
 from blond.generals.distributed.distributed_array import DistributedArray
@@ -21,9 +16,8 @@ from blond.physics.feedbacks.cavity_feedback import (
     IQCavityFeedback,
     IQCavityFeedbackTimingClass,
 )
-from blond.physics.impedances.solvers import (
-    SingleTurnResonatorConvolutionSolver,
-)
+
+DEBUG_PLOTTING = False
 
 
 class IQFDBKTester(IQCavityFeedback):
@@ -37,59 +31,10 @@ class IQFDBKTester(IQCavityFeedback):
         pass
 
 
-@pytest.mark.skip
-class wtftest(unittest.TestCase):
-    def setUp(self) -> None:
-        # self.profile = StaticProfile.from_cutoff(0, 1e-9, 5e9)
-        self.profile = Mock(StaticProfile)
-        self.rf_station = SingleHarmonicRFStation(
-            phi_rf=0,
-            harmonic=5,
-            voltage=5e6,
-            local_wakefield=WakeField(
-                profile=self.profile,
-                solver=SingleTurnResonatorConvolutionSolver(),
-                sources=[
-                    Resonators(
-                        center_frequencies=1,
-                        quality_factors=1,
-                        shunt_impedances=1,
-                    )
-                ],
-            ),
-        )
-        circumference = 5
-        drift = DriftSimple(circumference, momentum_compaction_factor=0)
-        self.ring = Ring(
-            circumference=circumference, check_section_indices=False
-        )
-        self.ring.add_elements([self.rf_station, drift])
-
-        self.beam = Beam(
-            intensity=1, particle_type=mu_plus, is_counter_rotating=False
-        )
-        self.beam._dt = DistributedArray(np.zeros(5))
-        self.beam._dE = DistributedArray(np.zeros(5))
-        self.beam._ids = DistributedArray(np.arange(5))
-        self.beam._flags = DistributedArray(np.zeros(5))
-
-        cnst_cycle = ConstantMagneticCycle(
-            reference_particle=mu_plus, value=63.0e9, in_unit="momentum"
-        )
-
-        sim = Simulation(self.ring, cnst_cycle)
-
-        sim.run_simulation(self.beam, n_turns=5)
-
-    def test__init__(self) -> None:
-        pass
-
-
-class IQCavityFeedbackTimingClassTest(unittest.TestCase):
-    def setUp(self):
+class TestIQCavityFeedbackTimingClass:
+    def setup_simulation(self):
         # single section
         self.profile = StaticProfile.from_cutoff(0, 1e-9, 5e9)
-        # self.profile = Mock(spec=StaticProfile)
         self.rf_station = SingleHarmonicRFStation(
             phi_rf=0.0, harmonic=3, voltage=5e6
         )
@@ -108,15 +53,30 @@ class IQCavityFeedbackTimingClassTest(unittest.TestCase):
         self.beam._ids = DistributedArray(np.arange(5))
         self.beam._flags = DistributedArray(np.zeros(5))
 
-    def test_for_discontinuity(self) -> None:
-        import logging
+    test_data_discontinuity = [
+        (0, 0),
+        (0, 0.13),
+        (0, -0.13),
+        (-1, 0),
+        (-1, 0.13),
+        (-1, -0.13),
+        (1, 0),
+        (1, 0.13),
+        (1, -0.13),
+    ]
 
-        logging.basicConfig(level=logging.DEBUG)
+    @pytest.mark.parametrize(
+        "phase_shift,delta_omega_factor", test_data_discontinuity
+    )
+    def test_for_discontinuity_single_section_no_acceleration(
+        self, phase_shift: float, delta_omega_factor: float
+    ) -> None:
+        self.setup_simulation()
         cav_fdbk_timing = IQCavityFeedbackTimingClass(
             profile=self.profile,
         )
         self.rf_station.attach_cavity_feedback(cav_fdbk_timing)
-        # self.rf_station.phi_rf_design = 3.
+        self.rf_station.phi_rf_design = phase_shift
 
         cnst_cycle = ConstantMagneticCycle(
             reference_particle=mu_plus, value=63.0e9, in_unit="momentum"
@@ -133,7 +93,7 @@ class IQCavityFeedbackTimingClassTest(unittest.TestCase):
         def callback(simulation: Simulation, beam: Beam):
             time_array.append(
                 np.linspace(
-                    0,  # cav_fdbk_timing.residual_time_last_turn_previous,
+                    0,
                     2
                     * np.pi
                     / self.rf_station.omega_rf_design
@@ -150,7 +110,7 @@ class IQCavityFeedbackTimingClassTest(unittest.TestCase):
             rf_centers_array.append(cav_fdbk_timing.rf_centers_current_turn)
             if simulation.turn_i.value == 0:
                 self.rf_station.delta_omega_rf = (
-                    0.13 * self.rf_station.omega_rf
+                    delta_omega_factor * self.rf_station.omega_rf
                 )
 
         n_turns_to_simulate = 10
@@ -168,23 +128,30 @@ class IQCavityFeedbackTimingClassTest(unittest.TestCase):
         total_time_array = time_array.flatten()
         import matplotlib.pyplot as plt
 
-        for trn_ind in range(0, n_turns_to_simulate):
-            plt.plot(time_array[trn_ind], voltage_array[trn_ind], marker="o")
-            for _ in range(len(rf_centers_array[trn_ind])):
-                plt.axvline(
-                    x=rf_centers_array[trn_ind][_], marker="x", color="green"
+        if DEBUG_PLOTTING:
+            for trn_ind in range(0, n_turns_to_simulate):
+                plt.plot(
+                    time_array[trn_ind], voltage_array[trn_ind], marker="o"
                 )
-            if trn_ind != 0:
-                plt.axvline(
-                    x=total_time_array[int(trn_ind * vals_per_turn)],
-                    color="red",
-                    ls="--",
-                )
+                for _ in range(len(rf_centers_array[trn_ind])):
+                    plt.axvline(
+                        x=rf_centers_array[trn_ind][_],
+                        marker="x",
+                        color="green",
+                    )
+                if trn_ind != 0:
+                    plt.axvline(
+                        x=total_time_array[int(trn_ind * vals_per_turn)],
+                        color="red",
+                        ls="--",
+                    )
 
-        # plt.xlim(total_time_array[450], total_time_array[550])
+            plt.show()
 
-        plt.show()
-
+        for ind in range(1, len(voltage_array) - 1):
+            np.testing.assert_allclose(
+                voltage_array[ind - 1][-1] + 3, voltage_array[ind][0] + 3
+            )  # +3 to be robust against zero-relative problems
         assert all(np.diff(voltage_array.flatten()) < 0.04)
 
         pass
