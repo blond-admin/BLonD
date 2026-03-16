0a0d102ece8ba1364d9b5cc83846f60ae501b4de
Alexandre Lasheen <alexandre.lasheen@cern.ch>
2026-03-16 14:06:09 +0100

    WIP: making the simulation with multiple [SR+Drift]

--- blond/examples/scripts/EX_Synchrotron_Radiation_Matched.py
+++ blond/examples/scripts/EX_Synchrotron_Radiation_Matched.py
@@ -30,6 +30,7 @@ from blond.acc_math.analytic.synchrotron_radiation.utilities import (
 )
 from blond.experimental.beam_preparation.synchrotron_radiation_matcher import (
     SynchrotronRadiationMatcher,
+    sawtooth_factor,
 )
 from blond.handle_results.observables import BeamStatisticsOncePerTurn
 from blond.physics.synchrotron_radiation.synchrotron_radiation_master import (
@@ -79,22 +80,34 @@ class SynchrotronRadiationSimulation:
             self.circumference,
             radiation_integrals=self.radiation_integrals,
         )
-        self.ring.add_element(self.cavity)
 
-        # checks for rfcavity in each section prevents raising the following
-        # number
-        number_of_sections = 1
-        for i in range(number_of_sections):
+        # self.ring.add_element(self.cavity)
+
+        one_turn_execution_order = [self.cavity]
+        self.number_of_sections = 2
+
+        for i in range(self.number_of_sections):
             drift = DriftSimple(
                 name=f"drift{i + 1}",
-                orbit_length=self.circumference / number_of_sections,
+                orbit_length=self.circumference / self.number_of_sections,
                 momentum_compaction_factor=self.momentum_compaction_factor,
                 section_index=i,
             )
-            self.ring.add_element(
-                drift,
-                section_index=i,
+
+            one_turn_execution_order.append(drift)
+
+            if i == (self.number_of_sections - 1):
+                continue
+
+            empty_rf_station = SingleHarmonicRFStation(
+                voltage=0,
+                harmonic=1,
+                phi_rf=0,
+                section_index=i + 1,
             )
+            one_turn_execution_order.append(empty_rf_station)
+
+        self.ring.add_elements(one_turn_execution_order, reorder=False)
 
         self.SRHandler = SynchrotronRadiationMaster(
             # track_before_element_type = DriftBaseClass,
@@ -154,11 +167,19 @@ def main(n_turns: int = 100):
 
     # custom_action(simulation, beam=params.beam)
 
+    def get_bunch_relative_energy(simulation, beam):
+        bunch_relative_energy[simulation.turn_i.value + 1] = np.mean(
+            beam.read_partial_dE()
+        )
+
+    bunch_relative_energy = np.zeros(n_turns + 1)
+    bunch_relative_energy[0] = np.mean(params.beam.read_partial_dE())
+
     simulation.run_simulation(
         beams=(params.beam,),
         n_turns=params.n_turns,
         observe=(phase_observation, bunch_statistics),
-        # callbacks=custom_action,
+        callbacks=[get_bunch_relative_energy],
     )
 
     energy_loss_per_turn, damping_time, natural_energy_spread = (
@@ -168,7 +189,7 @@ def main(n_turns: int = 100):
             radiation_integrals=params.radiation_integrals,
         )
     )
-    fig, ax = plt.subplots(nrows=2, figsize=(8, 6), constrained_layout=True)
+    fig, ax = plt.subplots(nrows=3, figsize=(12, 6), constrained_layout=True)
     synchronous_phase = np.pi - np.arcsin(
         energy_loss_per_turn / params.cavity.voltage
     )
@@ -184,20 +205,31 @@ def main(n_turns: int = 100):
     ax[0].set_ylabel("Bunch position [ns]")
     ax[0].legend()
 
+    ax[1].plot(bunch_relative_energy, label="Bunch relative energy")
     ax[1].plot(
+        energy_loss_per_turn
+        * sawtooth_factor(params.number_of_sections, "sr+drift")
+        * np.ones(len(bunch_statistics.bunch_position)),
+        label="Reference energy before the kick",
+    )
+    ax[1].set_xlabel("Turn number")
+    ax[1].set_ylabel("Bunch relative energy [eV]")
+    ax[1].legend()
+
+    ax[2].plot(
         bunch_statistics.energy_spread / 20e9 * 100,
         label="Energy spread evolution",
     )
-    ax[1].plot(
+    ax[2].plot(
         natural_energy_spread
         * np.ones(len(bunch_statistics.bunch_position))
         * 100,
         "r--",
         label="Natural energy spread",
     )
-    ax[1].set_xlabel("Turn number")
-    ax[1].set_ylabel("Energy spread [%]")
-    ax[1].legend()
+    ax[2].set_xlabel("Turn number")
+    ax[2].set_ylabel("Energy spread [%]")
+    ax[2].legend()
     os.makedirs("results/EX_Synchrotron_Radiation/", exist_ok=True)
     plt.savefig("results/EX_Synchrotron_Radiation/energy_spread_evolution.png")
 
