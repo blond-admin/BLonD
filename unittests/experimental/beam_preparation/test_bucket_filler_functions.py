e4d628b7476cf83042a7df38412e0fe2e7985d8f
lthiele <leonard.thiele@cern.ch>
2026-03-16 17:58:44 +0100

    added unittest to assert warning on unsucessful minimisation

--- unittests/experimental/beam_preparation/test_bucket_filler_functions.py
+++ unittests/experimental/beam_preparation/test_bucket_filler_functions.py
@@ -17,14 +17,37 @@ from blond.experimental.beam_preparation.metric_functions import (
 
 class TestGeneralizedBucketFiller(unittest.TestCase):
     def setUp(self):
+        grid_size = 100
         self.time_grid, self.deltaE_grid = np.meshgrid(
-            np.arange(-10, 11, 21), np.arange(-10, 11, 21)
+            np.linspace(-10, 11, num=grid_size),
+            np.linspace(-10, 11, num=grid_size),
         )
         self.hamilton_2D = np.exp(-self.time_grid * self.deltaE_grid)
 
-    def test_can_converge(self):
+    def test_cannot_converge(self):
         desired_metric = 5
-        tolerance = 0.1
+        tolerance = 0.001
+        with self.assertWarnsRegex(
+            RuntimeWarning, "Specified metric accuracy was not reached"
+        ):
+            density = multibunch_match_metric_to_hamilton(
+                time_grid=self.time_grid,
+                deltaE_grid=self.deltaE_grid,
+                hamilton_2D=self.hamilton_2D,
+                metric_list=[desired_metric],
+                intensity_frac_list=[1],
+                n_buckets=1,
+                max_metric_diff=tolerance,
+                density_function=gaussian_density,
+                metric_function=rms_emittance,
+                max_iterations=1000,
+                # free_parameter_guess=10,
+            )
+
+    def test_can_converge(self):
+        desired_metric = 25
+        tolerance = 0.001
+
         density = multibunch_match_metric_to_hamilton(
             time_grid=self.time_grid,
             deltaE_grid=self.deltaE_grid,
@@ -35,8 +58,8 @@ class TestGeneralizedBucketFiller(unittest.TestCase):
             max_metric_diff=tolerance,
             density_function=gaussian_density,
             metric_function=rms_emittance,
-            max_iterations=100,
-            free_parameter_guess=10,
+            max_iterations=1000,
+            # free_parameter_guess=10,
         )
 
         fitted_metric = rms_emittance(
