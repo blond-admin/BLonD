2219388a6a7149e0eae0fe80dd1676721ad678f7
slauber <simon.fabian.lauber@cern.ch>
2026-03-16 11:21:01 +0100

    Revert "Deleted files to help merging `develop` into `blonder`, to be reverted after merge"
    This reverts commit 01608123db0283bfb674a790e5c0b33c8299de28.

---
+++ dev_tools/performance_blond3/backends/histogram.py
@@ -0,0 +1,56 @@
+# Copyright CERN. This software is distributed under the
+# terms of the GNU General Public Licence version 3 (GPL Version 3),
+# copied verbatim in the file LICENCE.txt.
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization or
+# submit itself to any jurisdiction.
+# Project website: http://blond.web.cern.ch/
+
+"""
+Measure the performance of the histogram function.
+
+Notes
+-----
+Authors:
+Leonard Thiele
+"""
+
+import time
+
+import numba
+import numpy as np
+
+from blond.core.backends.backend import backend
+
+
+def main():
+    """Measure the performance of the histogram function."""
+    rng = np.random.default_rng(42)
+    arr_sizes = np.array([1e3, 1e5, 1e7, 1e9], dtype=int)
+    specials = ["numba", "python", "cpp"]
+    times = np.zeros((len(specials), len(arr_sizes)))
+    for arr_ind, arr_size in enumerate(arr_sizes):
+        input_array = (rng.random(arr_size) - 0.5) * 20
+        for spec_ind, special in enumerate(specials):
+            for _ in range(10):
+                backend.set_specials(special)
+                numba.set_num_threads(14)
+                array_write = backend.zeros(21, dtype=backend.float)
+                t0 = time.perf_counter()
+                backend.specials.histogram(
+                    array_read=backend.array(
+                        input_array, dtype=backend.float
+                    ),  # casting to correct data type
+                    array_write=array_write,
+                    start=-12.0,
+                    stop=8.0,
+                )
+                t1 = time.perf_counter()
+                times[spec_ind, arr_ind] += t1 - t0
+    print("numba times" + str(times[0].tolist()))
+    print("python times" + str(times[1].tolist()))
+    print("cpp times" + str(times[2].tolist()))
+
+
+if __name__ == "__main__":
+    main()
