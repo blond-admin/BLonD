f20af2eafbadd726ba8e96c2ee37a6bdc349aaa2
lvalle <lina.valle@cern.ch>
2026-04-07 15:59:22 +0200

    added notes on the Schedulable class to help developpers using the feature

--- blond/core/base.py
+++ blond/core/base.py
@@ -148,6 +148,20 @@ class Schedulable:
     schedules
         Dictionary to update a certain attribute by some value
         via `apply_schedules`
+
+    Notes
+    -----
+    Schedulable attributes require two manual entries in the dedicated class:
+    - in the initialization:
+    >>> def __init__(self, attribute_to_be_scheduled)
+    >>>     self._add_intended_schedule("attribute_to_be_scheduled")
+    - in the tracking method:
+    >>> def __track(self, beam: BeamBaseClass, **kwargs):
+    >>>     if self.schedule_active:
+    >>>         self.apply_schedules(
+    >>>            turn_i=self._turn_i.value,
+    >>>            reference_time=float(beam.reference.time),
+    >>>         )
     """
 
     def __init__(self, **kwargs) -> None:
