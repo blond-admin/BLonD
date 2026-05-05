99625d1876395db813a3a7affb1df46586283280
elamb <elamb@cern.ch>
2026-05-05 16:10:09 +0200

    added unit tests to test matching convergence

--- unittests/beam_preparation/test_filamenation_matcher.py
+++ unittests/beam_preparation/test_filamenation_matcher.py
@@ -22,8 +22,8 @@ class TestFilamentationMatcher(unittest.TestCase):
         self.sim = self.simulation_.simulation
         self.beam = self.simulation_.beam1
 
-        self.time_limit = (0.0, 1e-9)
-        self.energy_limit = (-1e6, 1e6)
+        self.time_limit = (0.0, 3e-9)
+        self.energy_limit = (-1e9, 1e9)
 
     def test___init__(self):
         matcher = FilamentationMatcher(
@@ -100,6 +100,59 @@ class TestFilamentationMatcher(unittest.TestCase):
         matcher.prepare_beam(simulation=self.sim, beam=self.beam)
 
         self.assertEqual(self.beam.intensity, initial_intensity)
+
+    def test_converges_towards_matched_bunch(self):
+        simulation_1 = ExampleSimulation01()
+        simulation_2 = ExampleSimulation01()
+        simulation_1.simulation.turn_i.value = 0  # there is some cacheing
+        simulation_2.simulation.turn_i.value = 0
+
+        # --- Matcher with few iterations (poorly matched)
+        matcher_few = FilamentationMatcher(
+            time_limit=self.time_limit,
+            energy_limit=self.energy_limit,
+            n_macroparticles=1000,
+            n_iter=5,
+            animate=True,
+            purge=True,
+            purge_limit_time=self.time_limit,
+            purge_limit_energy=self.energy_limit,
+        )
+
+        # --- Matcher with more iterations
+        matcher_many = FilamentationMatcher(
+            time_limit=self.time_limit,
+            energy_limit=self.energy_limit,
+            n_macroparticles=1000,
+            n_iter=400,
+            animate=True,
+            purge=True,
+            purge_limit_time=self.time_limit,
+            purge_limit_energy=self.energy_limit,
+        )
+
+        matcher_few.prepare_beam(simulation_1.simulation, simulation_1.beam1)
+        matcher_many.prepare_beam(simulation_2.simulation, simulation_2.beam1)
+
+        # at injection
+        dt_few_before = simulation_1.beam1.read_partial_dt().copy()
+        dt_many_before = simulation_2.beam1.read_partial_dt().copy()
+
+        simulation_1.simulation.run_simulation(
+            beams=[simulation_1.beam1], n_turns=5, show_progressbar=False
+        )
+        simulation_2.simulation.run_simulation(
+            beams=[simulation_2.beam1], n_turns=5, show_progressbar=False
+        )
+
+        dt_few_after = simulation_1.beam1.read_partial_dt()
+        dt_many_after = simulation_2.beam1.read_partial_dt()
+
+        change_few = np.std(dt_few_after - dt_few_before)
+        change_many = np.std(dt_many_after - dt_many_before)
+
+        # Better matched beam should change less
+        self.assertLess(change_many, change_few)
 
 
 if __name__ == "__main__":
