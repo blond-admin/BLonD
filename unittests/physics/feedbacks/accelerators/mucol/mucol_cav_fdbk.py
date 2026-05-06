corrected calculation of input parameters

unittests/physics/feedbacks/accelerators/mucol/mucol_cav_fdbk.py
@@ -13,12 +13,10 @@ from handle_results.observables_as_elements import (
 from physics.feedbacks.cavity_feedback import IQCavityFeedbackTimingClass
 from physics.impedances.solvers import (
     MultiPassResonatorSolver,
-    SingleTurnResonatorConvolutionSolver,
 )
 from scipy.constants import elementary_charge, speed_of_light
 from scipy.interpolate import interp1d
 from specifics.muon_collider.beam_preparation import (
-    load_beam_coordinates_counterrot_from_file,
     load_beam_coordinates_from_file,
 )
 
@@ -35,7 +33,6 @@ from blond import (
     mu_plus,
 )
 from blond.cycles.magnetic_cycle import MagneticCyclePerTurnAllRFStations
-from unittests.physics.impedances.comparisons.mtw import voltage_per_cavity
 
 n_slices = 2**10
 
@@ -89,14 +86,14 @@ def setup_and_run(
 
     if rcs == "RCS1":
         R_over_Q = 3 * 518
-        Q_L = 1.29e6
+        # Q_L = 1.29e6
         phi_s = 2.5830872929516078  # 143
         alpha_p = 10.395e-4
         bunch_intensity = 2.7e12
         circumference = 5990
         injection_energy = 63e9
         ejection_energy = 313.8e9
-        f_det = -1040
+        # f_det = -1040
         harmonic = 25900
         n_turns = 18
 
@@ -109,37 +106,37 @@ def setup_and_run(
         circumference = 35000
         injection_energy = 1500e9
         ejection_energy = 5000e9
-        f_det = -190
+        # f_det = -190
         harmonic = 151400
         n_turns = 55
     else:
         raise ValueError("Unknown RCS")
-    f_det = 0
+    # f_det = 0
     # phi_s = np.pi / 2
     harmonic = int(harmonic - harmonic % n_stations)
 
     voltage_per_cavity = 31140000.0
     energy_gain_per_turn = (ejection_energy - injection_energy) / n_turns
     total_voltage = energy_gain_per_turn / np.sin(phi_s)
-    total_voltage = 1e9
+    # total_voltage = 1e9
     voltage_per_station = total_voltage / n_stations
     n_cavities = voltage_per_station / voltage_per_cavity * n_stations
     cav_per_station = n_cavities / n_stations
 
-    delta_omega = 2 * np.pi * f_det
+    # delta_omega = 2 * np.pi * f_det
 
     ring = Ring(circumference=circumference, check_section_indices=False)
     magnetic_cycle = MagneticCyclePerTurnAllRFStations(
         value_init=injection_energy,
-        # values_after_rf_station_per_turn=np.linspace(
-        #     injection_energy + energy_gain_per_turn / n_stations,
-        #     ejection_energy,
-        #     n_turns * n_stations,
-        # ).reshape(n_stations, n_turns, order="F"),
-        values_after_rf_station_per_turn=injection_energy
-        * np.ones(
+        values_after_rf_station_per_turn=np.linspace(
+            injection_energy + energy_gain_per_turn / n_stations,
+            ejection_energy,
             n_turns * n_stations,
         ).reshape(n_stations, n_turns, order="F"),
+        # values_after_rf_station_per_turn=injection_energy
+        # * np.ones(
+        #     n_turns * n_stations,
+        # ).reshape(n_stations, n_turns, order="F"),
         in_unit="total energy",
         reference_particle=mu_plus,
     )
@@ -163,22 +160,34 @@ def setup_and_run(
 
     F_b = 2 * (-0.8330691630689783 - 0.060605390015254904j)
 
-    # delta_omega = omega_rf * R_over_Q * np.abs(F_b) * beam_current * np.cos(phi_s) / (2 * voltage_per_cavity)
-    phi_s = np.pi / 2
+    delta_omega = (
+        -omega_rf
+        * R_over_Q
+        * np.abs(F_b)
+        * beam_current
+        * np.sin(phi_s)
+        / (2 * voltage_per_cavity)
+    )
+    # delta_omega = 0
+    f_det = delta_omega / (2 * np.pi)
+    # phi_s = np.pi / 2
     Q_L = voltage_per_cavity / (
-        R_over_Q * (np.abs(F_b) * beam_current * np.sin(phi_s)) ** 2
-        - (
-            np.abs(F_b) * beam_current * np.cos(phi_s)
-            + voltage_per_cavity * 2 * delta_omega / (omega_rf * R_over_Q)
+        R_over_Q
+        * np.sqrt(
+            (np.abs(F_b) * beam_current * np.cos(phi_s)) ** 2
+            + (
+                np.abs(F_b) * beam_current * np.sin(phi_s)
+                + voltage_per_cavity * 2 * delta_omega / (omega_rf * R_over_Q)
+            )
+            ** 2
         )
-        ** 2
     )
 
     I_g = (
         voltage_per_cavity
         / (2 * R_over_Q)
         * (1 / Q_L - 2j * delta_omega / omega_rf)
-        + np.abs(F_b) * beam_current * np.exp(1j * (phi_s - np.pi / 2)) / 2
+        + np.abs(F_b) * beam_current * np.exp(-1j * (phi_s - np.pi / 2)) / 2
     )
 
     I_g_ampl = np.abs(I_g)
@@ -325,19 +334,18 @@ def setup_and_run(
 
     bunch_observation.active = True
 
-    beam_freq = np.fft.rfftfreq(
-        100 * profile_list[-1].n_bins, profile_list[-1].hist_step
-    )
-    beam_spectrum = profile_list[-1].beam_spectrum(
-        100 * profile_list[-1].n_bins
-    )
-    rf_frequency_component = (
-        interp1d(beam_freq, beam_spectrum)(omega_rf / (2 * np.pi))
-        / beam_spectrum[0]
-    )
+    # F_B calculation
+    # beam_freq = np.fft.rfftfreq(
+    #     100 * profile_list[-1].n_bins, profile_list[-1].hist_step
+    # )
+    # beam_spectrum = profile_list[-1].beam_spectrum(
+    #     100 * profile_list[-1].n_bins
+    # )
+    # rf_frequency_component = (
+    #     interp1d(beam_freq, beam_spectrum)(omega_rf / (2 * np.pi))
+    #     / beam_spectrum[0]
+    # )
     # (-0.8330691630689783-0.060605390015254904j)
-
-    profile_list[-1].beam_spectrum(100 * profile_list[-1].n_bins)
 
     sim.run_simulation(
         (beam,),
@@ -443,6 +451,13 @@ def plot_ind_volt_cav_fdbk_voltage(ind_volt_obs_list, cav_fdbk_obs_list):
             np.real(cav_fdbk_obs_list[1][0].kick_voltage_fine[idx])
             - cavity_voltage_raw,
             ls="--",
+            color=clr,
+            label="real fdbk",
+        )
+        ax[0, 1].plot(
+            np.imag(cav_fdbk_obs_list[1][0].kick_voltage_fine[idx]),
+            # - cavity_voltage_raw,
+            ls=":",
             color=clr,
             label="real fdbk",
         )