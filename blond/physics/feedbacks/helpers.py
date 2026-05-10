cf7b16202f8b9524383b4ef2ac77c973749cac9e
lthiele <leonard.thiele@cern.ch>
2026-05-10 14:02:39 +0200

    helpers should be correct, I_F and Q_F look correct

--- blond/physics/feedbacks/helpers.py
+++ blond/physics/feedbacks/helpers.py
@@ -161,9 +161,9 @@ def rf_beam_current(
     # Mix with frequency of interest; remember factor 2 demodulation
     # TODO: where do we have to apply the demodulation?
     I_f = (
-        -2.0 * charges * np.cos(omega_c * profile.hist_x)
+        2.0 * charges * np.cos(omega_c * profile.hist_x)
     )  # TODO: flipperydoo?
-    Q_f = 2.0 * charges * np.sin(omega_c * profile.hist_x)
+    Q_f = -2.0 * charges * np.sin(omega_c * profile.hist_x)
 
     # Pass through a low-pass filter
     if use_lowpass_filter is True:
