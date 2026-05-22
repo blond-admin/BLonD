3ffc6a7a27d4caac2e18514ebaa891c1d569b2c6
Birk Emil Karlsen-Baeck <birk.beck@cern.ch>
2026-05-22 15:11:14 +0200

    [blond][physics][feedbacks][accelerators] Cosmetic changes to __init__ files

--- blond/physics/feedbacks/accelerators/psb/__init__.py
+++ blond/physics/feedbacks/accelerators/psb/__init__.py
@@ -6,14 +6,6 @@
 # submit itself to any jurisdiction.
 # Project website: http://blond.web.cern.ch/
 
-# Copyright CERN. This software is distributed under the
-# terms of the GNU General Public Licence version 3 (GPL Version 3),
-# copied verbatim in the file LICENCE.txt.
-# In applying this licence, CERN does not waive the privileges and immunities
-# granted to it by virtue of its status as an Intergovernmental Organization or
-# submit itself to any jurisdiction.
-# Project website: http://blond.web.cern.ch/
-
 """Module to manage and describe rf feedback systems for the Proton Synchrotron Booster."""
 
 __all__ = ["PSBBeamControl"]
