f927a5d8cafc111953d9625d04151fbef87d6922
Birk Emil Karlsen-Baeck <birk.beck@cern.ch>
2026-06-11 15:57:26 +0200

    [blond][legacy][blond2][utils][butils wrap python] Another attempt at fixing the failing unittests. Copied type ignore from blond3

--- blond/legacy/blond2/utils/butils_wrap_python.py
+++ blond/legacy/blond2/utils/butils_wrap_python.py
@@ -537,13 +537,13 @@ def beam_phase(
     phi_rf: float,
     bin_size: float,
 ) -> float:
-    scoeff = trapezoid(
+    scoeff = trapezoid( # type: ignore
         np.exp(alpha * (bin_centers))
         * np.sin(omega_rf * bin_centers + phi_rf)
         * profile,
         dx=bin_size,
     )
-    ccoeff = trapezoid(
+    ccoeff = trapezoid( # type: ignore
         np.exp(alpha * (bin_centers))
         * np.cos(omega_rf * bin_centers + phi_rf)
         * profile,
@@ -560,10 +560,10 @@ def beam_phase_fast(
     phi_rf: float,
     bin_size: float,
 ) -> float:
-    scoeff = trapezoid(
+    scoeff = trapezoid( # type: ignore
         profile * np.sin(omega_rf * bin_centers + phi_rf), dx=bin_size
     )
-    ccoeff = trapezoid(
+    ccoeff = trapezoid( # type: ignore
         profile * np.cos(omega_rf * bin_centers + phi_rf), dx=bin_size
     )
 
