38294e444609f8b1bac5bc10ccc8cb018c210ea6
lvalle <lina.valle@cern.ch>
2026-07-22 09:37:05 +0200

    fixed type hint

--- blond/trackers/tracker.py
+++ blond/trackers/tracker.py
@@ -319,7 +319,7 @@ class RingAndRFTracker:
         periodicity: bool = False,
         interpolation: bool = False,
         with_xsuite: bool = False,
-        profile: Optional[Profile] = None,
+        profile: Optional[Profile|SparseProfileBaseClass] = None,
         total_induced_voltage: Optional[TotalInducedVoltage] = None,
     ):
         # Set up logging
