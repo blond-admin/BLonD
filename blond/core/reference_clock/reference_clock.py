6779b6d328e80818174fd943ff61d3399b8773a4
slauber <simon.fabian.lauber@cern.ch>
2026-09-25 17:37:46 +0200

    Revert "Used math.isnan in ReferenceCoordinates.beta"
    This reverts commit 5b7de324c2d0d1b1060a92883fdc822133c6fc1c.

--- blond/core/reference_clock/reference_clock.py
+++ blond/core/reference_clock/reference_clock.py
@@ -10,8 +10,9 @@
 
 from __future__ import annotations
 
-import math
 from typing import TYPE_CHECKING
+
+import numpy as np
 
 from blond.core.reference_clock.reference_clock_numba import beta as beta_nb
 from blond.core.reference_clock.reference_clock_numba import gamma as gamma_nb
@@ -116,7 +117,7 @@ class ReferenceCoordinates:
             Beam reference fraction of speed of light (v/c0) [].
         """
         beta = beta_nb(self._total_energy, self._particle_type.mass_inv)
-        assert not math.isnan(beta), f"{beta=}"  # np.isnan is slow on floats
+        assert not np.isnan(beta), f"{beta=}"
         return beta
 
     @property
